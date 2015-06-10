a0ff1f2801e7110eba2e478002dda74ada9071f6
ruki <waruqi@gmail.com>
2015-06-10 15:35:31 +0800

    add option switch for xmake.lua

--- tests/console/src/demo/xmake.lua
+++ tests/console/src/demo/xmake.lua
@@ -9,6 +9,10 @@ add_target("demo_c")
     add_cflags("-DHELLO1")
 
     if modes("release") then add_defines("NDEBUG") end
+    if option("option1") then add_defines("OPTION1") end
+    if option("option2") then add_defines("OPTION2") end
+    if option("option3") then add_defines("OPTION3") end
+    if option("option4") then add_defines("OPTION4") end
 
 add_target("demo_cpp")
     set_kind("binary")
@@ -19,6 +23,7 @@ add_target("demo_cpp")
     add_includedirs("$(buildir)")
     add_undefines("HELLO2")
     add_cxxflags("-DHELLO1")
+    add_options("option1", "option2", "option3", "option4")
 
     if plats("macosx", "ios") then
 
