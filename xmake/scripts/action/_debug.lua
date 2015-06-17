292a8fde945c4de3ac3c28e808d383ac029ed62f
ruki <waruqi@gmail.com>
2015-06-17 09:12:11 +0800

    add man and debug action

---
+++ xmake/scripts/action/_debug.lua
@@ -0,0 +1,74 @@
+--!The Automatic Cross-platform Build Tool
+-- 
+-- XMake is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation; either version 2.1 of the License, or
+-- (at your option) any later version.
+-- 
+-- XMake is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with XMake; 
+-- If not, see <a href="http://www.gnu.org/licenses/"> http://www.gnu.org/licenses/</a>
+-- 
+-- Copyright (C) 2009 - 2015, ruki All rights reserved.
+--
+-- @author      ruki
+-- @file        _debug.lua
+--
+
+-- define module: _debug
+local _debug = _debug or {}
+
+-- load modules
+local utils     = require("base/utils")
+local config    = require("base/config")
+local platform  = require("platform/platform")
+    
+-- done the given config
+function _debug.done()
+
+ 
+    -- ok
+    return true
+end
+
+-- the menu
+function _debug.menu()
+
+    return {
+                -- xmake d
+                shortname = 'd'
+
+                -- usage
+            ,   usage = "xmake debug|d [options] [target]"
+
+                -- description
+            ,   description = "Debug target."
+
+                -- options
+            ,   options = 
+                {
+                    {'f', "file",       "kv", "xmake.lua",  "Create a given xmake.lua file."                                }
+                ,   {'P', "project",    "kv", nil,          "Create from the given project directory."
+                                                          , "Search priority:"
+                                                          , "    1. The Given Command Argument"
+                                                          , "    2. The Envirnoment Variable: XMAKE_PROJECT_DIR"
+                                                          , "    3. The Current Directory"                                  }
+
+                ,   {}
+                ,   {'v', "verbose",    "k",  nil,          "Print lots of verbose information."                            }
+                ,   {nil, "version",    "k",  nil,          "Print the version number and exit."                            }
+                ,   {'h', "help",       "k",  nil,          "Print this help message and exit."                             }
+         
+                ,   {}
+                ,   {nil, "target",     "v",  nil,          "Debug a given target"                                          }   
+                }
+            }
+end
+
+-- return module: _debug
+return _debug
