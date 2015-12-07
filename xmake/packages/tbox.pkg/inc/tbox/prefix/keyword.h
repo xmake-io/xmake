47fb1d79952e420735436f9c98d2e683fbb25022
ruki <waruqi@gmail.com>
2015-12-07 11:04:09 +0800

    add static and shared library template for tbox

--- xmake/packages/tbox.pkg/inc/tbox/prefix/keyword.h
+++ xmake/packages/tbox.pkg/inc/tbox/prefix/keyword.h
@@ -170,6 +170,15 @@
 #   define __tb_extern_c_leave__                
 #endif
 
+// export for the shared library
+#if defined(TB_COMPILER_IS_MSVC)
+#   define __tb_export__                        __declspec(dllexport)
+#elif defined(TB_COMPILER_IS_GCC) && ((__GNUC__ >= 4) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3))
+#   define __tb_export__                        __attribute__((visibility("default")))
+#else
+#   define __tb_export__         
+#endif
+
 // has feature
 #ifdef __has_feature
 #   define __tb_has_feature__(x)                            __has_feature(x)
---
+++ xmake/templates/c++/shared_library_tbox/project/src/demo/xmake.lua
@@ -0,0 +1,31 @@
+-- add target
+add_target("demo")
+
+    -- set kind
+    set_kind("binary")
+
+    -- add deps
+    add_deps("[targetname]")
+
+    -- add defines
+    add_defines("__tb_prefix__=\"demo\"")
+
+    -- set the object files directory
+    set_objectdir("$(buildir)/.objs")
+
+    -- add links directory
+    add_linkdirs("$(buildir)")
+
+    -- add includes directory
+    add_includedirs("$(buildir)")
+    add_includedirs("$(buildir)/[targetname]")
+
+    -- add links
+    add_links("[targetname]")
+
+    -- add packages
+    add_options("tbox", "base")
+
+    -- add files
+    add_files("*.cpp") 
+
