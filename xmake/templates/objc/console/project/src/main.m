1b0d7bf2ea3d8c9baa58a4fff86de9d474c1df7c
ruki <waruqi@gmail.com>
2015-12-09 09:38:13 +0800

    add templates for objc/objc++/swift

---
+++ xmake/templates/objc/console/project/src/main.m
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, char** argv)
+{
+    @autoreleasepool
+    {
+        NSLog(@"hello world!");
+    }
+    return 0;
+}
---
+++ xmake/templates/objc/console/project/xmake.lua
@@ -0,0 +1,43 @@
+-- the debug mode
+if modes("debug") then
+    
+    -- enable the debug symbols
+    set_symbols("debug")
+
+    -- disable optimization
+    set_optimize("none")
+end
+
+-- the release mode
+if modes("release") then
+
+    -- set the symbols visibility: hidden
+    set_symbols("hidden")
+
+    -- enable fastest optimization
+    set_optimize("fastest")
+
+    -- strip all symbols
+    set_strip("all")
+end
+
+-- for macosx or ios
+if os("macosx", "ios") then
+
+    -- add framework
+    add_mxflags("-framework Foundation", "-framework CoreFoundation")
+    add_ldflags("-framework Foundation", "-framework CoreFoundation")
+
+    -- enable arc?
+    add_mxflags("-fobjc-arc")
+end
+
+-- add target
+add_target("[targetname]")
+
+    -- set kind
+    set_kind("binary")
+
+    -- add files
+    add_files("src/*.m") 
+
