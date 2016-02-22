c50639da01bb4e03f933e68eb308910a67974632
ruki <waruqi@gmail.com>
2016-02-22 09:51:35 +0800

    add merge static library test

---
+++ tests/merge_static_library/src/interface.h
@@ -0,0 +1,8 @@
+/*! calculate add(a, b) 
+ *
+ * @param a     the first argument
+ * @param b     the second argument
+ *
+ * @return      the result
+ */
+int             add(int a, int b);
---
+++ tests/merge_static_library/xmake.lua
@@ -0,0 +1,50 @@
+-- the debug mode
+if is_mode("debug") then
+    
+    -- enable the debug symbols
+    set_symbols("debug")
+
+    -- disable optimization
+    set_optimize("none")
+end
+
+-- the release mode
+if is_mode("release") then
+
+    -- set the symbols visibility: hidden
+    set_symbols("hidden")
+
+    -- enable fastest optimization
+    set_optimize("fastest")
+
+    -- strip all symbols
+    set_strip("all")
+end
+
+-- add target
+target("merge_static_library")
+
+    -- set kind
+    set_kind("static")
+
+    -- add files
+    add_files("../static_library_c/build/*.a") 
+
+-- add target
+target("test")
+
+    -- set kind
+    set_kind("binary")
+
+    -- add deps
+    add_deps("merge_static_library")
+
+    -- add files
+    add_files("src/test.c") 
+
+    -- add links
+    add_links("merge_static_library")
+
+    -- add link directory
+    add_linkdirs("$(buildir)")
+
