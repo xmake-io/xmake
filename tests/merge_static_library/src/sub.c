f998beb84b38500d372265e1cd04178c79dec029
ruki <waruqi@gmail.com>
2016-07-01 16:43:34 +0800

    clean files

--- tests/merge_static_library/src/sub.c
+++
@@ -1,4 +0,0 @@
-int sub(int a, int b)
-{
-    return a - b;
-}
--- xmake/templates/c++/console/project/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main(int argc, char** argv)
-{
-    cout << "hello world!" << endl; 
-    return 0;
-}
