f06f5d8b44cf5adadf1d6474c7bc93663fda9ced
ruki <waruqi@gmail.com>
2016-09-17 21:14:56 +0800

    clear xmake config first for install.bat

---
+++ core/src/demo/xmake.lua
@@ -0,0 +1,29 @@
+-- add target
+target("demo")
+
+    -- add deps
+    add_deps("xmake")
+
+    -- make as a binary 
+    set_kind("binary")
+
+    -- add defines
+    add_defines("__tb_prefix__=\"xmake\"")
+
+    -- set the object files directory
+    set_objectdir("$(buildir)/.objs")
+
+    -- add includes directory
+    add_includedirs("$(projectdir)")
+    add_includedirs("$(projectdir)/src")
+
+    -- add links and directory
+    add_links("xmake")
+    add_linkdirs("$(buildir)")
+
+    -- add packages
+    add_packages("tbox", "luajit", "base")
+
+    -- add the common source files
+    add_files("**.c") 
+       
