32991ba6fad3bbc6c2069d9840a17347e60182e6
ruki <waruqi@gmail.com>
2016-12-19 09:38:33 +0800

    add languages

---
+++ xmake/languages/swift/xmake.lua
@@ -0,0 +1,42 @@
+--!The Make-like Build Utility based on Lua
+-- 
+-- XMake is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation; either version 2.1 of the License, or
+-- (at your option) any later version.
+-- 
+-- XMake is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+-- 
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with XMake; 
+-- If not, see <a href="http://www.gnu.org/licenses/"> http://www.gnu.org/licenses/</a>
+-- 
+-- Copyright (C) 2015 - 2016, ruki All rights reserved.
+--
+-- @author      ruki
+-- @file        xmake.lua
+--
+
+-- define language
+language("swift")
+
+    -- set source file kinds
+    set_sourcekinds(".swift")
+
+    -- on load
+    on_load(function ()
+
+        -- init flags
+        _g.scflags  = {}
+        _g.ldflags  = {}
+        _g.arflags  = {}
+        _g.shflags  = {}
+
+    end)
+
+
+
+
