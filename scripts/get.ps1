1c2132e3456d2f022024f6ee64ed19bc86d2833c
TitanSnow <sweeto@live.cn>
2017-04-22 09:33:25 +0800

    revert 440edf5

--- scripts/get.ps1
+++ scripts/get.ps1
@@ -4,8 +4,8 @@
 
 $outfile=($env:TMP,$env:TEMP,'.' -ne $null)[0]+"\$pid-xmake-installer.exe"
 $ver='v2.1.3'
-Invoke-Webrequest "https://github.com/tboox/xmake/releases/download/$ver/xmake-$ver.exe" -OutFile "$outfile" -ErrorAction Stop
-Start-Process -FilePath "$outfile" -ArgumentList '/S /D=C:\xmake' -Wait -ErrorAction Stop
+Invoke-Webrequest "https://github.com/tboox/xmake/releases/download/$ver/xmake-$ver.exe" -OutFile "$outfile"
+Start-Process -FilePath "$outfile" -ArgumentList '/S /D=C:\xmake' -Wait
 Remove-Item "$outfile"
 $env:Path+=";C:\xmake"
 [Environment]::SetEnvironmentVariable("Path",[Environment]::GetEnvironmentVariable("Path",[System.EnvironmentVariableTarget]::User)+";C:\xmake",[System.EnvironmentVariableTarget]::User)
