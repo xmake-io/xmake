4f5fabcc16216ecd3f0439a8966d261e74a978a5
ruki <waruqi@gmail.com>
2017-05-05 10:00:45 +0800

    remove build for deps tests

--- tests/projects/deps_console_c/xmake.lua
+++ tests/projects/deps_console_c/xmake.lua
@@ -2,19 +2,6 @@
 package("mbedtls")
     set_urls("git@github.com:ARMmbed/mbedtls.git")
     add_requires("git@github.com:glennrp/libpng.git@libpng >=1.6.28")
-    on_build(function (package)
-    end)
-    on_install(function (package)
-    end)
-package_end()
-
-
-package("zlib")
-    set_urls           ("http://zlib.net/zlib-$(version).tar.gz",
-                        "https://downloads.sourceforge.net/project/libpng/zlib/$(version)/zlib-$(version).tar.gz")
-    set_versions       ("1.2.10", "1.2.11")
-    set_sha256s        ("8d7e9f698ce48787b6e1c67e6bff79e487303e66077e25cb9784ac8835978017",
-                        "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1")
 package_end()
 
 -- group packages
