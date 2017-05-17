349c952442d74a4d7dd9774e9968e3b4ca9b9a45
ruki <waruqi@gmail.com>
2017-05-17 09:21:35 +0800

    move .travis dir to scripts

--- scripts/.travis/setenv_lua.sh
+++ scripts/.travis/setenv_lua.sh
@@ -1,3 +1,3 @@
 export PATH=${PATH}:$HOME/.lua:$HOME/.local/bin:${TRAVIS_BUILD_DIR}/install/luarocks/bin
-bash .travis/setup_lua.sh
+bash scripts/.travis/setup_lua.sh
 eval `$HOME/.lua/luarocks path`
