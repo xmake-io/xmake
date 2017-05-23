9e014f7b045966f612cdbbde7096ab889d01a0cc
TitanSnow <sweeto@live.cn>
2017-05-23 18:27:49 +0800

    do not test install on windows

--- tests/build.lua
+++ tests/build.lua
@@ -5,18 +5,24 @@ function main(argv)
     os.exec("xmake m -b")
     os.exec("xmake f -c")
     os.exec("xmake")
-    os.exec("sudo xmake install")
-    os.exec("sudo xmake uninstall")
+    if os.host() ~= "windows" then
+        os.exec("sudo xmake install")
+        os.exec("sudo xmake uninstall")
+    end
     os.exec("xmake p")
     os.exec("xmake c")
     os.exec("xmake f -m release")
     os.exec("xmake -r -a -v --backtrace")
-    os.exec("sudo xmake install --all -v --backtrace")
-    os.exec("sudo xmake uninstall -v --backtrace")
+    if os.host() ~= "windows" then
+        os.exec("sudo xmake install --all -v --backtrace")
+        os.exec("sudo xmake uninstall -v --backtrace")
+    end
     os.exec("xmake f --mode=debug --verbose --backtrace")
     os.exec("xmake --rebuild --all --verbose --backtrace")
-    os.exec("xmake install -o /tmp -a --verbose --backtrace")
-    os.exec("xmake uninstall --installdir=/tmp --verbose --backtrace")
+    if os.host() ~= "windows" then
+        os.exec("xmake install -o /tmp -a --verbose --backtrace")
+        os.exec("xmake uninstall --installdir=/tmp --verbose --backtrace")
+    end
     os.exec("xmake p --verbose --backtrace")
     os.exec("xmake c --verbose --backtrace")
     os.exec("xmake m -e buildtest")
