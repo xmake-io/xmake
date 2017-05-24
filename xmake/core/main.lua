a00ed183e738749f8c5068a3fe56dab54b4fb961
TitanSnow <sweeto@live.cn>
2017-05-25 07:01:48 +0800

    Revert "add xmake to path"
    This reverts commit 89e2cc76f4e4cfd45a089b4967e02756ca8f52cb.

--- xmake/core/main.lua
+++ xmake/core/main.lua
@@ -155,11 +155,6 @@ Or you can add `--root` option to allow run as root temporarily.
         end
     end
 
-    -- add xmake to path
-    if xmake._EXECUTABLE_PATH then
-        os.addenv("PATH", path.directory(xmake._EXECUTABLE_PATH))
-    end
-
     -- start profiling
     if option.get("profile") then
         profiler:start()
