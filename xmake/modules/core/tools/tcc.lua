2fe2efe5d8e2f57f6c535f5cb2d942b948ab8bb9
ruki <waruqi@gmail.com>
2017-10-17 23:34:27 +0800

    add tcc

---
+++ xmake/modules/core/tools/tcc.lua
@@ -0,0 +1,263 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+-- Copyright (C) 2015 - 2017, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        tcc.lua
+--
+
+-- imports
+import("core.base.option")
+import("core.project.config")
+import("core.project.project")
+import("core.language.language")
+import("detect.tools.find_ccache")
+
+-- init it
+function init(self)
+
+    -- init mxflags
+    _g.mxflags = {  "-fmessage-length=0"
+                ,   "-pipe"
+                ,   "-fpascal-strings"
+                ,   "-DIBOutlet=__attribute__((iboutlet))"
+                ,   "-DIBOutletCollection(ClassName)=__attribute__((iboutletcollection(ClassName)))"
+                ,   "-DIBAction=void)__attribute__((ibaction)"}
+
+    -- init shflags
+    _g.shflags = { "-shared", "-fPIC" }
+
+    -- init cxflags for the kind: shared
+    _g.shared          = {}
+    _g.shared.cxflags  = {"-fPIC"}
+
+    -- init flags map
+    _g.mapflags = 
+    {
+        -- warnings
+        ["-W1"] = "-Wall"
+    ,   ["-W2"] = "-Wall"
+    ,   ["-W3"] = "-Wall"
+
+         -- strip
+    ,   ["-s"]  = "-s"
+    ,   ["-S"]  = "-S"
+    }
+
+    -- init buildmodes
+    _g.buildmodes = 
+    {
+        ["object:sources"] = false
+    }
+end
+
+-- get the property
+function get(self, name)
+
+    -- get it
+    return _g[name]
+end
+
+-- make the strip flag
+function nf_strip(self, level)
+
+    -- the maps
+    local maps = 
+    {   
+        debug = "-S"
+    ,   all   = "-s"
+    }
+
+    -- make it
+    return maps[level]
+end
+
+-- make the symbol flag
+function nf_symbol(self, level)
+
+    -- the maps
+    local maps = 
+    {   
+        debug  = "-g"
+    ,   hidden = "-fvisibility=hidden"
+    }
+
+    -- make it
+    return maps[level] 
+end
+
+-- make the warning flag
+function nf_warning(self, level)
+
+    -- the maps
+    local maps = 
+    {   
+        none  = "-w"
+    ,   less  = "-W1"
+    ,   more  = "-W3"
+    ,   all   = "-Wall"
+    ,   error = "-Werror"
+    }
+
+    -- make it
+    return maps[level]
+end
+
+-- make the define flag
+function nf_define(self, macro)
+    return "-D" .. macro
+end
+
+-- make the undefine flag
+function nf_undefine(self, macro)
+    return "-U" .. macro
+end
+
+-- make the includedir flag
+function nf_includedir(self, dir)
+    return "-I" .. os.args(dir)
+end
+
+-- make the link flag
+function nf_link(self, lib)
+    return "-l" .. lib
+end
+
+-- make the linkdir flag
+function nf_linkdir(self, dir)
+    return "-L" .. os.args(dir)
+end
+
+-- make the link arguments list
+function linkargv(self, objectfiles, targetkind, targetfile, flags)
+    return self:program(), table.join("-o", targetfile, objectfiles, flags)
+end
+
+-- link the target file
+function link(self, objectfiles, targetkind, targetfile, flags)
+
+    -- ensure the target directory
+    os.mkdir(path.directory(targetfile))
+
+    -- link it
+    os.runv(linkargv(self, objectfiles, targetkind, targetfile, flags))
+end
+
+-- make the complie arguments list
+function _compargv1(self, sourcefile, objectfile, flags)
+
+    -- get ccache
+    local ccache = nil
+    if config.get("ccache") then
+        ccache = find_ccache()
+    end
+
+    -- make argv
+    local argv = table.join("-c", flags, "-o", objectfile, sourcefile)
+
+    -- uses cache?
+    local program = self:program()
+    if ccache then
+            
+        -- parse the filename and arguments, .e.g "xcrun -sdk macosx clang"
+        if not os.isexec(program) then
+            argv = table.join(program:split("%s"), argv)
+        else 
+            table.insert(argv, 1, program)
+        end
+        return ccache, argv
+    end
+
+    -- no cache
+    return program, argv
+end
+
+-- complie the source file
+function _compile1(self, sourcefile, objectfile, depinfo, flags)
+
+    -- ensure the object directory
+    os.mkdir(path.directory(objectfile))
+
+    -- compile it
+    try
+    {
+        function ()
+            local outdata, errdata = os.iorunv(_compargv1(self, sourcefile, objectfile, flags))
+            return (outdata or "") .. (errdata or "")
+        end,
+        catch
+        {
+            function (errors)
+
+                -- try removing the old object file for forcing to rebuild this source file
+                os.tryrm(objectfile)
+
+                -- find the start line of error
+                local lines = errors:split("\n")
+                local start = 0
+                for index, line in ipairs(lines) do
+                    if line:find("error:", 1, true) or line:find("错误：", 1, true) then
+                        start = index
+                        break
+                    end
+                end
+
+                -- get 16 lines of errors
+                if start > 0 or not option.get("verbose") then
+                    if start == 0 then start = 1 end
+                    errors = table.concat(table.slice(lines, start, start + ifelse(#lines - start > 16, 16, #lines - start)), "\n")
+                end
+
+                -- raise compiling errors
+                raise(errors)
+            end
+        },
+        finally
+        {
+            function (ok, warnings)
+
+                -- print some warnings
+                if warnings and #warnings > 0 and (option.get("verbose") or option.get("warning")) then
+                    cprint("${yellow}%s", table.concat(table.slice(warnings:split('\n'), 1, 8), '\n'))
+                end
+            end
+        }
+    }
+end
+
+-- make the complie arguments list
+function compargv(self, sourcefiles, objectfile, flags)
+
+    -- only support single source file now
+    assert(type(sourcefiles) ~= "table", "'object:sources' not support!")
+
+    -- for only single source file
+    return _compargv1(self, sourcefiles, objectfile, flags)
+end
+
+-- complie the source file
+function compile(self, sourcefiles, objectfile, depinfo, flags)
+
+    -- only support single source file now
+    assert(type(sourcefiles) ~= "table", "'object:sources' not support!")
+
+    -- for only single source file
+    _compile1(self, sourcefiles, objectfile, depinfo, flags)
+end
+
