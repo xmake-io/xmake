af7fafb3c03680052207039cae975628192d3109
ruki <waruqi@gmail.com>
2018-03-08 22:41:23 +0800

    improve check flags for nvcc

--- xmake/modules/detect/tools/nvcc/has_flags.lua
+++ xmake/modules/detect/tools/nvcc/has_flags.lua
@@ -23,5 +23,106 @@
 --
 
 -- imports
-inherit("detect.tools.gcc.has_flags")
+import("lib.detect.cache")
+import("core.language.language")
+
+-- is linker?
+function _islinker(flags, opt)
+  
+    -- the flags is "-Wl,<arg>" or "-Xlinker <arg>"?
+    local flags_str = table.concat(flags, " ")
+    if flags_str:startswith("-Wl,") or flags_str:startswith("-Xlinker ") then
+        return true
+    end
+
+    -- the tool kind is ld or sh?
+    local toolkind = opt.toolkind or ""
+    return toolkind == "ld" or toolkind == "sh" or toolkind:endswith("-ld") or toolkind:endswith("-sh")
+end
+
+-- try running 
+function _try_running(...)
+
+    local argv = {...}
+    local errors = nil
+    return try { function () os.runv(unpack(argv)); return true end, catch { function (errs) errors = (errs or ""):trim() end }}, errors
+end
+
+-- attempt to check it from the argument list
+function _check_from_arglist(flags, opt, islinker)
+
+    -- check for the builtin flags
+    local builtin_flags = {["-code"] = true, ["--gpu-code"] = true, ["-gencode"] = true, ["--generate-code"] = true, ["-arch"] = true, ["--gpu-architecture"] = true}
+    if builtin_flags[flags[1]] then
+        return true
+    end
+
+    -- check from the `--help` menu, only for linker
+    if islinker or #flags > 1 then
+        return 
+    end
+
+    -- make cache key
+    local key = "detect.tools.nvcc.has_flags"
+
+    -- make flags key
+    local flagskey = opt.program .. "_" .. (opt.programver or "")
+
+    -- load cache
+    local cacheinfo  = cache.load(key)
+
+    -- get all flags from argument list
+    local allflags = cacheinfo[flagskey]
+    if not allflags then
+
+        -- get argument list
+        allflags = {}
+        local arglist = os.iorunv(opt.program, {"--help"})
+        if arglist then
+            for arg in arglist:gmatch("%s+(%-[%-%a%d]+)%s+") do
+                allflags[arg] = true
+            end
+        end
+
+        -- save cache
+        cacheinfo[flagskey] = allflags
+        cache.save(key, cacheinfo)
+    end
+
+    -- ok?
+    return allflags[flags[1]]
+end
+
+-- try running to check flags
+function _check_try_running(flags, opt, islinker)
+
+    -- make an stub source file
+    local sourcefile = path.join(os.tmpdir(), "detect", "nvcc_has_flags.cu")
+    if not os.isfile(sourcefile) then
+        io.writefile(sourcefile, "int main(int argc, char** argv)\n{return 0;}")
+    end
+
+    -- check flags
+    return _try_running(opt.program, table.join(flags, "-o", os.nuldev(), sourcefile))
+end
+
+-- has_flags(flags)?
+-- 
+-- @param opt   the argument options, .e.g {toolname = "", program = "", programver = "", toolkind = "cu"}
+--
+-- @return      true or false
+--
+function main(flags, opt)
+
+    -- is linker?
+    local islinker = _islinker(flags, opt)
+
+    -- attempt to check it from the argument list
+    if _check_from_arglist(flags, opt, islinker) then
+        return true
+    end
+
+    -- try running to check it
+    return _check_try_running(flags, opt, islinker)
+end
 
--- xmake/templates/cuda/console/project/xmake.lua
+++ xmake/templates/cuda/console/project/xmake.lua
@@ -12,11 +12,12 @@ target("[targetname]")
 
     -- generate SASS code for each SM architecture
     for _, sm in ipairs({"30", "35", "37", "50", "52", "60", "61", "70"}) do
-        add_cuflags("-gencode arch=compute_" .. sm .. ",code=sm_" .. sm, {force = true})
-        add_ldflags("-gencode arch=compute_" .. sm .. ",code=sm_" .. sm, {force = true})
+        add_cuflags("-gencode arch=compute_" .. sm .. ",code=sm_" .. sm)
+        add_ldflags("-gencode arch=compute_" .. sm .. ",code=sm_" .. sm)
     end
 
     -- generate PTX code from the highest SM architecture to guarantee forward-compatibility
     sm = "70"
-    add_cuflags("-gencode arch=compute_" .. sm .. ",code=compute_" .. sm, {force = true})
-    add_ldflags("-gencode arch=compute_" .. sm .. ",code=compute_" .. sm, {force = true})
+    add_cuflags("-gencode arch=compute_" .. sm .. ",code=compute_" .. sm)
+    add_ldflags("-gencode arch=compute_" .. sm .. ",code=compute_" .. sm)
+
