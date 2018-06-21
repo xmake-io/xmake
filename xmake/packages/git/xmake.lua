289b920223525715c70d87c6be2501df30b50af1
ruki <waruqi@gmail.com>
2018-06-21 22:44:26 +0800

    update repo url

--- xmake/packages/git/xmake.lua
+++ xmake/packages/git/xmake.lua
@@ -3,19 +3,18 @@ package("git")
     set_kind("binary")
     set_homepage("https://git-scm.com/")
     set_description("A free and open source distributed version control system")
-    set_versions("v1.0.1")
 
     if os.host() == "windows" then
         if os.arch() == "x64" then
-            add_urls("https://gitee.com/tboox/xmake-winenv/raw/master/win64/$(version).zip")
-            add_urls("https://coding.net/u/waruqi/p/xmake-winenv/git/raw/master/win64/$(version).zip")
-            add_urls("https://github.com/tboox/xmake-win64env/archive/$(version).zip")
-            add_sha256s("v1.0.1", "95170b1d144ebb30961611fd87b1e9404bbe37d2d904adb15f3e202bb3e19c21")
+            add_urls("https://github.com/tboox/xmake-win64env/archive/$(version).zip", {alias = "github"})
+            add_urls("https://gitlab.com/tboox/xmake-win64env/-/archive/$(version)/xmake-win64env-$(version).zip", {alias = "gitlab"})
+            add_versions("github:v1.0.1", "95170b1d144ebb30961611fd87b1e9404bbe37d2d904adb15f3e202bb3e19c21")
+            add_versions("gitlab:v1.0.1", "00ec9a71d34725fb974fcdef3e6b21104a0b1681d0cd7d07ee64d4a73eed0c87")
         else
-            add_urls("https://gitee.com/tboox/xmake-winenv/raw/master/win32/$(version).zip")
-            add_urls("https://coding.net/u/waruqi/p/xmake-winenv/git/raw/master/win32/$(version).zip")
-            add_urls("https://github.com/tboox/xmake-win32env/archive/$(version).zip")
-            add_sha256s("v1.0.1", "3c46983379329a246596a2b5f0ff971b55e3eccbbfd34272e7121e13fb346db5")
+            add_urls("https://github.com/tboox/xmake-win32env/archive/$(version).zip", {alias = "github"})
+            add_urls("https://gitlab.com/tboox/xmake-win32env/-/archive/$(version)/xmake-win32env-$(version).zip", {alias = "gitlab"})
+            add_versions("github:v1.0.1", "3c46983379329a246596a2b5f0ff971b55e3eccbbfd34272e7121e13fb346db5")
+            add_versions("gitlab:v1.0.1", "c9bd70a5394b9d943607268f876b81d29c360f7f2860375cec1e4146b44023bf")
         end
     else
         add_imports("package.manager.install")
