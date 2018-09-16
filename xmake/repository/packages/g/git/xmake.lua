8d58658bcbb776bb2bbc201c7c8cfab69e46acb2
ruki <waruqi@gmail.com>
2018-09-16 22:03:57 +0800

    update winenv

--- xmake/repository/packages/g/git/xmake.lua
+++ xmake/repository/packages/g/git/xmake.lua
@@ -8,13 +8,13 @@ package("git")
         if os.arch() == "x64" then
             add_urls("https://github.com/tboox/xmake-win64env/archive/$(version).zip", {alias = "github"})
             add_urls("https://gitlab.com/tboox/xmake-win64env/-/archive/$(version)/xmake-win64env-$(version).zip", {alias = "gitlab"})
-            add_versions("github:v1.0.2", "33c7876930dd12041f253b662c2ded7a07c387341b2f91776d385ea88c90653e")
-            add_versions("gitlab:v1.0.2", "733bcf61a32579d61f924f4910f686fbfcae4d6a83ad0ba05aada596cc6dc4ce")
+            add_versions("github:v1.0.3", "df43e419a358b136cfc0ae343560684321722a39713c205a7a894edd9d15b2b5")
+            add_versions("gitlab:v1.0.3", "376807a3386f15e734f3b2f51ed5d151d434fb99b7349412a2fb9e83d5e29d5c")
         else
             add_urls("https://github.com/tboox/xmake-win32env/archive/$(version).zip", {alias = "github"})
             add_urls("https://gitlab.com/tboox/xmake-win32env/-/archive/$(version)/xmake-win32env-$(version).zip", {alias = "gitlab"})
-            add_versions("github:v1.0.2", "5d98239c2726885f6c9faafe7394c67cf7fbc4419a5596bcf85ca6256af50550")
-            add_versions("gitlab:v1.0.2", "1dd80a91602e857df2f6aa103adeb863c171955751ba04e53b7da9cc1ac64982")
+            add_versions("github:v1.0.3", "2a3b71baae67f3ebb057748d1261970d18d80be52e88427a4719d2185594fc21")
+            add_versions("gitlab:v1.0.3", "2a627f78349702a85b710399c14f5dc0acfdb5ad840765e3e2637d554a373807")
         end
     end
 
