01c4041b1d62ce8439bd14335857c5f154d86ae0
ruki <waruqi@gmail.com>
2018-10-20 10:49:03 +0800

    add llvm-rc

---
+++ xmake/modules/core/tools/llvm_rc.lua
@@ -0,0 +1,118 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+-- Copyright (C) 2015 - 2018, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        llvm_rc.lua
+--
+
+-- imports
+import("core.base.option")
+import("core.project.project")
+
+-- init it
+function init(self)
+
+    -- init buildmodes
+    _g.buildmodes = 
+    {
+        ["object:sources"] = false
+    }
+end
+
+-- get the property
+function get(self, name)
+    return _g[name]
+end
+
+-- make the define flag
+function nf_define(self, macro)
+    return "/D " .. macro
+end
+
+-- make the undefine flag
+function nf_undefine(self, macro)
+    return "/U " .. macro
+end
+
+-- make the includedir flag
+function nf_includedir(self, dir)
+    return "/I " .. os.args(dir)
+end
+
+-- make the complie arguments list
+function _compargv1(self, sourcefile, objectfile, flags)
+    return self:program(), table.join(flags, "/FO", objectfile, sourcefile)
+end
+
+-- complie the source file
+function _compile1(self, sourcefile, objectfile, dependinfo, flags)
+
+    -- ensure the object directory
+    os.mkdir(path.directory(objectfile))
+
+    -- compile it
+    try
+    {
+        function ()
+            local outdata, errdata = os.iorunv(_compargv1(self, sourcefile, objectfile, flags))
+            return (outdata or "") .. (errdata or "")
+        end,
+        catch
+        {
+            function (errors)
+
+                -- compiling errors
+                os.raise(errors)
+            end
+        },
+        finally
+        {
+            function (ok, warnings)
+
+                -- print some warnings
+                if warnings and #warnings > 0 and option.get("verbose") then
+                    cprint("${yellow}%s", table.concat(table.slice(warnings:split('\n'), 1, 8), '\n'))
+                end
+            end
+        }
+    }
+end
+
+-- make the complie arguments list
+function compargv(self, sourcefiles, objectfile, flags)
+
+    -- only support single source file now
+    assert(type(sourcefiles) ~= "table", "'object:sources' not support!")
+
+    -- for only single source file
+    return _compargv1(self, sourcefiles, objectfile, flags)
+end
+
+-- complie the source file
+function compile(self, sourcefiles, objectfile, dependinfo, flags)
+
+    -- only support single source file now
+    assert(type(sourcefiles) ~= "table", "'object:sources' not support!")
+
+    -- for only single source file
+    _compile1(self, sourcefiles, objectfile, dependinfo, flags)
+end
+
+
---
+++ xmake/modules/detect/tools/find_llvm_rc.lua
@@ -0,0 +1,51 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015 - 2018, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        find_llvm_rc.lua
+--
+
+-- imports
+import("lib.detect.find_program")
+import("lib.detect.find_programver")
+
+-- find llvm-rc
+--
+-- @param opt   the argument options, .e.g {version = true, program = "c:\xxx\llvm_rc.exe"}
+--
+-- @return      program, version
+--
+-- @code
+--
+-- local llvm_rc = find_llvm_rc()
+-- local llvm_rc, version = find_llvm_rc({version = true})
+-- local llvm_rc, version = find_llvm_rc({version = true, program = "c:\xxx\llvm_rc.exe"})
+--
+-- @endcode
+--
+function main(opt)
+
+    -- init options
+    opt = opt or {}
+
+    -- find program
+    opt.check = opt.check or "/?"
+    return find_program(opt.program or "llvm-rc", opt)
+end
