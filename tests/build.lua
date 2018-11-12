bcfe7b269dc8ccca180099c8ce62f333a405ed11
ruki <waruqi@gmail.com>
2018-11-12 23:53:25 +0800

    remove some --backtrace

--- tests/build.lua
+++ tests/build.lua
@@ -10,21 +10,21 @@ function main(argv)
     -- generic?
     os.exec("xmake f -c")
     os.exec("xmake")
-    os.exec("xmake p --verbose --backtrace")
+    os.exec("xmake p --verbose -D")
     if os.host() ~= "windows" then
-        os.exec("xmake install -o /tmp -a --verbose --backtrace")
-        os.exec("xmake uninstall --installdir=/tmp --verbose --backtrace")
+        os.exec("xmake install -o /tmp -a --verbose -D")
+        os.exec("xmake uninstall --installdir=/tmp --verbose -D")
     end
-    os.exec("xmake c --verbose --backtrace")
-    os.exec("xmake f --mode=debug --verbose --backtrace")
+    os.exec("xmake c --verbose -D")
+    os.exec("xmake f --mode=debug --verbose -D")
     os.exec("xmake m -b")
-    os.exec("xmake -r -a -v --backtrace")
+    os.exec("xmake -r -a -v -D")
     os.exec("xmake m -e buildtest")
     os.exec("xmake m -l")
     os.exec("xmake m buildtest")
     if sudo.has() then
-        sudo.exec("xmake install --all -v --backtrace")
-        sudo.exec("xmake uninstall -v --backtrace")
+        sudo.exec("xmake install --all -v -D")
+        sudo.exec("xmake uninstall -v -D")
     end
     os.exec("xmake m -d buildtest")
 
