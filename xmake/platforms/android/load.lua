a4b58d86da4cf3df4aeae5f390eaa1b69b8fffe8
ruki <waruqi@gmail.com>
2018-11-14 23:21:09 +0800

    remove android support links

--- xmake/platforms/android/load.lua
+++ xmake/platforms/android/load.lua
@@ -132,8 +132,8 @@ function main(platform)
                 platform:add("ldflags", format("-L%s/libs/%s", cxxstl_sdkdir, toolchains_archs[arch]))
                 platform:add("shflags", format("-L%s/libs/%s", cxxstl_sdkdir, toolchains_archs[arch]))
                 if cxxstl_sdkdir:find("llvm-libc++", 1, true) then
-                    platform:add("ldflags", "-lc++_static", "-lc++abi", "-landroid_support")
-                    platform:add("shflags", "-lc++_static", "-lc++abi", "-landroid_support")
+                    platform:add("ldflags", "-lc++_static", "-lc++abi")
+                    platform:add("shflags", "-lc++_static", "-lc++abi")
                 else
                     platform:add("ldflags", "-lgnustl_static")
                     platform:add("shflags", "-lgnustl_static")
