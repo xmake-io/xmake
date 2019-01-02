a5edf8e991f7fcab4981657c4f5aa9821411b544
ruki <waruqi@gmail.com>
2019-01-03 00:32:01 +0800

    add dark and emoji styles

---
+++ xmake/themes/dark/xmake.lua
@@ -0,0 +1,46 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+-- Copyright (C) 2015 - 2019, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        xmake.lua
+--
+
+-- define theme
+theme("dark")
+
+    -- set color of the error info
+    set_color("error", "bright red")
+    set_color("error.verbose", "dim red")
+
+    -- set color of the warning info
+    set_color("warning", "bright cyan")
+    set_color("warning.verbose", "dim cyan")
+
+    -- the color of the building progress
+    set_color("build.progress", "green")
+
+    -- the color of the building object file
+    set_color("build.object", "clear")
+    set_color("build.object.verbose", "dim")
+
+    -- the color of the building target file
+    set_color("build.target", "magenta")
+    set_color("build.target.verbose", "dim magenta")
+
