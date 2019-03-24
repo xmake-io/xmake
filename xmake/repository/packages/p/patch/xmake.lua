8aa3277936c2d653c814e1ce3740a33d102dbc75
ruki <waruqi@gmail.com>
2019-03-24 23:47:32 +0800

    improve patch package

--- xmake/repository/packages/p/patch/xmake.lua
+++ xmake/repository/packages/p/patch/xmake.lua
@@ -5,17 +5,24 @@ package("patch")
     set_description("GNU patch, which applies diff files to original files.")
 
     if is_host("windows") then
-        add_urls("https://sourceforge.net/projects/gnuwin32/files/patch/$(version)/patch-$(version)-bin.zip/download")
+        add_urls("https://gitlab.com/xmake-mirror/patch/uploads/2ff76c0c6a35a4a6af98bb7052544c38/patch-$(version)-bin.zip")
+        add_urls("https://github.com/xmake-mirror/patch/releases/download/v2.5.9/patch-$(version)-bin.zip")
         add_versions("2.5.9-7", "fabd6517e7bd88e067db9bf630d69bb3a38a08e044fa73d13a704ab5f8dd110b")
     else
---        add_urls("https://ftp.gnu.org/gnu/patch/patch-$(version).tar.bz2", {alias = "gnuftp"})
---        add_urls("https://github.com/xmake-mirror/patch/archive/v$(version).tar.gz", {alias = "github"})
+        add_urls("https://ftp.gnu.org/gnu/patch/patch-$(version).tar.bz2", {alias = "gnuftp"})
+        add_urls("https://github.com/xmake-mirror/patch/archive/v$(version).tar.gz", {alias = "github"})
         add_urls("https://github.com/xmake-mirror/patch.git",
                  "https://gitlab.com/xmake-mirror/patch.git",
                  "https://gitee.com/xmake-mirror/patch.git")
         add_versions("gnuftp:2.7.6", "3d1d001210d76c9f754c12824aa69f25de7cb27bb6765df63455b77601a0dcc9")
         add_versions("github:2.7.6", "33d5a86bad9813b27dbbe890123d0b88fbcc74d5d997aeadde60c670a2bd0eb9")
+        add_versions("2.7.6", "v2.7.6")
     end
+
+    --[[
+    if is_plat("macosx", "linux") then
+        add_deps("autoconf", "automake")
+    end]]
 
     on_install("windows", function (package)
         os.cp("bin/*", package:installdir("bin"))
