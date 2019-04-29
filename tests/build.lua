7dce9f35887396f9b160595f4ffc1ef6d5558045
ruki <waruqi@gmail.com>
2019-04-30 00:28:29 +0800

    remove test verbose info

--- tests/build.lua
+++ tests/build.lua
@@ -10,21 +10,21 @@ function main(argv)
     -- generic?
     os.exec("xmake f -c")
     os.exec("xmake")
-    os.exec("xmake p --verbose -D")
+    os.exec("xmake p -D")
     if os.host() ~= "windows" then
-        os.exec("xmake install -o /tmp -a --verbose -D")
-        os.exec("xmake uninstall --installdir=/tmp --verbose -D")
+        os.exec("xmake install -o /tmp -a -D")
+        os.exec("xmake uninstall --installdir=/tmp -D")
     end
-    os.exec("xmake c --verbose -D")
-    os.exec("xmake f --mode=debug --verbose -D")
+    os.exec("xmake c -D")
+    os.exec("xmake f --mode=debug -D")
     os.exec("xmake m -b")
-    os.exec("xmake -r -a -v -D")
+    os.exec("xmake -r -a -D")
     os.exec("xmake m -e buildtest")
     os.exec("xmake m -l")
     os.exec("xmake m buildtest")
     if sudo.has() then
-        sudo.exec("xmake install --all -v -D")
-        sudo.exec("xmake uninstall -v -D")
+        sudo.exec("xmake install --all -D")
+        sudo.exec("xmake uninstall -D")
     end
     os.exec("xmake m -d buildtest")
 
