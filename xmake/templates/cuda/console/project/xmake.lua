52d8932c29ccf051e2cd400493c38ff1c90f4ccb
OpportunityLiu <opportunity@live.in>
2019-06-02 17:35:26 +0800

    rename add_cugencodes

--- xmake/templates/cuda/console/project/xmake.lua
+++ xmake/templates/cuda/console/project/xmake.lua
@@ -2,8 +2,8 @@
 -- add modes: debug and release
 add_rules("mode.debug", "mode.release")
 
--- add helper function add_cugencode
-includes('add_cugencode.lua')
+-- add helper function add_cugencodes
+includes('add_cugencodes.lua')
 
 -- define target
 target("[targetname]")
@@ -15,13 +15,13 @@ target("[targetname]")
     add_files("src/*.cu")
 
     -- generate SASS code for SM architecture of current host
-    add_cugencode("native")
+    add_cugencodes("native")
 
     -- generate PTX code for the virtual architecture to guarantee compatibility
-    add_cugencode("compute_30")
+    add_cugencodes("compute_30")
 
     -- -- generate SASS code for each SM architecture
-    -- add_cugencode("sm_30", "sm_35", "sm_37", "sm_50", "sm_52", "sm_60", "sm_61", "sm_70", "sm_75")
+    -- add_cugencodes("sm_30", "sm_35", "sm_37", "sm_50", "sm_52", "sm_60", "sm_61", "sm_70", "sm_75")
 
     -- -- generate PTX code from the highest SM architecture to guarantee forward-compatibility
-    -- add_cugencode("compute_75")
+    -- add_cugencodes("compute_75")
