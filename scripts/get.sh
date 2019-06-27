9cabfe72de6ac6c3c71e14e4fa9c061240a44a9a
ruki <waruqi@gmail.com>
2019-06-27 23:30:37 +0800

    fix get.sh

--- scripts/get.sh
+++ scripts/get.sh
@@ -111,6 +111,7 @@ then
         git clone --depth=1 -b "$branch" "https://github.com/$mirror/xmake.git" --recursive $projectdir || my_exit "$(echo -e 'Clone Fail\nCheck your network or branch name')"
     fi
 else
+    git submodule update --init --recursive
     projectdir=`pwd`
 fi
 if [ 'x__install_only__' != "x$2" ]
