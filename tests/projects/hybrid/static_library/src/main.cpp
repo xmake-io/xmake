2abe0ac3e71743147c433e0b901eaf3d244ef099
ruki <waruqi@gmail.com>
2019-09-04 22:29:54 +0800

    fix ci errors

--- tests/projects/hybrid/static_library/src/main.cpp
+++ tests/projects/hybrid/static_library/src/main.cpp
@@ -4,7 +4,9 @@ int main(int argc, char** argv)
 {
     test1();
     test2();
+#ifdef MACOSX
     test3();
     test4();
+#endif
     return 0;
 }
--- tests/projects/hybrid/static_library/xmake.lua
+++ tests/projects/hybrid/static_library/xmake.lua
@@ -3,12 +3,17 @@ add_rules("mode.release", "mode.debug")
 target("test")
     set_kind("static")
     add_files("src/*.c", "src/*.cpp")
-    add_files("src/*.m", "src/*.mm") 
+    if is_plat("macosx") then
+        add_files("src/*.m", "src/*.mm") 
+    end
 
 target("demo")
     set_kind("binary")
     add_deps("test")
     add_files("src/main.cpp") 
+    if is_plat("macosx") then
+        add_defines("MACOSX")
+    end
 
 target("demo2")
     set_kind("binary")
