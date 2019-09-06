5c0286dd6ec8c3f8bf7e6c4e7cecd9d5903948e8
ruki <waruqi@gmail.com>
2019-09-06 22:48:20 +0800

    add impl_unit tests

---
+++ tests/projects/c++/modules/impl_unit/src/main.cpp
@@ -0,0 +1,8 @@
+import hello;
+
+int main() {
+    hello::say_hello();
+    hello::say_xz();
+    // hello::say_hi();
+    return 0;
+}
---
+++ tests/projects/c++/modules/impl_unit/xmake.lua
@@ -0,0 +1,5 @@
+target("impl_unit")
+    set_kind("binary")
+    add_files("src/*.cpp", "src/*.mpp") 
+
+
