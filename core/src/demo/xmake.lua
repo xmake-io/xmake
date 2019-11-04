f00316a42943a9a75303658f8d646d35f9e8d1b9
ruki <waruqi@gmail.com>
2019-11-04 22:35:12 +0800

    fix core xmake.lua again

--- core/src/demo/xmake.lua
+++ core/src/demo/xmake.lua
@@ -47,6 +47,6 @@ target("demo")
 
     -- copy target to the build directory
     after_build(function (target)
-        os.cp(target:targetfile(), path.join(get_config("buildir"), path.filename(target:targetfile())))
+        os.cp(target:targetfile(), "$(buildir)/xmake" .. (is_plat("windows") and ".exe" or ""))
     end)
 
