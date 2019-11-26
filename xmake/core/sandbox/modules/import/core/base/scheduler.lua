4e7e61681429e7ea13501cae574f88bf90c5900a
ruki <waruqi@gmail.com>
2019-11-27 00:52:36 +0800

    add scheduler stub

---
+++ xmake/core/sandbox/modules/import/core/base/scheduler.lua
@@ -0,0 +1,37 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+-- Copyright (C) 2015 - 2019, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        scheduler.lua
+--
+
+-- define module
+local sandbox_core_base_scheduler = sandbox_core_base_scheduler or {}
+
+-- load modules
+local scheduler = require("base/scheduler")
+local raise     = require("sandbox/modules/raise")
+
+-- run loop
+function sandbox_core_base_scheduler.runloop(main)
+    local ok, errors = scheduler:runloop(main)
+    if not ok then
+        raise(errors)
+    end
+end
+
+-- return module
+return sandbox_core_base_scheduler
