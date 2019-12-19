3e67c61cd338e9ad214590d24e6ad506ce5b57f6
ruki <waruqi@gmail.com>
2019-12-19 22:28:54 +0800

    add sched_udp tests

---
+++ tests/modules/socket/sched_udp/echo_client.lua
@@ -0,0 +1,19 @@
+import("core.base.socket")
+import("core.base.scheduler")
+
+function _session(addr, port, data)
+    local sock = socket.udp()
+    local send = sock:sendto(data or "hello xmake!", addr, port, {block = true})
+    print("%s: send to %s:%d %d bytes!", sock, addr, port, send)
+    local recv, data, peer_addr, peer_port = sock:recvfrom(8112, {block = true})
+    if recv > 0 then
+        print("%s: recv %d bytes from %s:%d", sock, recv, peer_addr, peer_port)
+        data:dump()
+    end
+    sock:close()
+end
+
+function main(data)
+    scheduler.co_start(_session, "127.0.0.1", 9001, data)
+    scheduler.runloop()
+end
---
+++ tests/modules/socket/sched_udp/echo_server.lua
@@ -0,0 +1,22 @@
+import("core.base.socket")
+import("core.base.scheduler")
+
+function _listen(addr, port)
+    local sock = socket.udp()
+    sock:bind(addr, port)
+    while true do 
+        print("%s: recv in %s:%d ..", sock, addr, port)
+        local recv, data, peer_addr, peer_port = sock:recvfrom(8192, {block = true})
+        print("%s: recv %d bytes from: %s:%d", sock, recv, peer_addr, peer_port)
+        if data then
+            data:dump()
+            sock:sendto(data, peer_addr, peer_port)
+        end
+    end
+    sock:close()
+end
+
+function main()
+    scheduler.co_start(_listen, "127.0.0.1", 9001)
+    scheduler.runloop()
+end
