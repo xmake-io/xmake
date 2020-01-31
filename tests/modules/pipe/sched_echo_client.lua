7fc59d939bd2cf426c9fb823a5a2616b0dd3b508
ruki <waruqi@gmail.com>
2020-02-01 00:40:43 +0800

    update tbox and tests

---
+++ tests/modules/pipe/sched_echo_client.lua
@@ -0,0 +1,24 @@
+import("core.base.pipe")
+import("core.base.scheduler")
+
+function _session(id)
+    local pipefile = pipe.open("test" .. id, 'w')
+    local count = 0
+    while count < 10000 do
+        local write = pipefile:write("hello world..", {block = true})
+        if write <= 0 then
+            break
+        end
+        count = count + 1
+    end
+    print("%s/%d: write ok, count: %d!", pipefile, id, count)
+    pipefile:close()
+end
+
+function main(count)
+    count = count and tonumber(count) or 1
+    for i = 1, count do
+        scheduler.co_start(_session, i)
+    end
+    scheduler.runloop()
+end
---
+++ tests/modules/pipe/sched_echo_server.lua
@@ -0,0 +1,32 @@
+import("core.base.pipe")
+import("core.base.scheduler")
+
+function _session(id)
+
+    local pipefile = pipe.open("test" .. id, 'r')
+    if pipefile:connect() > 0 then
+        print("%s/%d: connected", pipefile, id) 
+        local count = 0
+        local result = nil
+        while count < 10000 do
+            local read, data = pipefile:read(13, {block = true})
+            if read > 0 then
+                result = data
+                count = count + 1
+            else
+                break
+            end
+        end
+        print("%s/%d: read: %d, count: %d", pipefile, id, result and result:size() or 0, count)
+        result:dump()
+    end
+    pipefile:close()
+end
+
+function main(count)
+    count = count and tonumber(count) or 1
+    for i = 1, count do
+        scheduler.co_start(_session, i)
+    end
+    scheduler.runloop()
+end
