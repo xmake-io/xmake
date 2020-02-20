6cb8b7ca86ed9f9eb86cc0a5c48c752331efb420
ruki <waruqi@gmail.com>
2020-02-20 23:49:42 +0800

    enable color8 for gcc/clang diagnostics

--- xmake/modules/core/tools/gcc.lua
+++ xmake/modules/core/tools/gcc.lua
@@ -360,7 +360,7 @@ end
 function _has_color_diagnostics(self)
     local colors_diagnostics = _g._HAS_COLOR_DIAGNOSTICS
     if colors_diagnostics == nil then
-        if io.isatty() and colors.color256() then
+        if io.isatty() and (colors.color8() or colors.color256()) then
             local theme = colors.theme()
             if theme and theme:name() ~= "plain" then
                 colors_diagnostics = self:has_flags("-fcolor-diagnostics", "cxflags") or false
