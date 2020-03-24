ff2e3546b8622c64889418e077ee1a1038b1d059
ruki <waruqi@gmail.com>
2020-03-24 23:58:52 +0800

    add tests for os.args/argv

--- tests/modules/os/test.lua
+++ tests/modules/os/test.lua
@@ -60,3 +60,19 @@ function test_setenv(t)
     -- assert mclock
     t:require(os.mclock() >= tm)
 end
+
+function test_argv(t)
+    t:are_equal(os.argv("aa bb cc"), {"aa", "bb", "cc"})
+    t:are_equal(os.argv("aa --bb=bbb -c"), {"aa", "--bb=bbb", "-c"})
+    t:are_equal(os.argv("\"aa bb cc\" dd"), {"aa bb cc", "dd"})
+    t:are_equal(os.argv("aa\\bb/cc dd"), {"aa\\bb/cc", "dd"})
+    t:are_equal(os.argv("\"aa\\\\bb/cc dd\" ee"), {"aa\\bb/cc dd", "ee"})
+end
+
+function test_args(t)
+    t:are_equal(os.args({"aa", "bb", "cc"}), "aa bb cc")
+    t:are_equal(os.args({"aa", "--bb=bbb", "-c"}), "aa --bb=bbb -c")
+    t:are_equal(os.args({"aa bb cc", "dd"}), "\"aa bb cc\" dd")
+--    t:are_equal(os.args({"aa\\bb/cc", "dd"}), "aa\\bb/cc dd")
+    t:are_equal(os.args({"aa\\bb/cc dd", "ee"}), "\"aa\\\\bb/cc dd\" ee")
+end
