92fce96e6c49ac88824f9266a1a4b5023167b803
ruki <waruqi@gmail.com>
2020-03-26 22:40:34 +0800

    improve find_package for brew/cmake

--- xmake/modules/package/manager/brew/find_package.lua
+++ xmake/modules/package/manager/brew/find_package.lua
@@ -21,8 +21,31 @@
 -- imports
 import("lib.detect.find_tool")
 import("lib.detect.find_file")
+import("lib.detect.find_path")
 import("lib.detect.pkg_config")
+import("core.project.target")
 import("package.manager.find_package")
+
+-- get the root directory of the brew packages
+function _brew_pkg_rootdir()
+    local brew_pkg_rootdir = _g.brew_pkg_rootdir 
+    if brew_pkg_rootdir == nil then
+        local brew = find_tool("brew")
+        if brew then
+            brew_pkg_rootdir = try 
+            { 
+                function () 
+                    return os.iorunv(brew.program, {"--prefix"}) 
+                end 
+            } or "/usr/local"
+        end
+        if brew_pkg_rootdir then
+            brew_pkg_rootdir = brew_pkg_rootdir:trim()
+        end
+        _g.brew_pkg_rootdir = brew_pkg_rootdir or false
+    end
+    return brew_pkg_rootdir or nil
+end
 
 -- find package from the brew package manager
 --
@@ -31,23 +54,20 @@ import("package.manager.find_package")
 --
 function main(name, opt)
 
-    -- find brew
-    local brew = find_tool("brew")
-    if not brew then
-        return 
+    -- find the prefix directory of brew 
+    local brew_pkg_rootdir = _brew_pkg_rootdir()
+    if not brew_pkg_rootdir then
+        return
     end
+    brew_pkg_rootdir = path.join(brew_pkg_rootdir, opt.plat == "macosx" and "Cellar" or "opt")
 
     -- parse name, e.g. pcre/libpcre16
     local nameinfo = name:split('/')
     local pcname   = nameinfo[2] or nameinfo[1]
 
-    -- find the prefix directory of brew 
-    local brew_pkg_root = try { function () return os.iorunv(brew.program, {"--prefix"}) end } or "/usr/local"
-    brew_pkg_root = path.join(brew_pkg_root:trim(), opt.plat == "macosx" and "Cellar" or "opt")
-
     -- find package from pkg-config/*.pc
     local result = nil
-    local pcfile = find_file(pcname .. ".pc", path.join(brew_pkg_root, nameinfo[1], "*/lib/pkgconfig"))
+    local pcfile = find_file(pcname .. ".pc", path.join(brew_pkg_rootdir, nameinfo[1], "*/lib/pkgconfig"))
     if pcfile then
         opt.configdirs = path.directory(pcfile)
         result = find_package("pkg_config::" .. pcname, opt)
@@ -64,10 +84,20 @@ function main(name, opt)
 
     -- find package from xxx/lib, xxx/include
     if not result then
-        local libfile = find_file("*.a", path.join(brew_pkg_root, nameinfo[1], "*", "lib"))
-        if libfile then
-            opt.linkdirs    = path.directory(libfile)
-            opt.includedirs = path.join(path.directory(opt.linkdirs), "include")
+        local pkgdir = find_path("lib", path.join(brew_pkg_rootdir, nameinfo[1], "*"))
+        if pkgdir then
+            local links = {}
+            for _, libfile in ipairs(os.files(path.join(pkgdir, "lib", "*.a"))) do
+                table.insert(links, target.linkname(path.filename(libfile)))
+            end
+            for _, libfile in ipairs(os.files(path.join(pkgdir, "lib", opt.plat == "macosx" and "*.dylib" or "*.so"))) do
+                if not os.islink(libfile) then
+                    table.insert(links, target.linkname(path.filename(libfile)))
+                end
+            end
+            opt.links       = links
+            opt.linkdirs    = path.join(pkgdir, "lib")
+            opt.includedirs = path.join(pkgdir, "include")
             result = find_package("system::" .. name, opt)
         end
     end
