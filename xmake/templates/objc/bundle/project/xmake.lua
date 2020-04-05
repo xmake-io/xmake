c063689761dbc18e9222ac076c5e1ecda22897dd
ruki <waruqi@gmail.com>
2020-04-05 14:47:59 +0800

    add objc/bundle template

---
+++ xmake/templates/objc/bundle/project/xmake.lua
@@ -0,0 +1,10 @@
+add_rules("mode.debug", "mode.release")
+
+target("${TARGETNAME}")
+    add_rules("xcode.bundle")
+    add_files("src/*.m")
+    add_headerfiles("src/*.h")
+    add_installfiles("src/Info.plist")
+    --set_values("xcode.codesign_identity", "Apple Development: xxx@gmail.com (T3NA4MRVPU)")
+
+${FAQ}
