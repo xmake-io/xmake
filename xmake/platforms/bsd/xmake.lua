8ed7581a8c3049a6f7d1efc737dfb6ebfe390787
ruki <waruqi@gmail.com>
2020-05-09 09:14:19 +0800

    add bsd platform

---
+++ xmake/platforms/bsd/xmake.lua
@@ -0,0 +1,68 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+-- Copyright (C) 2015-2020, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        xmake.lua
+--
+
+-- define platform
+platform("bsd")
+
+    -- set os
+    set_os("bsd")
+
+    -- set hosts
+    set_hosts("bsd")
+
+    -- set archs
+    set_archs("i386", "x86_64")
+
+    -- set formats
+    set_formats {static = "lib$(name).a", object = "$(name).o", shared = "lib$(name).so", symbol = "$(name).sym"}
+
+    -- set install directory
+    set_installdir("/usr/local")
+
+    -- on check project configuration
+    on_config_check("config")
+
+    -- on check global configuration
+    on_global_check("global")
+
+    -- on load
+    on_load("load")
+
+    -- set menu
+    set_menu {
+                config = 
+                {   
+                    {category = "Cuda SDK Configuration"                                            }
+                ,   {nil, "cuda",           "kv", "auto",       "The Cuda SDK Directory"            }
+                ,   {category = "Qt SDK Configuration"                                              }
+                ,   {nil, "qt",             "kv", "auto",       "The Qt SDK Directory"              }
+                ,   {nil, "qt_sdkver",      "kv", "auto",       "The Qt SDK Version"                }
+                }
+
+            ,   global = 
+                {   
+                    {category = "Cuda SDK Configuration"                                            }
+                ,   {nil, "cuda",           "kv", "auto",       "The Cuda SDK Directory"            }
+                ,   {category = "Qt SDK Configuration"                                              }
+                ,   {nil, "qt",             "kv", "auto",       "The Qt SDK Directory"              }
+                }
+            }
+
+
