2b50cfa31f9d4c979b868fcadbf1112597dad8c1
ruki <waruqi@gmail.com>
2020-05-19 22:49:53 +0800

    add yasm toolchain

---
+++ xmake/toolchains/yasm/xmake.lua
@@ -0,0 +1,36 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+-- Copyright (C) 2015-2020, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        xmake.lua
+--
+
+-- define toolchain
+toolchain("yasm")
+        
+    -- set toolsets
+    set_toolsets("as", "yasm")
+
+    -- on load
+    on_load(function (toolchain)
+        if is_plat("macosx") then
+            toolchain:add("yasm.asflags", "-f", is_arch("x86_64") and "macho64" or "macho32")
+        elseif is_plat("linux") then
+            toolchain:add("yasm.asflags", "-f", is_arch("x86_64") and "elf64" or "elf32")
+        elseif is_plat("windows") then
+            toolchain:add("yasm.asflags", "-f", is_arch("x64") and "win64" or "win32")
+        end
+    end)
