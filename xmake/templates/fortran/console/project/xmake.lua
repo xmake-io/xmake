8874de88d7cc0003c9f83042a39cc7f81a8e88ac
ruki <waruqi@gmail.com>
2020-07-05 10:46:01 +0800

    add fortran templates

---
+++ xmake/templates/fortran/console/project/xmake.lua
@@ -0,0 +1,6 @@
+add_rules("mode.debug", "mode.release")
+
+target("${TARGETNAME}")
+    set_kind("binary")
+    add_files("src/*.f90")
+
---
+++ xmake/templates/fortran/static/project/src/test.f90
@@ -0,0 +1,8 @@
+module test
+    implicit none (type, external)
+    
+contains
+    subroutine print_hello()
+        print *, "Hello World!"
+    end subroutine print_hello
+end module test
