c93f12ad36c4b19683fb9aefae2dd6bae8db02b7
ruki <waruqi@gmail.com>
2020-07-11 12:13:56 +0800

    add qt tips

--- xmake/rules/qt/env/xmake.lua
+++ xmake/rules/qt/env/xmake.lua
@@ -37,6 +37,8 @@ rule("qt.env")
             target:add("runenvs", "PATH", qt.bindir)
             target:set("runenv", "QML2_IMPORT_PATH", path.join(qt.sdkdir, "qml"))
             target:set("runenv", "QML_IMPORT_TRACE", "1")
+        elseif is_plat("msys", "cygwin") then
+            raise("please run `xmake f -p mingw --mingw=/mingw64` to support Qt/Mingw64 on Msys!")
         end
     end)
 
