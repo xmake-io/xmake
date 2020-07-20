2ef5c7639b752c71b1190cd3f2204fe58ba0af59
ruki <waruqi@gmail.com>
2020-07-21 00:35:53 +0800

    add json test

---
+++ tests/modules/json/test.lua
@@ -0,0 +1,19 @@
+import("core.base.json")
+
+-- test decode
+function test_json_decode(t)
+    t:are_equal(json.decode('{}'), {})
+    t:are_equal(json.decode('[]'), {})
+    t:are_equal(json.decode('{"a":1, "b":"2", "c":true, "d":false, "e":null, "f":[]}'), {a = 1, b = "2", c = true, d = false, e = json.null, f = {}})
+    t:are_equal(json.decode('{"a":[], "b":[1,2], "c":{"a":1}}'), {a = {}, b = {1,2}, c = {a = 1}})
+    t:are_equal(json.decode('[1,"2"]'), {1, "2"})
+    t:are_equal(json.decode('[1,"2", {"a":1, "b":true}]'), {1, "2", {a = 1, b = true}})
+end
+
+-- test encode
+function test_json_encode(t)
+    t:are_equal(json.encode({}), '{}')
+    t:are_equal(json.encode({}), '{}')
+    t:are_equal(json.encode({json.null, 1, "2", false, true}), '[null,1,"2",false,true]')
+    t:are_equal(json.encode({1, "2", {a = 1, b = true}}), '[1,"2",{"a":1,"b":true}]')
+end
