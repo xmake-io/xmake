cfe9172c46fa3a5bb5accc8ea1ea18e4391ab491
BarrOff <58253563+BarrOff@users.noreply.github.com>
2020-07-30 03:11:13 +0200

    fix configuration for gdc compiler

--- xmake/modules/core/tools/gdc.lua
+++ xmake/modules/core/tools/gdc.lua
@@ -11,15 +11,162 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
--- 
+--
 -- Copyright (C) 2015-2020, TBOOX Open Source Group.
 --
--- @author      ruki
+-- @author      ruki, BarrOff
 -- @file        gdc.lua
 --
 
--- inherit dmd
-inherit("dmd")
+-- imports
+import("core.base.option")
+import("core.project.config")
+import("core.project.project")
 
+-- init it
+function init(self)
+    
+    -- init arflags
+    self:set("dcarflags", "-lib")
 
+    -- init shflags
+    self:set("dcshflags", "-shared", "-fPIC")
+
+    -- init dcflags for the kind: shared
+    self:set("shared.dcflags", "-fPIC")
+end
+
+-- make the optimize flag
+function nf_optimize(self, level)
+    local maps =
+    {
+        fast        = "-O"
+    ,   faster      = "-O -frelease"
+    ,   fastest     = "-O -frelease -fbounds-check=off"
+    ,   smallest    = "-O -frelease -fbounds-check=off"
+    ,   aggressive  = "-O -frelease -fbounds-check=off"
+    }
+    return maps[level]
+end
+
+-- make the strip flag
+function nf_strip(self, level)
+    local maps = 
+    {   
+        debug       = "-S"
+    ,   all         = "-s"
+    }
+    return maps[level] 
+end
+
+-- make the symbol flag
+function nf_symbol(self, level)
+    local maps = 
+    {   
+        debug = "-g -debug"
+    }
+    return maps[level] 
+end
+
+-- make the warning flag
+function nf_warning(self, level)
+    local maps =
+    {
+        none        = "-w"
+    ,   less        = "-Wall"
+    ,   more        = "-Wall"
+    ,   all         = "-Wall"
+    ,   everything  = "-Wall -Wdeprecated -Wextra"
+    ,   error       = "-Werror"
+    }
+    return maps[level]
+end
+
+-- make the vector extension flag
+function nf_vectorext(self, extension)
+    local maps =
+    {
+        mmx   = "-mmmx"
+    ,   sse   = "-msse"
+    ,   sse2  = "-msse2"
+    ,   sse3  = "-msse3"
+    ,   ssse3 = "-mssse3"
+    ,   avx   = "-mavx"
+    ,   avx2  = "-mavx2"
+    }
+    return maps[extension]
+end
+
+-- make the includedir flag
+function nf_includedir(self, dir)
+    return "-I" .. os.args(dir)
+end
+
+-- make the link flag
+function nf_link(self, lib)
+    return "-l" .. lib
+end
+
+-- make the syslink flag
+function nf_syslink(self, lib)
+    return nf_link(self, lib)
+end
+
+-- make the linkdir flag
+function nf_linkdir(self, dir)
+    return "-L" .. os.args(dir)
+end
+
+-- make the rpathdir flag
+function nf_rpathdir(self, dir)
+    dir = path.translate(dir)
+    if self:has_flags("-rpath=" .. dir, "ldflags") then
+        return "-rpath=" .. os.args(dir:gsub("@[%w_]+", function (name)
+            local maps = {["@loader_path"] = "$ORIGIN", ["@executable_path"] = "$ORIGIN"}
+            return maps[name]
+        end))
+
+    elseif self:has_flags("-rpath " .. dir, "ldflags") then
+        return "-rpath " .. os.args(dir:gsub("%$ORIGIN", "@loader_path"))
+    end
+end
+
+-- make the link arguments list
+function linkargv(self, objectfiles, targetkind, targetfile, flags)
+
+    -- add rpath for dylib (macho), e.g. -install_name @rpath/file.dylib
+    local flags_extra = {}
+    if targetkind == "shared" and is_plat("macosx") then
+        table.insert(flags_extra, "-install_name")
+        table.insert(flags_extra, "@rpath/" .. path.filename(targetfile))
+    end
+
+    -- init arguments
+    return self:program(), table.join(flags, flags_extra, "-o" .. targetfile, objectfiles)
+end
+
+-- link the target file
+function link(self, objectfiles, targetkind, targetfile, flags)
+
+    -- ensure the target directory
+    os.mkdir(path.directory(targetfile))
+
+    -- link it
+    os.runv(linkargv(self, objectfiles, targetkind, targetfile, flags))
+end
+
+-- make the compile arguments list
+function compargv(self, sourcefile, objectfile, flags)
+    return self:program(), table.join("-c", flags, "-o" .. objectfile, sourcefile)
+end
+
+-- compile the source file
+function compile(self, sourcefile, objectfile, dependinfo, flags)
+
+    -- ensure the object directory
+    os.mkdir(path.directory(objectfile))
+
+    -- compile it
+    os.runv(compargv(self, sourcefile, objectfile, flags))
+end
 
--- xmake/modules/detect/tools/gdc/has_flags.lua
+++ xmake/modules/detect/tools/gdc/has_flags.lua
@@ -11,13 +11,113 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
--- 
+--
 -- Copyright (C) 2015-2020, TBOOX Open Source Group.
 --
--- @author      ruki
+-- @author      ruki, BarrOff
 -- @file        has_flags.lua
 --
 
 -- imports
-inherit("detect.tools.dmd.has_flags")
+import("lib.detect.cache")
 
+-- is linker?
+function _islinker(flags, opt)
+
+    -- the flags is "-L=<arg>" or "-L<arg>"?
+    local flags_str = table.concat(flags, " ")
+    if flags_str:startswith("-L=") or flags_str:startswith("-L-") then
+        return true
+    end
+
+    -- the tool kind is ld or sh?
+    local toolkind = opt.toolkind or ""
+    return toolkind == "ld" or toolkind == "sh" or toolkind:endswith("ld") or toolkind:endswith("sh")
+end
+
+-- try running
+function _try_running(...)
+
+    local argv = {...}
+    local errors = nil
+    return try { function () os.runv(unpack(argv)); return true end, catch { function (errs) errors = (errs or ""):trim() end }}, errors
+end
+
+-- attempt to check it from the argument list
+function _check_from_arglist(flags, opt, islinker)
+
+    -- only for compiler
+    if islinker or #flags > 1 then
+        return
+    end
+
+    -- make cache key
+    local key = "detect.tools.gdc.has_flags"
+
+    -- make allflags key
+    local flagskey = opt.program .. "_" .. (opt.programver or "")
+
+    -- load cache
+    local cacheinfo  = cache.load(key)
+
+    -- get all allflags from argument list
+    local allflags = cacheinfo[flagskey]
+    if not allflags then
+
+        -- get argument list
+        allflags = {}
+        local arglist = os.iorunv(opt.program, {"--help"})
+        if arglist then
+            for arg in arglist:gmatch("%s+(%-[%-%a%d]+)%s+") do
+                allflags[arg] = true
+            end
+        end
+
+        -- save cache
+        cacheinfo[flagskey] = allflags
+        cache.save(key, cacheinfo)
+    end
+
+    -- ok?
+    return allflags[flags[1]]
+end
+
+-- try running to check flags
+function _check_try_running(flags, opt, islinker)
+
+    -- make an stub source file
+    local sourcefile = path.join(os.tmpdir(), "detect", "gdc_has_flags.d")
+    local objectfile = path.join(os.tmpdir(), "detect", "gdc_has_flags.o")
+    if not os.isfile(sourcefile) then
+        io.writefile(sourcefile, "void main() {\n}")
+    end
+
+    -- init argv
+    local argv = table.join(flags, "-o" .. objectfile, sourcefile)
+    if not islinker then
+        table.insert(argv, 1, "-c")
+    end
+
+    -- check it
+    return _try_running(opt.program, argv)
+end
+
+-- has_flags(flags)?
+--
+-- @param opt   the argument options, e.g. {toolname = "", program = "", programver = "", toolkind = "[cc|cxx|ld|ar|sh|gc|rc|dc|mm|mxx]"}
+--
+-- @return      true or false
+--
+function main(flags, opt)
+
+    -- is linker?
+    local islinker = _islinker(flags, opt)
+
+    -- attempt to check it from the argument list
+    if _check_from_arglist(flags, opt, islinker) then
+        return true
+    end
+
+    -- try running to check it
+    return _check_try_running(flags, opt, islinker)
+end
