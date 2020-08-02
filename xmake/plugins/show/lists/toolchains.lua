0641936cd7800e0ec27f953566e9ad7d2f743529
ruki <waruqi@gmail.com>
2020-08-02 20:23:03 +0800

    fix show toolchains

--- xmake/plugins/show/lists/toolchains.lua
+++ xmake/plugins/show/lists/toolchains.lua
@@ -21,13 +21,16 @@
 -- imports
 import("core.tool.toolchain")
 import("core.base.text")
+import("core.project.config")
+import("core.project.project")
 
 -- show all toolchains
 function main()
 
+    config.load()
     local tbl = {align = 'l', sep = "        "}
     for _, name in ipairs(toolchain.list()) do
-        local t = toolchain.load(name)
+        local t = os.isfile(os.projectfile()) and project.toolchain(name) or toolchain.load(name)
         table.insert(tbl, {name, t:get("description")})
     end
     print(text.table(tbl))
