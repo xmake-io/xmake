679191eb773e4f8c026a4e71979771d6a5a8ba9c
ruki <waruqi@gmail.com>
2020-08-11 22:39:06 +0800

    improve templates again

--- xmake/templates/c++/console/project/xmake.lua
+++ xmake/templates/c++/console/project/xmake.lua
@@ -1,14 +1,8 @@
 
--- add modes: debug and release
 add_rules("mode.debug", "mode.release")
 
--- add target
 target("${TARGETNAME}")
-
-    -- set kind
     set_kind("binary")
-
-    -- add files
     add_files("src/*.cpp")
 
 ${FAQ}
