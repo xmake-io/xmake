3571d12eed5cf39a8fb0ae97da7529551c66a95e
ruki <waruqi@gmail.com>
2020-08-11 22:35:25 +0800

    improve templates

--- xmake/templates/c/shared/project/xmake.lua
+++ xmake/templates/c/shared/project/xmake.lua
@@ -1,26 +1,13 @@
 
--- add modes: debug and release
 add_rules("mode.debug", "mode.release")
 
--- add target
 target("${TARGETNAME}")
-
-    -- set kind
     set_kind("shared")
-
-    -- add files
     add_files("src/interface.c")
 
--- add target
 target("${TARGETNAME}_demo")
-
-    -- set kind
     set_kind("binary")
-
-    -- add deps
     add_deps("${TARGETNAME}")
-
-    -- add files
-    add_files("src/test.c")
+    add_files("src/main.c")
 
 ${FAQ}
