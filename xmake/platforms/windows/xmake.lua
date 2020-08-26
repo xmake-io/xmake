54b41f1e3c9760b4788851744b2ccdcdf31fce31
ruki <waruqi@gmail.com>
2020-08-26 23:32:07 +0800

    fix ci

--- xmake/platforms/windows/xmake.lua
+++ xmake/platforms/windows/xmake.lua
@@ -48,7 +48,7 @@ platform("windows")
     end)
 
     -- set toolchains
-    set_toolchains("msvc", "clang", "yasm", "nasm", "cuda", "dlang", "rust", "go", "gfortran", "zig")
+    set_toolchains("msvc", "clang", "yasm", "nasm", "cuda", "dlang", "rust", "go", "gfortran", "zig", "tinyc")
 
     -- set menu
     set_menu {
