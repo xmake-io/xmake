95981ed19ba69b2367db5a0e33e2ed80ba811882
ruki <waruqi@gmail.com>
2020-09-09 00:40:36 +0800

    add values.fortran.moduledir

--- xmake/rules/fortran/xmake.lua
+++ xmake/rules/fortran/xmake.lua
@@ -21,7 +21,7 @@
 -- define rule: fortran.build.modules
 rule("fortran.build.modules")
     before_build(function (target)
-        local modulesdir = path.join(target:objectdir(), ".modules")
+        local modulesdir = target:values("fortran.moduledir") or path.join(target:objectdir(), ".modules")
         os.mkdir(modulesdir)
         target:add("fcflags", "-J" .. modulesdir)
         target:add("includedirs", modulesdir, {public = true})
