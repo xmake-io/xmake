c6704add1bd642b5134f6a6eaa32a10e41f8c5c3
ruki <waruqi@gmail.com>
2020-09-20 11:20:10 +0800

    improve yasm tests

--- tests/projects/asm/yasm/xmake.lua
+++ tests/projects/asm/yasm/xmake.lua
@@ -1,3 +1,8 @@
 target("test")
     set_kind("binary")
-    add_files("src/*.S", "src/*.c")
+    add_files("src/*.c")
+    if is_plat("linux") then
+        add_files("src/main_elf.S")
+    else
+        add_files("src/main.S")
+    end
