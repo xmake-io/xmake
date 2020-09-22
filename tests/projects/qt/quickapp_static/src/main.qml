815a7bce252923d39d2096a9d04adf0e2a8ed0bf
ruki <waruqi@gmail.com>
2020-09-22 23:42:35 +0800

    improve qt quickapp templates

--- tests/projects/qt/quickapp_static/src/main.qml
+++ tests/projects/qt/quickapp_static/src/main.qml
@@ -1,9 +1,17 @@
 import QtQuick 2.6
 import QtQuick.Window 2.2
+import QtQuick.Controls 2.2
 
 Window {
+    id: root
     visible: true
     width: 640
     height: 480
     title: qsTr("Hello World")
+    Button {
+        text: "Ok"
+        onClicked: {
+            root.color = Qt.rgba(Math.random(), Math.random(), Math.random(), 1);
+        }
+    }
 }
--- tests/projects/qt/quickapp_static/xmake.lua
+++ tests/projects/qt/quickapp_static/xmake.lua
@@ -2,11 +2,14 @@ add_rules("mode.debug", "mode.release")
 
 includes("qt_add_static_plugins.lua")
 
-target("qt_demo")
+target("demo")
     add_rules("qt.quickapp_static")
     add_headerfiles("src/*.h")
     add_files("src/*.cpp")
     add_files("src/qml.qrc")
+    add_frameworks("QtQuickControls2", "QtQuickTemplates2")
     qt_add_static_plugins("QtQuick2Plugin", {linkdirs = "qml/QtQuick.2", links = "qtquick2plugin"})
     qt_add_static_plugins("QtQuick2WindowPlugin", {linkdirs = "qml/QtQuick/Window.2", links = "windowplugin"})
+    qt_add_static_plugins("QtQuickControls2Plugin", {linkdirs = "qml/QtQuick/Controls.2", links = "qtquickcontrols2plugin"})
+    qt_add_static_plugins("QtQuickTemplates2Plugin", {linkdirs = "qml/QtQuick/Templates.2", links = "qtquicktemplates2plugin"})
 
