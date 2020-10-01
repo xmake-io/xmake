f08654fba0a52248d3ae7e5764907bc2ae1a36b8
ruki <waruqi@gmail.com>
2020-10-01 23:14:08 +0800

    disable workaround for 47bits lua light pointer

--- core/src/xmake/prefix.h
+++ core/src/xmake/prefix.h
@@ -40,7 +40,8 @@
  * private interfaces
  */
 
-#if TB_CPU_BIT64
+// this issue has been fixed, @see https://github.com/LuaJIT/LuaJIT/commit/e9af1abec542e6f9851ff2368e7f196b6382a44c
+#if 0//TB_CPU_BIT64
 /* we use this interface instead of lua_pushlightuserdata() to fix bad light userdata pointer bug
  *
  * @see https://github.com/xmake-io/xmake/issues/914
