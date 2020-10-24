e35442b19096ec0de49ae8023d1aa475e420a7b0
ruki <waruqi@gmail.com>
2020-10-24 20:29:03 +0800

    fix repo order

--- xmake/core/sandbox/modules/import/core/package/repository.lua
+++ xmake/core/sandbox/modules/import/core/package/repository.lua
@@ -71,28 +71,8 @@ end
 -- get all repositories from global or local directory
 function sandbox_core_package_repository.repositories(is_global)
 
-    -- add main global xmake repository
-    local repositories = {}
-    if is_global and global.get("network") ~= "private" then
-
-        -- import fasturl
-        import("net.fasturl")
-
-        -- sort main urls
-        local mainurls = {"https://github.com/xmake-io/xmake-repo.git", "https://gitlab.com/tboox/xmake-repo.git", "https://gitee.com/tboox/xmake-repo.git"}
-        fasturl.add(mainurls)
-        mainurls = fasturl.sort(mainurls)
-
-        -- add main url
-        if #mainurls > 0 then
-            local repo = repository.load("xmake-repo", mainurls[1], "master", true)
-            if repo then
-                table.insert(repositories, repo)
-            end
-        end
-    end
-
     -- load repositories from repository cache
+    local repositories = {}
     for name, repoinfo in pairs(table.wrap(repository.repositories(is_global))) do
         local url = repoinfo
         local branch = nil
@@ -122,6 +102,26 @@ function sandbox_core_package_repository.repositories(is_global)
                 end
             else
                 raise("invalid repository: %s", repo)
+            end
+        end
+    end
+
+    -- add main global xmake repository
+    if is_global and global.get("network") ~= "private" then
+
+        -- import fasturl
+        import("net.fasturl")
+
+        -- sort main urls
+        local mainurls = {"https://github.com/xmake-io/xmake-repo.git", "https://gitlab.com/tboox/xmake-repo.git", "https://gitee.com/tboox/xmake-repo.git"}
+        fasturl.add(mainurls)
+        mainurls = fasturl.sort(mainurls)
+
+        -- add main url
+        if #mainurls > 0 then
+            local repo = repository.load("xmake-repo", mainurls[1], "master", true)
+            if repo then
+                table.insert(repositories, repo)
             end
         end
     end
