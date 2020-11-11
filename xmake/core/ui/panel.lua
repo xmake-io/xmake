a027a704b2799c402f983cf373f689d0d9c6efdc
ruki <waruqi@gmail.com>
2020-11-12 00:45:53 +0800

    improve panel:remove

--- xmake/core/ui/panel.lua
+++ xmake/core/ui/panel.lua
@@ -144,7 +144,7 @@ function panel:insert(v, opt)
 end
 
 -- remove view
-function panel:remove(v)
+function panel:remove(v, opt)
 
     -- check
     assert(v:parent() == self)
@@ -158,7 +158,11 @@ function panel:remove(v)
 
     -- select next view
     if self:current() == v then
-        self:select_next(nil, true)
+        if opt and opt.select_prev then
+            self:select_prev(nil, true)
+        else
+            self:select_next(nil, true)
+        end
     end
 
     -- invalidate it
