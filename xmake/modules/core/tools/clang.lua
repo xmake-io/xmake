cb310f3595525dd90bcb41d608c7a02e5f00e242
Jonathan Poelen <jonathan.poelen@gmail.com>
2020-11-14 03:13:44 +0100

    -W4 implies -Wall and -Wextra with gcc and clang

--- xmake/modules/core/tools/clang.lua
+++ xmake/modules/core/tools/clang.lua
@@ -52,7 +52,7 @@ function init(self)
         ["-W1"] = "-Wall"
     ,   ["-W2"] = "-Wall"
     ,   ["-W3"] = "-Wall"
-    ,   ["-W4"] = "-Wextra"
+    ,   ["-W4"] = "-Wall -Wextra"
 
          -- strip
     ,   ["-s"]  = "-s"
