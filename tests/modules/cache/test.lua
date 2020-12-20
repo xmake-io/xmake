28696782d6d96f03d05198dc956901f39b1b1a23
ruki <waruqi@gmail.com>
2020-12-20 22:54:57 +0800

    improve test

--- tests/modules/cache/test.lua
+++ tests/modules/cache/test.lua
@@ -1,12 +1,15 @@
 import("core.cache.memcache")
 
 function test_memcache(t)
+    memcache.set("mycache", "xyz", {1, 2, 3})
     memcache.set2("mycache", "foo", "bar", "1")
+    t:are_equal(memcache.get("mycache", "xyz"), {1, 2, 3})
     t:are_equal(memcache.get2("mycache", "foo", "bar"), "1")
     memcache.clear("mycache")
     t:are_equal(memcache.get2("mycache", "foo", "bar"), nil)
     memcache.set2("mycache", "foo", "bar", "1")
     memcache.clear()
+    t:are_equal(memcache.get("mycache", "xyz"), nil)
     t:are_equal(memcache.get2("mycache", "foo", "bar"), nil)
 end
 
