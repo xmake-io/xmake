16b51323f32c8951c49f7637c1038d5ee841dbe1
ruki <waruqi@gmail.com>
2021-01-10 00:26:59 +0800

    improve add_defines test

--- tests/apis/add_defines/src/main.cpp
+++ tests/apis/add_defines/src/main.cpp
@@ -7,5 +7,8 @@ int main(int argc, char** argv)
     cout << TEST1 << endl;
     cout << TEST2 << endl;
     cout << TEST3 << endl;
+    cout << TEST4 << endl;
+    cout << TEST5 << endl;
+    cout << TEST6 << endl;
     return 0;
 }
