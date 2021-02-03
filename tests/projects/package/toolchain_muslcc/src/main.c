f757d41673f9297057bf4ca34067559468b1e46d
ruki <waruqi@gmail.com>
2021-02-03 23:54:42 +0800

    improve test

--- tests/projects/package/toolchain_muslcc/src/main.c
+++ tests/projects/package/toolchain_muslcc/src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <zlib.h>
 
 int main(int argc, char** argv)
 {
     printf("hello world!\n");
+    inflate(0, 0);
     return 0;
 }
--- tests/projects/package/toolchain_muslcc/xmake.lua
+++ tests/projects/package/toolchain_muslcc/xmake.lua
@@ -1,7 +1,9 @@
 add_rules("mode.debug", "mode.release")
 add_requires("muslcc")
+add_requires("zlib")
 
 target("test")
     set_kind("binary")
     add_files("src/*.c")
+    add_packages("zlib")
     set_toolchains("muslcc", {packages = "muslcc"})
