ab2f5db2e1d30b1184b1546ad275890381048b04
Joao Matos <joao@tritao.eu>
2021-02-04 16:12:04 +0000

    Fix Zig templates to work with latest toolchain version

--- xmake/templates/zig/shared/project/src/main.zig
+++ xmake/templates/zig/shared/project/src/main.zig
@@ -3,6 +3,6 @@ const std = @import("std");
 extern fn add(a: i32, b: i32) i32;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().outStream();
-    try stdout.print("Hello, {} - {}!\n", .{"world", add(1, 1)});
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Hello, {s} - {d}!\n", .{"world", add(1, 1)});
 }
