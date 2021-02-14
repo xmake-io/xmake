916228567b6892afe7486bef61b12e409670a208
ruki <waruqi@gmail.com>
2021-02-14 12:09:58 +0800

    improve muslcc toolchain test

--- tests/projects/package/toolchain_muslcc/xmake.lua
+++ tests/projects/package/toolchain_muslcc/xmake.lua
@@ -5,7 +5,11 @@ set_plat("cross")
 set_arch("arm")
 
 -- add library packages
-add_requires("zlib", "libplist",  {system = false})
+-- for testing zlib/xmake, libplist/autoconf, libogg/cmake
+add_requires("zlib", "libogg",  {system = false})
+if is_host("macosx", "linux", "bsd") then
+    add_requires("libplist", {system = false})
+end
 
 -- add toolchains package
 add_requires("muslcc")
@@ -16,4 +20,7 @@ set_toolchains("@muslcc")
 target("test")
     set_kind("binary")
     add_files("src/*.c")
-    add_packages("zlib", "libplist")
+    add_packages("zlib", "libplist", "libogg")
+    if has_package("libplist") then
+        add_defines("HAVE_LIBPLIST")
+    end
