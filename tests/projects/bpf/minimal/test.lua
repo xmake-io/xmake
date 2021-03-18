f762654a5b85301a71b752754e4c036a20eb6399
ruki <waruqi@gmail.com>
2021-03-18 23:31:25 +0800

    fix test

--- tests/projects/bpf/minimal/test.lua
+++ tests/projects/bpf/minimal/test.lua
@@ -1,6 +1,6 @@
 function main(t)
     if is_host("linux") then
-        os.vrun("xmake f -p android -vD")
-        os.vrun("xmake -vD")
+        os.vrun("xmake f -y -p android -vD")
+        os.vrun("xmake -y -vD")
     end
 end
