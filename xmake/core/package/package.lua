2643e14e4dc294f4719963bdcb61ab12eac28161
ruki <waruqi@gmail.com>
2021-05-05 00:33:28 +0800

    fix find_package

--- xmake/core/package/package.lua
+++ xmake/core/package/package.lua
@@ -1050,7 +1050,7 @@ function _instance:_fetch_tool(opt)
         fetchinfo = on_fetch(self, {force = opt.force,
                                     system = opt.system,
                                     require_version = opt.require_version})
-        if opt.require_version and not opt.require_version:find(".", 1, true) then
+        if opt.require_version and opt.require_version:find(".", 1, true) then
             local version = type(fetchinfo) == "table" and fetchinfo.version
             if not (version and (version == opt.require_version or semver.satisfies(version, opt.require_version))) then
                 fetchinfo = nil
@@ -1098,7 +1098,7 @@ function _instance:_fetch_library(opt)
                                     system = opt.system,
                                     external = opt.external,
                                     require_version = opt.require_version})
-        if opt.require_version and not opt.require_version:find(".", 1, true) then
+        if opt.require_version and opt.require_version:find(".", 1, true) then
             local version = fetchinfo and fetchinfo.version
             if not (version and (version == opt.require_version or semver.satisfies(version, opt.require_version))) then
                 fetchinfo = nil
