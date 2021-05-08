76d737fcc850ba14de9ca18cdb49c7bfbf3e6620
ruki <waruqi@gmail.com>
2021-05-08 14:34:54 +0800

    improve nvcc/has_flags

--- xmake/modules/detect/tools/nvcc/has_flags.lua
+++ xmake/modules/detect/tools/nvcc/has_flags.lua
@@ -37,10 +37,9 @@ function _islinker(flags, opt)
 end
 
 -- try running
-function _try_running(...)
-    local argv = {...}
+function _try_running(program, argv, opt)
     local errors = nil
-    return try { function () os.runv(unpack(argv)); return true end, catch { function (errs) errors = (errs or ""):trim() end }}, errors
+    return try { function () os.runv(program, argv, opt); return true end, catch { function (errs) errors = (errs or ""):trim() end }}, errors
 end
 
 -- attempt to check it from the argument list
@@ -124,7 +123,7 @@ function _check_try_running(flags, opt, islinker)
     end
 
     -- check flags
-    return _try_running(opt.program, table.join(flags, args))
+    return _try_running(opt.program, table.join(flags, args), opt)
 end
 
 -- has_flags(flags)?
