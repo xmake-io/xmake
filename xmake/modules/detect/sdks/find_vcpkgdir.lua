3c1335a5ef10f4f406db3599b1ec8af95da53676
ruki <waruqi@gmail.com>
2021-06-01 00:45:57 +0800

    improve find vcpkgdir

---
+++ xmake/modules/detect/sdks/find_vcpkgdir.lua
@@ -0,0 +1,64 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        find_vcpkgdir.lua
+--
+
+-- imports
+import("core.base.option")
+import("core.base.global")
+import("core.project.config")
+import("core.cache.detectcache")
+import("lib.detect.find_tool")
+
+-- find vcpkgdir
+function main()
+    local vcpkgdir = detectcache:get("detect.sdks.find_vcpkgdir")
+    if vcpkgdir == nil then
+        if not vcpkgdir then
+            vcpkgdir = config.get("vcpkg") or global.get("vcpkg")
+            if vcpkgdir then
+                if os.isfile(vcpkgdir) then
+                    vcpkgdir = path.directory(vcpkgdir)
+                end
+            end
+        end
+        if not vcpkgdir then
+            vcpkgdir = os.getenv("VCPKG_ROOT") or os.getenv("VCPKG_INSTALLATION_ROOT")
+        end
+        if not vcpkgdir and is_host("macosx", "linux") then
+            local brew = find_tool("brew")
+            if brew then
+                dir = try
+                {
+                    function ()
+                        return os.iorunv(brew.program, {"--prefix", "vcpkg"})
+                    end
+                }
+            end
+            if dir then
+                dir = path.join(dir:trim(), "libexec")
+                if os.isdir(path.join(dir, "installed")) then
+                    vcpkgdir = dir
+                end
+            end
+        end
+        detectcache:set("detect.sdks.find_vcpkgdir", vcpkgdir or false)
+        detectcache:save()
+    end
+    return vcpkgdir or nil
+end
--- xmake/modules/package/manager/vcpkg/find_package.lua
+++ xmake/modules/package/manager/vcpkg/find_package.lua
@@ -24,39 +24,7 @@ import("lib.detect.find_library")
 import("lib.detect.find_tool")
 import("core.project.config")
 import("core.project.target")
-
--- find vcpkg root directory
-function _find_vcpkgdir()
-    local vcpkgdir = _g.vcpkgdir
-    if vcpkgdir == nil then
-        local vcpkg = find_tool("vcpkg")
-        if vcpkg then
-            local dir = path.directory(vcpkg.program)
-            if os.isdir(path.join(dir, "installed")) then
-                vcpkgdir = dir
-            elseif is_host("macosx", "linux") then
-                local brew = find_tool("brew")
-                if brew then
-                    dir = try
-                    {
-                        function ()
-                            return os.iorunv(brew.program, {"--prefix", "vcpkg"})
-                        end
-                    }
-                end
-                if dir then
-                    dir = path.join(dir:trim(), "libexec")
-                    if os.isdir(path.join(dir, "installed")) then
-                        vcpkgdir = dir
-                    end
-                end
-
-            end
-        end
-        _g.vcpkgdir = vcpkgdir or false
-    end
-    return vcpkgdir or nil
-end
+import("detect.sdks.find_vcpkgdir")
 
 -- find package from the vcpkg package manager
 --
@@ -66,7 +34,7 @@ end
 function main(name, opt)
 
     -- attempt to find vcpkg directory
-    local vcpkgdir = _find_vcpkgdir()
+    local vcpkgdir = find_vcpkgdir()
     if not vcpkgdir then
         return
     end
