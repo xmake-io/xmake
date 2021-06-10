d473fbde0ed198f725debff21ad0fca70d6d864d
ruki <waruqi@gmail.com>
2021-06-10 22:49:31 +0800

    improve wdk sign

--- xmake/rules/wdk/sign/sign.lua
+++ xmake/rules/wdk/sign/sign.lua
@@ -56,11 +56,13 @@ function main(target, filepath, mode)
     -- get signtool
     local signtool = _get_tool(target, "signtool")
 
-    -- get timestamp
-    local timestamp = target:values("wdk.sign.timestamp") or "http://timestamp.verisign.com/scripts/timestamp.dll"
-
     -- init arguments
-    local argv = {"sign", "/v", "/t", timestamp}
+    local argv = {"sign", "/v"}
+    local timestamp = target:values("wdk.sign.timestamp")
+    if timestamp then
+        table.insert(argv, "/t")
+        table.insert(argv, timestamp)
+    end
     local company = target:values("wdk.sign.company")
     if company then
         table.insert(argv, "/n")
