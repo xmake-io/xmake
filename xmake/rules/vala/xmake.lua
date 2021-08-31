365de804eac5a08518de1a8df0078339799b520d
ruki <waruqi@gmail.com>
2021-08-31 23:01:38 +0800

    improve vala rules and add more vala tests

--- xmake/rules/vala/xmake.lua
+++ xmake/rules/vala/xmake.lua
@@ -18,13 +18,28 @@
 -- @file        xmake.lua
 --
 
-rule("vala")
+rule("vala.build")
     set_extensions(".vala")
     on_load(function (target)
         -- only vala source files? we need patch c source kind for linker
         local sourcekinds = target:sourcekinds()
         if #sourcekinds == 0 then
             table.insert(sourcekinds, "cc")
+        end
+
+        -- we disable to build across targets in parallel, because the source files may depend on other target modules
+        target:set("policy", "build.across_targets_in_parallel", false)
+
+        -- get vapi file
+        local vapifile = target:data("vala.vapifile")
+        if not vapifile then
+            local vapiname = target:values("vala.vapi")
+            if vapiname then
+                vapifile = path.join(target:targetdir(), vapiname)
+            else
+                vapifile = path.join(target:targetdir(), target:name() .. ".vapi")
+            end
+            target:data_set("vala.vapifile", vapifile)
         end
     end)
     before_buildcmd_file(function (target, batchcmds, sourcefile_vala, opt)
@@ -52,6 +67,21 @@ rule("vala")
                 table.insert(argv, package)
             end
         end
+        if target:is_binary() then
+            for _, dep in ipairs(target:orderdeps()) do
+                if dep:is_shared() or dep:is_static() then
+                    local vapifile = dep:data("vala.vapifile")
+                    if vapifile then
+                        table.join2(argv, vapifile)
+                    end
+                end
+            end
+        else
+            local vapifile = target:data("vala.vapifile")
+            if vapifile then
+                table.insert(argv, "--vapi=" .. vapifile)
+            end
+        end
         table.insert(argv, sourcefile_vala)
         batchcmds:vrunv(valac.program, argv)
         batchcmds:compile(sourcefile_c, objectfile)
@@ -61,4 +91,55 @@ rule("vala")
         batchcmds:set_depmtime(os.mtime(objectfile))
         batchcmds:set_depcache(target:dependfile(objectfile))
     end)
+
+    after_install(function (target)
+        if target:is_shared() or target:is_static() then
+            local vapifile = target:data("vala.vapifile")
+            if vapifile then
+                local installdir = target:installdir()
+                if installdir then
+                    local sharedir = path.join(installdir, "share")
+                    os.mkdir(sharedir)
+                    os.vcp(vapifile, sharedir)
+                end
+            end
+        end
+    end)
+
+    after_uninstall(function (target)
+        if target:is_shared() or target:is_static() then
+            local vapifile = target:data("vala.vapifile")
+            if vapifile then
+                local installdir = target:installdir()
+                if installdir then
+                    os.rm(path.join(installdir, "share", path.filename(vapifile)))
+                end
+            end
+        end
+    end)
+
+rule("vala")
+
+    -- add build rules
+    add_deps("vala.build")
+
+    -- set compiler runtime, e.g. vs runtime
+    add_deps("utils.compiler.runtime")
+
+    -- inherit links and linkdirs of all dependent targets by default
+    add_deps("utils.inherit.links")
+
+    -- support `add_files("src/*.o")` and `add_files("src/*.a")` to merge object and archive files to target
+    add_deps("utils.merge.object", "utils.merge.archive")
+
+    -- we attempt to extract symbols to the independent file and
+    -- strip self-target binary if `set_symbols("debug")` and `set_strip("all")` are enabled
+    add_deps("utils.symbols.extract")
+
+    -- check targets
+    add_deps("utils.check.targets")
+
+    -- check licenses
+    add_deps("utils.check.licenses")
+
 
