1306c34442e6089f05ef54b0772d4fb629c25117
ruki <waruqi@gmail.com>
2021-09-02 23:37:05 +0800

    add shared test for pascal

---
+++ tests/projects/pascal/shared/src/foo.pas
@@ -0,0 +1,23 @@
+library subs;
+
+function SubStr(CString: PChar;FromPos,ToPos: Longint): PChar; cdecl;
+
+var
+  Length: Integer;
+
+begin
+  Length := StrLen(CString);
+  SubStr := CString + Length;
+  if (FromPos > 0) and (ToPos >= FromPos) then
+  begin
+    if Length >= FromPos then
+      SubStr := CString + FromPos;
+    if Length > ToPos then
+    CString[ToPos+1] := #0;
+  end;
+end;
+
+exports
+  SubStr;
+
+end.
---
+++ tests/projects/pascal/shared/src/main.pas
@@ -0,0 +1,14 @@
+uses strings;
+
+function SubStr(const CString: PChar; FromPos, ToPos: longint): PChar;
+  cdecl; external 'subs';
+
+var
+  s: PChar;
+  FromPos, ToPos: Integer;
+begin
+  s := strnew('TestMe');
+  FromPos := 2;
+  ToPos := 3;
+  WriteLn(SubStr(s, FromPos, ToPos));
+end.
