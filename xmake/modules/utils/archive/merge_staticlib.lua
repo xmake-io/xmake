b89f852330498a8968c54140d25345841c59136a
ruki <waruqi@gmail.com>
2021-09-04 11:11:34 +0800

    merge_staticlib stub

---
+++ xmake/modules/utils/archive/merge_staticlib.lua
@@ -0,0 +1,56 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        merge_staticlib.lua
+--
+
+-- imports
+import("core.base.option")
+
+-- merge *.a archive libraries for ar
+function _merge_for_ar(program, outputfile, libraryfiles, opt)
+    opt = opt or {}
+end
+
+-- merge *.a archive libraries for msvc/lib.exe
+function _merge_for_msvclib(program, outputfile, libraryfiles, opt)
+    opt = opt or {}
+end
+
+-- merge *.a archive libraries
+function main(target, outputfile, libraryfiles)
+    local program, toolname = target:tool("ar")
+    if program and toolname then
+        if toolname:find("ar") then
+            _merge_for_ar(program, outputfile, libraryfiles)
+        elseif toolname == "link" and target:is_plat("windows") then
+            local msvc
+            for _, toolchain_inst in ipairs(target:toolchains()) do
+                if toolchain_inst:name() == "msvc" then
+                    msvc = toolchain_inst
+                    break
+                end
+            end
+            _merge_for_msvclib((program:gsub("link%.exe", "lib.exe")), outputfile, libraryfiles, {envs = msvc and msvc:runenvs()})
+        else
+            raise("cannot merge (%s): unknown ar tool %s!", table.concat(libraryfiles, ", "), toolname)
+        end
+    else
+        raise("cannot merge (%s): ar not found!", table.concat(libraryfiles, ", "))
+    end
+end
+
