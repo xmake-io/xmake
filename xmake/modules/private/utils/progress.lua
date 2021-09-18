dc0319e63ea90ed6028ca8f9455350bf6ca85b66
ruki <waruqi@gmail.com>
2021-09-18 13:59:09 +0800

    fix progress

--- xmake/modules/private/utils/progress.lua
+++ xmake/modules/private/utils/progress.lua
@@ -114,6 +114,7 @@ end
 
 -- get the message text with process
 function text(progress, format, ...)
+    progress = math.floor(progress)
     local progress_prefix = "${color.build.progress}" .. theme.get("text.build.progress_format") .. ":${clear} "
     if option.get("verbose") then
         return string.format(progress_prefix .. "${dim}" .. format, progress, ...)
