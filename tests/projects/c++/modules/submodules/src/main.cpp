55b632b420a23550991083471c90bbd042a974f7
ruki <waruqi@gmail.com>
2021-10-13 23:18:34 +0800

    add c++ submodules and partitions

---
+++ tests/projects/c++/modules/submodules/src/main.cpp
@@ -0,0 +1,10 @@
+import std.core;
+import math;
+
+int main() {
+
+    std::cout << std::endl;
+
+    std::cout << "add(3, 4): " << add(3, 4) << std::endl;
+    std::cout << "mul(3, 4): " << mul(3, 4) << std::endl;
+}
