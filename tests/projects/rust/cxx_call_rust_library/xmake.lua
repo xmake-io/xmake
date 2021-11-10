886c51e8ce29b07adb96d4db0f73e38ce9318990
ruki <waruqi@gmail.com>
2021-11-11 00:49:33 +0800

    improve rust tests

---
+++ tests/projects/rust/cxx_call_rust_library/xmake.lua
@@ -0,0 +1,13 @@
+add_rules("mode.debug", "mode.release")
+
+target("foo")
+    set_kind("static")
+    add_files("src/foo.rs")
+
+target("test")
+    set_kind("binary")
+    add_deps("foo")
+    add_files("src/main.cc")
+    add_files("src/bridge.rs", {rules = "rust.cxxbridge"})
+
+
