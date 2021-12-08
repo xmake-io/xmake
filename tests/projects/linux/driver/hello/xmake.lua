b76de4c1fb632724c412a1638af8694fb35e021d
ruki <waruqi@gmail.com>
2021-12-08 23:15:44 +0800

    add linux driver rule stub

---
+++ tests/projects/linux/driver/hello/xmake.lua
@@ -0,0 +1,9 @@
+add_rules("mode.release", "mode.debug")
+
+add_requires("linux-headers", {configs = {driver_modules = true}})
+
+target("hello")
+    add_rules("platform.linux.driver")
+    add_files("src/*.c")
+    add_packages("linux-headers")
+    set_license("GPL-2.0")
