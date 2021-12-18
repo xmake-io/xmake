2e7c3bb2459dc25e68a7241b8b8c2d2811849dd4
ruki <waruqi@gmail.com>
2021-12-18 15:58:13 +0800

    Update package.lua

--- xmake/core/package/package.lua
+++ xmake/core/package/package.lua
@@ -1316,7 +1316,7 @@ function _instance:find_package(name, opt)
                               mode = self:mode(),
                               plat = self:plat(),
                               arch = self:arch(),
-                              configs = self:configs(),
+                              configs = table.join(self:configs(), opt.configs),
                               buildhash = self:buildhash(), -- for xmake package or 3rd package manager, e.g. go:: ..
                               cachekey = opt.cachekey or "fetch_package_system",
                               external = opt.external,
