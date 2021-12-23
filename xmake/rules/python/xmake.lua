0a65e25c02c36764ba09e5051565d41a137bd0b5
ruki <waruqi@gmail.com>
2021-12-23 22:34:53 +0800

    add python.library rule

---
+++ xmake/rules/python/xmake.lua
@@ -0,0 +1,34 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        xmake.lua
+--
+
+-- @see https://github.com/xmake-io/xmake/issues/1896
+rule("python.library")
+    on_load(function (target)
+        target:set("kind", "shared")
+        target:set("prefixname", "_")
+        local soabi = target:extraconf("rules", "python.library", "soabi")
+        if soabi then
+        else
+            if target:is_plat("windows") then
+                target:set("extension", ".pyd")
+            end
+        end
+    end)
+
