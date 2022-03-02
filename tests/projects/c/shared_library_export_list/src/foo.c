b68ec3a3564e6fa261a45e01c6d590d49219600f
ruki <waruqi@gmail.com>
2022-03-02 22:44:22 +0800

    export list

---
+++ tests/projects/c/shared_library_export_list/src/foo.c
@@ -0,0 +1,14 @@
+#include "foo.h"
+#include <stdio.h>
+
+void stub() {
+    printf("stub\n");
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
--- tests/projects/c/shared_library_export_list/xmake.lua
+++ tests/projects/c/shared_library_export_list/xmake.lua
@@ -2,8 +2,10 @@ add_rules("mode.release", "mode.debug")
 
 target("foo")
     set_kind("shared")
-    add_files("src/foo.c", "src/bar.cpp")
-    add_rules("utils.symbols.export_all", {export_classes = true})
+    add_files("src/foo.c")
+    add_rules("utils.symbols.export_list", {symbols = {
+        "add",
+        "sub"}})
 
 target("test")
     set_kind("binary")
