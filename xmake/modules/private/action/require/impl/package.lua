8ba035ac5c335378192fc130ae2a0cac5d37776f
ruki <waruqi@gmail.com>
2022-03-06 20:11:03 +0800

    fix package policy

--- xmake/modules/private/action/require/impl/package.lua
+++ xmake/modules/private/action/require/impl/package.lua
@@ -462,12 +462,12 @@ function _init_requireinfo(requireinfo, package, opt)
         if not package:is_headeronly() then
             if package:is_library() then
                 requireinfo.configs.toolchains = requireinfo.configs.toolchains or project.get("target.toolchains")
-                if package:policy("package.inherit_external_configs") then
+                if project.policy("package.inherit_external_configs") then
                     requireinfo.configs.toolchains = requireinfo.configs.toolchains or get_config("toolchain")
                 end
             end
             requireinfo.configs.vs_runtime = requireinfo.configs.vs_runtime or project.get("target.runtimes")
-            if package:policy("package.inherit_external_configs") then
+            if project.policy("package.inherit_external_configs") then
                 requireinfo.configs.vs_runtime = requireinfo.configs.vs_runtime or get_config("vs_runtime")
             end
         end
