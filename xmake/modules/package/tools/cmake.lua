d5fa1e2e2efaef66bd34ff071e9b9f5c89d41045
ruki <waruqi@gmail.com>
2022-03-25 22:56:02 +0800

    fix tools.cmake

--- xmake/modules/package/tools/cmake.lua
+++ xmake/modules/package/tools/cmake.lua
@@ -410,7 +410,7 @@ function _get_configs_for_cross(package, configs, opt)
         name = name:gsub("clang%-", "clang++-")
         name = name:gsub("gcc$", "g++")
         name = name:gsub("gcc%-", "g++-")
-        envs.CMAKE_CXX_COMPILER = dir and path.join(dir, name) or name
+        envs.CMAKE_CXX_COMPILER = _translate_bin_path(dir and path.join(dir, name) or name)
     end
     -- @note The link command line is set in Modules/CMake{C,CXX,Fortran}Information.cmake and defaults to using the compiler, not CMAKE_LINKER,
     -- so we need set CMAKE_CXX_LINK_EXECUTABLE to use CMAKE_LINKER as linker.
