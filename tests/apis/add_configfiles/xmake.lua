4f2abecc0782004f06d15f8967db6ea7ac635aae
ruki <waruqi@gmail.com>
2022-04-01 21:01:46 +0800

    fix copy only

--- tests/apis/add_configfiles/xmake.lua
+++ tests/apis/add_configfiles/xmake.lua
@@ -26,7 +26,7 @@ target("test")
     set_configdir("$(buildir)/config")
     add_configfiles("test.c.in", {filename = "mytest.c"})
     add_configfiles("config.h.in", {variables = {hello = "xmake"}, prefixdir = "header"})
-    add_configfiles("*.man", {copyonly = true, prefixdir = "man"})
+    add_configfiles("*.man", {onlycopy = true, prefixdir = "man"})
     add_includedirs("$(buildir)/config/header")
 
 
