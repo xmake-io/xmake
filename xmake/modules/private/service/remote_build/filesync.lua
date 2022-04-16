489b241302ae13ef9405f27ac9b881db8550dd5a
ruki <waruqi@gmail.com>
2022-04-16 11:42:20 +0800

    add filesync stub

---
+++ xmake/modules/private/service/remote_build/filesync.lua
@@ -0,0 +1,41 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        filesync.lua
+--
+
+-- imports
+import("core.base.object")
+
+-- define module
+local filesync = filesync or object()
+
+-- init filesync
+function filesync:init(rootdir)
+    self._ROOTDIR = rootdir
+end
+
+-- get root directory
+function filesync:rootdir()
+    return self._ROOTDIR
+end
+
+function main(rootdir)
+    local instance = filesync()
+    instance:init(rootdir)
+    return instance
+end
