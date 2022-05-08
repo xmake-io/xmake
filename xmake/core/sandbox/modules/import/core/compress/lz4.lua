4045ac9d0c0e49c04794e2f8a253e81d6d29287c
ruki <waruqi@gmail.com>
2022-05-08 17:29:38 +0800

    add lz4 modules

---
+++ xmake/core/sandbox/modules/import/core/compress/lz4.lua
@@ -0,0 +1,47 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        lz4.lua
+--
+
+-- define module
+local sandbox_core_base_lz4 = sandbox_core_base_lz4 or {}
+
+-- load modules
+local lz4   = require("base/lz4")
+local raise = require("sandbox/modules/raise")
+
+-- compress data
+function sandbox_core_base_lz4.compress(data, opt)
+    local result, errors = lz4.compress(data, opt)
+    if not result and errors then
+        raise(errors)
+    end
+    return result
+end
+
+-- decompress data
+function sandbox_core_base_lz4.decompress(data, opt)
+    local result, errors = lz4.decompress(data, opt)
+    if not result and errors then
+        raise(errors)
+    end
+    return result
+end
+
+-- return module
+return sandbox_core_base_lz4
