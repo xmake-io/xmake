b3e3b7d6d7465abe006b15458701fbc720442b93
ruki <waruqi@gmail.com>
2022-05-08 23:46:19 +0800

    update todo

--- xmake/core/compress/lz4.lua
+++ xmake/core/compress/lz4.lua
@@ -72,7 +72,7 @@ function lz4.decompress(data, opt)
     return bytes(result)
 end
 
--- TODO use lz4file
+-- TODO use stream in core
 -- compress file data
 function lz4.compress_file(srcpath, dstpath, opt)
     local data, errors = io.readfile(srcpath, {encoding = "binary"})
@@ -85,7 +85,7 @@ function lz4.compress_file(srcpath, dstpath, opt)
     return false, errors or "compress failed"
 end
 
--- TODO use lz4file
+-- TODO use stream in core
 -- decompress file data
 function lz4.decompress_file(srcpath, dstpath, opt)
     local data, errors = io.readfile(srcpath, {encoding = "binary"})
