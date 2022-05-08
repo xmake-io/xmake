517b91f7101291627e992f587e382f930855c320
ruki <waruqi@gmail.com>
2022-05-08 19:29:28 +0800

    fix compress test

--- tests/modules/compress/test.lua
+++ tests/modules/compress/test.lua
@@ -1,6 +1,6 @@
 import("core.compress.lz4")
 
 function test_lz4_frame_compress(t)
-    t:are_equal(lz4.decompress(lz4.compress("hello world")), "hello world")
+    t:are_equal(lz4.decompress(lz4.compress("hello world")):str(), "hello world")
 end
 
