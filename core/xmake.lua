69e5fc7755942c14712c92c2411fb0af667e54e4
ruki <waruqi@gmail.com>
2022-05-26 00:29:38 +0800

    disable vectorexts

--- core/xmake.lua
+++ core/xmake.lua
@@ -29,11 +29,12 @@ add_cxflags("-Wno-error=deprecated-declarations", "-fno-strict-aliasing", "-Wno-
 add_defines("_GNU_SOURCE=1", "_FILE_OFFSET_BITS=64", "_LARGEFILE_SOURCE")
 
 -- add vectorexts
+--[[
 if is_arch("x86", "x64", "i386", "x86_64") then
     add_vectorexts("sse", "sse2", "sse3", "avx", "avx2")
 elseif is_arch("arm.*") then
     add_vectorexts("neon")
-end
+end]]
 
 -- for the windows platform (msvc)
 if is_plat("windows") then
