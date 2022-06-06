ad74f3fd1c2c5bf9a95ccad188cf1e412e223235
couc <1425042285@qq.com>
2022-06-06 10:19:05 +0800

    fix that files cannot be copied to the installation directory correctly when updating when there is a soft link(directory junction) in the installation path.

--- xmake/scripts/update-script.bat
+++ xmake/scripts/update-script.bat
@@ -4,4 +4,4 @@ echo Removing old files
 cd /d "%~1"
 del actions core includes languages modules platforms plugins repository rules scripts templates themes /S /Q /F >nul
 echo Copying from temp directory to "%~1"
-xcopy "%~2" "%~1" /S /Y /Q >nul
+robocopy "%~2" "%~1" /S /IS /NFL /NDL /NJH /NJS >nul
