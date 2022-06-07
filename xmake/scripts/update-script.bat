da7a45347a82af5096bd0bf54a9713f7a1ee6843
couc <1425042285@qq.com>
2022-06-07 20:17:59 +0800

    try to be compatible with an older version of Windows.

--- xmake/scripts/update-script.bat
+++ xmake/scripts/update-script.bat
@@ -4,4 +4,13 @@ echo Removing old files
 cd /d "%~1"
 del actions core includes languages modules platforms plugins repository rules scripts templates themes /S /Q /F >nul
 echo Copying from temp directory to "%~1"
-robocopy "%~2" "%~1" /S /IS /NFL /NDL /NJH /NJS >nul
+
+if exist "%WINDIR%\System32\Robocopy.exe" (
+    robocopy "%~2" "%~1" /S /IS /NFL /NDL /NJH /NJS >nul
+) else (
+    if exist "%WINDIR%\SysWOW64\Robocopy.exe" (
+        robocopy "%~2" "%~1" /S /IS /NFL /NDL /NJH /NJS >nul
+    ) else (
+        xcopy "%~2" "%~1" /S /Y /Q >nul 
+    )
+)
