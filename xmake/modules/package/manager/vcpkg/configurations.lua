fe2fb1334bf9f9cb6da448c774ed9006d6e56e5d
ruki <waruqi@gmail.com>
2022-06-21 00:55:01 +0800

    fix typo

--- xmake/modules/package/manager/vcpkg/configurations.lua
+++ xmake/modules/package/manager/vcpkg/configurations.lua
@@ -43,7 +43,7 @@ function main()
         baseline           = {description = "set the builtin baseline."},
         features           = {description = "set the features of dependency."},
         default_features   = {description = "enables or disables any defaults provided by the dependency.", default = true},
-        registries         = {description = "set the registries in vcpkg-configuration.json"}
+        registries         = {description = "set the registries in vcpkg-configuration.json"},
         default_registries = {description = "set the default registries in vcpkg-configuration.json"}
     }
 end
