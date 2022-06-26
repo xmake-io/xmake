685fa705f82bda73e2fc25d3ebb0071be54aa51b
ruki <waruqi@gmail.com>
2022-06-26 21:41:37 +0800

    improve tests

---
+++ tests/projects/other/build_deps/src/dep5.c
@@ -0,0 +1,6 @@
+#include "interface.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ tests/projects/other/build_deps/src/test3.c
@@ -0,0 +1,8 @@
+#include "interface.h"
+#include <stdio.h>
+  
+int main(int argc, char** argv)
+{
+    printf("add(1, 2) = %d\n", add(1, 2));
+    return 0;
+}
