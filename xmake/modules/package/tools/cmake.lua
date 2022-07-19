338088a607b3ffe10602a44a2f1b393f4ba722b2
Jérôme Leclercq <lynix680@gmail.com>
2022-07-19 18:19:13 +0200

    cmake: add support of include_external_headers policy

--- xmake/modules/package/tools/cmake.lua
+++ xmake/modules/package/tools/cmake.lua
@@ -275,6 +275,9 @@ function _get_configs_for_generic(package, configs, opt)
     if package:config("pic") ~= false then
         table.insert(configs, "-DCMAKE_POSITION_INDEPENDENT_CODE=ON")
     end
+    if package:policy("package.include_external_headers") == false then
+        table.insert(configs, "-DCMAKE_NO_SYSTEM_FROM_IMPORTED=ON")
+    end
 end
 
 -- get configs for windows
