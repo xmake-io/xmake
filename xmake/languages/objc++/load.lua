618dc4eac96bfd94750b86ff5ede666307214e0f
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-07-25 19:17:48 +0200

    add module api to objc++

--- xmake/languages/objc++/load.lua
+++ xmake/languages/objc++/load.lua
@@ -80,6 +80,7 @@ function _get_apis()
         -- target.add_xxx
     ,   "target.add_headers"        -- TODO deprecated
     ,   "target.add_headerfiles"
+    ,   "target.add_modulefiles"
     ,   "target.add_linkdirs"
     ,   "target.add_includedirs"
     ,   "target.add_sysincludedirs"
