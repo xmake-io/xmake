63bf72bac39ea83aed26dc63d68aee67a3383a4a
ruki <waruqi@gmail.com>
2022-07-31 23:45:25 +0800

    fix touch

--- xmake/core/base/os.lua
+++ xmake/core/base/os.lua
@@ -559,6 +559,7 @@ end
 -- we will do not change it if atime/mtime is zero
 function os.touch(filepath, opt)
     opt = opt or {}
+    opt.mtime = os.time()
     if not os._touch(filepath, opt.atime or 0, opt.mtime or 0) then
         return false, string.format("cannot touch %s, %s", filepath, os.strerror())
     end
