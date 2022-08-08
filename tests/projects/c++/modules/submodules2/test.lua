c6ee1e60c83626215ba3f1bf8471fcc72ab3d4b4
ruki <waruqi@gmail.com>
2022-08-08 21:01:10 +0800

    fix tests

--- tests/projects/c++/modules/submodules2/test.lua
+++ tests/projects/c++/modules/submodules2/test.lua
@@ -5,12 +5,14 @@ function main(t)
     local test = false
     if is_subhost("windows") then
         test = true
-    elseif is_host("linux", "macosx") then
+    elseif is_host("linux") then
         local gcc = find_tool("gcc", {version = true})
+        if gcc and gcc.version and semver.compare(gcc.version, "11.0") >= 0 then
+            test = true
+        end
+    elseif is_host("macosx") then
         local clang = find_tool("clang", {version = true})
         if clang and clang.version and semver.compare(clang.version, "14.0") >= 0 then
-            test = true
-        elseif gcc and gcc.version and semver.compare(gcc.version, "11.0") >= 0 then
             test = true
         end
     end
