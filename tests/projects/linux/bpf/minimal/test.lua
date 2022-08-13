a8829eee52be60f9ee7cf3b29ea03c42bd6c986e
ruki <waruqi@gmail.com>
2022-08-13 10:27:24 +0800

    disable bpf test for arch

--- tests/projects/linux/bpf/minimal/test.lua
+++ tests/projects/linux/bpf/minimal/test.lua
@@ -1,5 +1,9 @@
 function main(t)
     if is_host("linux") and os.arch() == "x86_64" then
+        -- TODO, we need wait fix of linux kernel
+        if linuxos.name() == "archlinux" then
+            return
+        end
         os.vrun("xmake f -y -p android -vD")
         os.vrun("xmake -y -vD")
     end
