eb9a3a5c4a74452d78b221e0af738f040fbcd198
ruki <waruqi@gmail.com>
2022-10-08 00:39:37 +0800

    improve tests

--- tests/projects/dlang/shared_library/src/interfaces.d
+++ tests/projects/dlang/shared_library/src/interfaces.d
@@ -1,8 +1,8 @@
-import core.sys.windows.windows;
-import core.sys.windows.dll;
-
-version(Windows)
-mixin SimpleDllMain;
+version(Windows) {
+    import core.sys.windows.windows;
+    import core.sys.windows.dll;
+    mixin SimpleDllMain;
+}
 
 extern(C) int add(int a, int b) {
     return a + b;
