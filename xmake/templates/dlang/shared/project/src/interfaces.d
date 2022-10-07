2c4ce2f510178a00c43cd16d7c85819b4a289423
ruki <waruqi@gmail.com>
2022-10-08 00:40:27 +0800

    improve template

--- xmake/templates/dlang/shared/project/src/interfaces.d
+++ xmake/templates/dlang/shared/project/src/interfaces.d
@@ -1,9 +1,15 @@
-extern(C) int add(int a, int b)
-{
+version(Windows) {
+    import core.sys.windows.windows;
+    import core.sys.windows.dll;
+    mixin SimpleDllMain;
+}
+
+extern(C) int add(int a, int b) {
     return a + b;
 }
 
-extern(C) int sub(int a, int b)
-{
+extern(C) int sub(int a, int b) {
     return a - b;
 }
+
+
