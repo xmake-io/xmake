f6a1811657d2a2abc5ca6026bb340cb68942db0a
Jianpeng Peng <jacob.peng@nxp.com>
2022-10-19 11:15:47 +0800

    Add rule and test project for gnu-rm.

---
+++ tests/projects/embed/gnu-rm/hello/xmake.lua
@@ -0,0 +1,16 @@
+-- Configure xmake.conf with the actual environment variables.
+set_toolchains("envs", "gnu-rm")
+
+add_rules("mode.debug", "mode.release")
+
+target("foo")
+    add_rules("gnu-rm.static")
+    add_files("src/foo/*.c")
+
+target("hello")
+    add_deps("foo")
+    add_rules("gnu-rm.binary")
+    set_kind("binary")
+    add_files("src/*.c", "src/*.s")
+    add_files("src/*.ld")
+    add_includedirs("src/lib/cmsis")
