4752d974156a2aff70215b73df461f26c843d9a2
ruki <waruqi@gmail.com>
2022-11-08 22:35:24 +0800

    strip spaces

--- tests/projects/objc/metal_app/Application/AAPLViewController.m
+++ tests/projects/objc/metal_app/Application/AAPLViewController.m
@@ -21,13 +21,13 @@ Implementation of our cross-platform view controller
 
     // Set the view to use the default device
     _view = (MTKView *)self.view;
-    
+
     _view.device = MTLCreateSystemDefaultDevice();
-    
+
     NSAssert(_view.device, @"Metal is not supported on this device");
-    
+
     _renderer = [[AAPLRenderer alloc] initWithMetalKitView:_view];
-    
+
     NSAssert(_renderer, @"Renderer failed initialization");
 
     // Initialize our renderer with the view size
