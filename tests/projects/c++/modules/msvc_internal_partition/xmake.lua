624490c743ac474ddca3c3cdb93a9fbff8e26dc5
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-12-03 16:31:07 +0100

    add tests

---
+++ tests/projects/c++/modules/msvc_internal_partition/xmake.lua
@@ -0,0 +1,6 @@
+add_rules("mode.release", "mode.debug")
+set_languages("c++20")
+target("msvc_internal_partition")
+    set_kind("binary")
+    add_files("src/*.cpp", "src/hello.mpp")
+    add_files("src/hello_internal.mpp", {values = {["msvc.internalpartition"] = true}})
