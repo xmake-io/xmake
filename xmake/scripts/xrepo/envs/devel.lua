1c32eaad0fd0cb6389806bae00d326d77c775f01
ruki <waruqi@gmail.com>
2022-12-06 22:42:17 +0800

    improve py envs

--- xmake/scripts/xrepo/envs/devel.lua
+++ xmake/scripts/xrepo/envs/devel.lua
@@ -1,5 +1,9 @@
 add_requires("cmake")
-add_requires("python")
+if is_host("windows") and winos.version():le("win7") then
+    add_requires("python 3.7.x")
+else
+    add_requires("python 3.x")
+end
 if is_host("linux", "bsd", "macosx") then
     add_requires("pkg-config", "autoconf", "automake", "libtool")
 elseif is_host("windows") then
--- xmake/scripts/xrepo/envs/python3.lua
+++ xmake/scripts/xrepo/envs/python3.lua
@@ -1,1 +1,5 @@
-add_requires("python 3.x")
+if is_host("windows") and winos.version():le("win7") then
+    add_requires("python 3.7.x")
+else
+    add_requires("python 3.x")
+end
