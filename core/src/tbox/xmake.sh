a9f753074707e6fb71c67b28ea5f9803f1babfe8
ruki <waruqi@gmail.com>
2022-12-14 23:05:19 +0800

    add tbox to xmake.sh

--- core/src/tbox/xmake.sh
+++ core/src/tbox/xmake.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 includes "tbox/src"
+target_end
 
 # enable hash
 set_config "hash" true
@@ -14,6 +15,10 @@ set_configvar "TB_CONFIG_MODULE_HAVE_CHARSET" 1
 set_config "force_utf8" true
 set_configvar "TB_CONFIG_FORCE_UTF8" 1
 
+# enable float
+set_config "float" true
+set_configvar "TB_CONFIG_TYPE_HAVE_FLOAT" 1
+
 # disable demo
-set_config "demo" true
+set_config "demo" false
 
