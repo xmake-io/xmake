ba5101263cc13bd51e8d22616e1a315e95e652ed
ruki <waruqi@gmail.com>
2023-01-08 10:31:41 +0800

    Update xmake.lua

--- tests/projects/c++/modules/stl_headerunit_cpp_only/xmake.lua
+++ tests/projects/c++/modules/stl_headerunit_cpp_only/xmake.lua
@@ -1,8 +1,8 @@
 add_rules("mode.release", "mode.debug")
+set_languages("c++20")
 
 target("stl_headerunit_cpp_only")
     set_kind("binary")
     add_files("src/*.cpp")
-    set_languages("c++20")
     set_policy("build.c++.modules", true)
     set_values("c++.clang.modules.strict", true) -- clang std module clash with stl header units or c++23 std module so we disable it
