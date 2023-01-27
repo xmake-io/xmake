xmake-io/xmake

commit 02fb7ef69e78a5d6a8e0066f8866684533836ac3

improve archive

xmake/rules/qt/load.lua
@@ -130,7 +130,7 @@ function main(target, opt)
     local languages = target:get("languages")
     local cxxlang = false
     for _, lang in ipairs(languages) do
-        if lang:startswith("cxx") or lang:startswith("c++") then
+        if lang:startswith("xx") or lang:startswith("++") then
             cxxlang = true
             break
         end

scripts/archive-all
@@ -20,17 +20,19 @@ git clean -dfX
 git submodule foreach git clean -dfX
 
 # copy files to tmpdir/xmake
-topdir=$tmpdir/xmake
-mkdir -p $topdir/xmake/scripts
+version=`cat ./core/xmake.lua | grep -E "^set_version" | grep -oE "[0-9]*\.[0-9]*\.[0-9]*"`
+srcdir=$tmpdir/xmake
+topdir=$srcdir/xmake-$version
+mkdir -p $topdir/scripts
 cd $tmpdir/repo || exit
-cp -r ./core $topdir/xmake
-cp -r ./xmake $topdir/xmake/xmake
-cp ./scripts/*.sh $topdir/xmake/scripts
-cp -r ./scripts/man $topdir/xmake/scripts
-cp -r ./scripts/debian $topdir/xmake/scripts
-cp ./*.md $topdir/xmake
-cp configure $topdir/xmake
-cd $topdir || exit
+cp -r ./core $topdir
+cp -r ./xmake $topdir/xmake
+cp ./scripts/*.sh $topdir/scripts
+cp -r ./scripts/man $topdir/scripts
+cp -r ./scripts/debian $topdir/scripts
+cp ./*.md $topdir
+cp configure $topdir
+cd $srcdir || exit
 rm -rf ./core/src/tbox/tbox/src/demo
 rm -rf ./core/src/luajit
 

commit fcc5adff04b3b57d4ae68d86f9e7f75a7270b36b

Fix detection of target language for qt projects
Stray change in 02fb7ef69e78a5d6a8e0066f8866684533836ac3 broke it such
that language set via `set_languages()` wasn't detected and `-std=c++11`
was appended after `-std` flag that came from `set_languages()` in
`xmake.lua`.

To reproduce:
 1. `xmake create -tqt.quickapp`
 2. `vim xmake.lua` and add `set_languages("c++17")`
 3. `xmake --verbose` and look at `-std=` options

xmake/rules/qt/load.lua
@@ -141,7 +141,7 @@ function main(target, opt)
     local languages = target:get("languages")
     local cxxlang = false
     for _, lang in ipairs(languages) do
-        if lang:startswith("xx") or lang:startswith("++") then
+        if lang:startswith("cxx") or lang:startswith("c++") then
             cxxlang = true
             break
         end