19a31871b4bc3537a76dbce147c07196766494f2
233lol <18070600+233lol@users.noreply.github.com>
2023-02-10 16:34:51 +0800

    Update github_mirror.lua

--- xmake/scripts/pac/github_mirror.lua
+++ xmake/scripts/pac/github_mirror.lua
@@ -2,7 +2,7 @@ import("net.fasturl")
 
 function mirror(url)
     local configs = {}
-    local proxyurls = {"hub.fastgit.xyz"}
+    local proxyurls = {"hub.nuaa.cf"}
     fasturl.add(proxyurls)
     proxyurls = fasturl.sort(proxyurls)
     if #proxyurls > 0 then
