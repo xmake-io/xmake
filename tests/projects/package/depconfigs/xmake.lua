6015ba5e5b254ebab40a8222e2d857a9af4df1c2
ruki <waruqi@gmail.com>
2023-02-16 23:26:54 +0800

    fix tests

--- tests/projects/package/depconfigs/xmake.lua
+++ tests/projects/package/depconfigs/xmake.lua
@@ -1,11 +1,8 @@
 add_requires("libpng", {system = false, configs = {vs_runtime = "MD"}})
 add_requires("libtiff", {system = false, configs = {vs_runtime = "MD", zlib = true}})
-add_requires("libwebp")
 
-add_requireconfs("libwebp",            {system = false, configs = {shared = true, vs_runtime = "MD"}})
 add_requireconfs("libpng.zlib",        {system = false, override = true, configs = {cxflags = "-DTEST1"}, version = "1.2.10"})
 add_requireconfs("libtiff.*|cmake",    {system = false, configs = {cxflags = "-DTEST2"}})
-add_requireconfs("libwebp.**|cmake",   {system = false, configs = {cxflags = "-DTEST3"}})
 
 target("test")
     set_kind("binary")
@@ -39,18 +36,3 @@ target("test2")
         end
     end)
 
-target("test3")
-    set_kind("binary")
-    add_files("src/*.c")
-    add_packages("libwebp")
-    before_build(function (target)
-        if target:pkg("libwebp") then
-            local found
-            for _, linkdir in ipairs(target:pkg("libwebp"):get("linkdirs")) do
-                if linkdir:find("zlib", 1, true) then
-                    found = true
-                end
-            end
-            assert(found, "package(zlib) not found!")
-        end
-    end)
