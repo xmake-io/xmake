27c69b5a6b23f1b597c320a6404c4612c6625cf3
ruki <waruqi@gmail.com>
2023-02-20 22:57:01 +0800

    add comment

--- xmake/rules/cuda/devlink/xmake.lua
+++ xmake/rules/cuda/devlink/xmake.lua
@@ -37,6 +37,9 @@ rule("cuda.build.devlink")
         import("utils.progress")
 
         -- disable devlink?
+        --
+        -- @note cuda.build.devlink value will be deprecated
+        --
         local devlink = target:policy("build.cuda.devlink") or target:values("cuda.build.devlink")
         if devlink == false then
             return
