17c28518c434db16c563ade79b0ec9fc1976c757
pdagobert <pdagobert@users.noreply.github.com>
2023-03-27 03:17:08 +0200

    Dub package test platforms should match console test

--- tests/projects/dlang/dub_package/test.lua
+++ tests/projects/dlang/dub_package/test.lua
@@ -2,5 +2,9 @@
 function main(t)
 
     -- build project
-    t:build()
+    if is_host("macosx") and os.arch() ~= "arm64" then
+        t:build()
+    else
+        return t:skip("wrong host platform")
+    end
 end
