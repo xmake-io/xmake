cba6838a760e25a8c372ce5e547cd6691eb9ef61
ruki <waruqi@gmail.com>
2023-04-25 22:33:54 +0800

    fix strip for arm64

--- xmake/modules/core/tools/link.lua
+++ xmake/modules/core/tools/link.lua
@@ -59,6 +59,12 @@ end
 
 -- make the strip flag
 function nf_strip(self, level, target)
+
+    -- link.exe/arm64 does not support /opt:ref, /opt:icf
+    if target and target:is_arch("arm64") then
+        return
+    end
+
     -- @note we explicitly strip some useless code, because `/debug` may keep them
     -- @see https://github.com/xmake-io/xmake/issues/907
     if level == "all" then
