3df1ad8688967a638f3ef5825a0011505c02dc55
ruki <waruqi@gmail.com>
2023-05-02 00:56:39 +0800

    improve xmake generator

--- xmake/scripts/conan/extensions/generators/xmake_generator.py
+++ xmake/scripts/conan/extensions/generators/xmake_generator.py
@@ -12,6 +12,30 @@ class XmakeGenerator:
     def __init__(self, conanfile):
         self._conanfile = conanfile
     def generate(self):
-        print("XmakeGenerator generate")
+        print("XmakeGenerator: generating build info ..")
+
+        # Extract all dependencies
+        host_req = self._conanfile.dependencies.host
+        test_req = self._conanfile.dependencies.test
+        build_req = self._conanfile.dependencies.build
+
+        # Merge into one list
+        full_req = list(host_req.items()) \
+                   + list(test_req.items()) \
+                   + list(build_req.items())
+
+        # Process dependencies and accumulate globally required data
+        pkg_files = []
+        dep_names = []
+        for require, dep in full_req:
+            dep_name = require.ref.name
+            dep_names.append(dep_name)
+            print("dep_name", dep_name)
+
+            # Convert and aggregate dependency's
+            dep_cppinfo = dep.cpp_info.copy()
+            dep_cppinfo.set_relative_base_folder(dep.package_folder)
+            dep_aggregate = dep_cppinfo.aggregated_components()
+            print("dep_aggregate", dep_aggregate)
 
 
