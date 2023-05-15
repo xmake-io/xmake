c75621554028dece1350e9ed004b8f324b4878c9
fwqaaq <fwqaaq@gmail.com>
2023-05-15 23:07:32 +0800

    update

--- xmake/actions/create/main.lua
+++ xmake/actions/create/main.lua
@@ -31,9 +31,9 @@ end
 
 -- create project from template
 function _create_project(language, templateid, targetname)
-    if targetname == '.' then
-        raise("you should specify ${red}-P${reset} instead of directly using ${red}.${reset}")
-    end
+
+    -- check the targetname
+    assert(targetname ~= ".", "you should specify ${red}-P${reset} instead of directly using ${red}.${reset}")
 
     -- check the language
     assert(language, "no language!")
