9631ce418b854b39b1fc9a828d90b59cda02e865
ruki <waruqi@gmail.com>
2023-07-17 22:47:53 +0800

    revert tests

--- tests/projects/objc++/precompiled_header/xmake.lua
+++ tests/projects/objc++/precompiled_header/xmake.lua
@@ -3,6 +3,6 @@ add_rules("mode.debug", "mode.release")
 target("main")
     set_kind("binary")
     set_languages("cxx11")
-    set_pmheader("src/header.h")
+    set_pmxxheader("src/header.h")
     add_files("src/*.mm", "src/*.c", "*.mm")
 
