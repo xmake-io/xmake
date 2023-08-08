75e7ddd9d604fc4c762dbc0e187c4a0d0ae231cc
ruki <waruqi@gmail.com>
2023-08-08 23:40:49 +0800

    add cargo without std example

---
+++ tests/projects/rust/cargo_deps_without_std/src/main.rs
@@ -0,0 +1,10 @@
+// src/main.rs
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
---
+++ tests/projects/rust/cargo_deps_without_std/xmake.lua
@@ -0,0 +1,7 @@
+add_rules("mode.release", "mode.debug")
+add_requires("cargo::test", {configs = {cargo_toml = path.join(os.projectdir(), "Cargo.toml")}})
+
+target("test")
+    set_kind("binary")
+    add_files("src/main.rs")
+    add_packages("cargo::test")
