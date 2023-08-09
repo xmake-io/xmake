ff7d425096be0d1938c0730d20405c7dc01af09f
ruki <waruqi@gmail.com>
2023-08-09 22:28:42 +0800

    add stdmodules test with cpp only

---
+++ tests/projects/c++/modules/stdmodules_cpp_only/src/main.cpp
@@ -0,0 +1,9 @@
+import std;
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "hello world!" << endl;
+    return 0;
+}
---
+++ tests/projects/c++/modules/stdmodules_cpp_only/xmake.lua
@@ -0,0 +1,8 @@
+add_rules("mode.debug", "mode.release")
+
+target("stdmodules_cpp_only")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    set_policy("build.c++.modules", true)
+    set_languages("c++20")
+
