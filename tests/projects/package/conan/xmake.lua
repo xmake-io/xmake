fa88425b39b9548aa7b2a4411c455349d02f84e3
ruki <waruqi@gmail.com>
2023-10-16 22:40:09 +0800

    improve conan tests

--- tests/projects/package/conan/xmake.lua
+++ tests/projects/package/conan/xmake.lua
@@ -1,4 +1,5 @@
-add_requires("conan::zlib 1.2.11", {alias = "zlib", debug = true})
+add_requires("conan::zlib 1.2.11", {alias = "zlib", debug = true,
+    configs = {settings = "compiler.cppstd=14"}})
 add_requires("conan::openssl 1.1.1t", {alias = "openssl",
     configs = {options = "shared=True"}})
 
