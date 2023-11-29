da068da29b6978fa64923db3174f3817f8eaae74
ruki <waruqi@gmail.com>
2023-11-30 00:34:53 +0800

    improve to disconnect remote build

--- xmake/modules/private/service/remote_build/server.lua
+++ xmake/modules/private/service/remote_build/server.lua
@@ -100,9 +100,6 @@ function remote_build_server:_on_handle(stream, msg)
             end
             if msg:is_connect() then
                 session:open()
-            elseif msg:is_disconnect() then
-                session:close()
-                self._SESSIONS[session_id] = nil
             else
                 assert(session:is_connected(), "session has not been connected!")
                 if msg:is_diff() then
