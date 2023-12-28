945fa0908fbfed1f83020658e6fd9791bbc569b0
ruki <waruqi@gmail.com>
2023-12-28 14:25:49 +0800

    Update os.lua

--- xmake/core/base/os.lua
+++ xmake/core/base/os.lua
@@ -876,11 +876,11 @@ function os.execv(program, argv, opt)
             if waitok > 0 then
                 ok = status
             elseif waitok == 0 and opt.timeout then
-                print("kill .. ")
                 proc:kill()
-                print("kill end")
                 waitok, status = proc:wait(-1)
-                print("waitok", waitok, status)
+                if waitok > 0 then
+                    ok = status
+                end
                 errors = "wait process timeout"
             end
         else
