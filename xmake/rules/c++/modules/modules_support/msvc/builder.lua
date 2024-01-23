a1656243410a0d0b475835f347e8563065458844
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-01-23 21:36:56 +0100

    use execv instead of iorunv to keep syntax coloration of clang / cl output when compiling modules

--- xmake/rules/c++/modules/modules_support/msvc/builder.lua
+++ xmake/rules/c++/modules/modules_support/msvc/builder.lua
@@ -86,7 +86,7 @@ function _compile(target, flags, sourcefile)
 
     if not dryrun then
         -- do compile
-        assert(os.iorunv(compinst:program(), flags, {envs = msvc:runenvs()}))
+        assert(os.execv(compinst:program(), flags, {envs = msvc:runenvs()}))
     end
 end
 
