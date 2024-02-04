0d72b2ef9a60ee5a56cfc4f2d5d0eb7fc8176084
ruki <waruqi@gmail.com>
2024-02-04 09:27:30 +0800

    Update test.lua

--- tests/projects/c++/modules/circular_dependency/test.lua
+++ tests/projects/c++/modules/circular_dependency/test.lua
@@ -1,5 +1,7 @@
 import(".test_base", {alias = "test_build"})
 
 function main(t)
-  t:will_raise(test_build, "circular modules dependency detected")
+    if test_build.can_build() then
+        t:will_raise(test_build, "circular modules dependency detected")
+    end
 end
