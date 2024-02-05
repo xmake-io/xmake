a2d3c06fc509f749afbaedb90c1cbec7920e8207
ruki <waruqi@gmail.com>
2024-02-06 00:37:53 +0800

    improve test.lua

--- tests/projects/c/cosmocc/test.lua
+++ tests/projects/c/cosmocc/test.lua
@@ -1,5 +1,8 @@
 function main(t)
-    if is_host("windows", "macosx", "linux") then
+    if is_subhost("msys", "cygwin") then
+        os.exec("xmake f -p windows -vD")
+        os.exec("xmake -vD")
+    elseif is_host("macosx", "linux") then
         os.exec("xmake -vD")
     end
 end
