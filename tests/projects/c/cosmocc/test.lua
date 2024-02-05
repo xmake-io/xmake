8fa5433f3f8136c627dd594f1249edf175896cd8
ruki <waruqi@gmail.com>
2024-02-06 00:42:12 +0800

    fix test

--- tests/projects/c/cosmocc/test.lua
+++ tests/projects/c/cosmocc/test.lua
@@ -1,8 +1,8 @@
 function main(t)
     if is_subhost("msys", "cygwin") then
-        os.exec("xmake f -p windows -vD")
+        os.exec("xmake f -y -p windows -vD")
         os.exec("xmake -vD")
     elseif is_host("macosx", "linux") then
-        os.exec("xmake -vD")
+        os.exec("xmake -y -vD")
     end
 end
