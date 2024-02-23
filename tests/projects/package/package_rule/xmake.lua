b3fd12f939e8776ef66e81b4079119d8a70be730
ruki <waruqi@gmail.com>
2024-02-24 00:54:21 +0800

    switch to cpp

--- tests/projects/package/package_rule/xmake.lua
+++ tests/projects/package/package_rule/xmake.lua
@@ -5,7 +5,7 @@ add_repositories("myrepo ./repo")
 
 target("console")
     set_kind("binary")
-    add_files("src/main.c", "src/*.md")
+    add_files("src/main.cpp", "src/*.md")
     add_packages("foo")
     add_rules("@foo/test")
 
