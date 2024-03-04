4ff48f7a7027ab9c403f2448c918d2d1ded118fa
ruki <waruqi@gmail.com>
2024-03-05 00:02:34 +0800

    add select script tests

---
+++ tests/modules/private/select_script/test.lua
@@ -0,0 +1,23 @@
+import("private.core.base.select_script")
+
+function test_plat_only(t)
+end
+
+function test_arch_only(t)
+end
+
+function test_plat_arch(t)
+end
+
+function test_subhost_only(t)
+end
+
+function test_subarch_only(t)
+end
+
+function test_subarch_native(t)
+end
+
+function test_subhost_subarch(t)
+end
+
