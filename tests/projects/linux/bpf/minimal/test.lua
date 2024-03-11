b450d1d7e1f8da386fc8d5fcb0239232962a4eb0
ruki <waruqi@gmail.com>
2024-03-12 00:36:31 +0800

    update bpf ci

--- tests/projects/linux/bpf/minimal/test.lua
+++ tests/projects/linux/bpf/minimal/test.lua
@@ -1,12 +1,6 @@
 function main(t)
     if is_host("linux") and os.arch() == "x86_64" then
-        -- TODO, we need to wait fix of linux kernel
-        if linuxos.name() == "archlinux" then
-            return
-        end
-        --[[TODO
         os.vrun("xmake f -y -p android -vD")
         os.vrun("xmake -y -vD")
-        ]]
     end
 end
