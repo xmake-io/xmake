83abf6ad653df37a750c63afa7cae2acf03e11a4
ruki <waruqi@gmail.com>
2024-03-14 00:53:55 +0800

    improve test again

---
+++ tests/projects/c++/modules/partitions_implunit/src/foo.cpp
@@ -0,0 +1,8 @@
+// Foo.cpp
+module Foo;
+
+import :Impl;
+
+void foo() {
+    bar();
+}
--- tests/projects/c++/modules/partitions_implunit/src/main.cpp
+++ tests/projects/c++/modules/partitions_implunit/src/main.cpp
@@ -1,7 +1,6 @@
-import math;
+import Foo;
 
 int main(int argc, char** argv) {
-    auto a{ Math::lerchZeta(1, 2, 3) };
-    auto b{ Math::superLog(1, 2) };
+    foo();
     return 0;
 }
