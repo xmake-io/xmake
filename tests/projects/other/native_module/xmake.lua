15eb20076e2334387155c85b491c73da04e4a084
ruki <waruqi@gmail.com>
2024-04-01 23:24:42 +0800

    add native module tests

---
+++ tests/projects/other/native_module/xmake.lua
@@ -0,0 +1,14 @@
+add_rules("mode.debug", "mode.release")
+
+add_moduledirs("modules")
+
+target("test")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    on_config(function (target)
+        import("shared.foo")
+--        import("binary.bar")
+        print("1 + 1 = %d", foo.add(1, 1))
+--        print("1 - 1 = %s", bar.sub("1", "1"))
+    end)
+
