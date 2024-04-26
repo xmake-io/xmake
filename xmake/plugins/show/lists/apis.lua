539785cee40ea366af0128f5e6ca0b1a8a622855
ruki <waruqi@gmail.com>
2024-04-27 00:26:07 +0800

    ignore xmake.lua for apis

--- xmake/plugins/show/lists/apis.lua
+++ xmake/plugins/show/lists/apis.lua
@@ -275,7 +275,7 @@ function script_extension_module_apis()
     local moduledirs = module.directories()
     for _, moduledir in ipairs(moduledirs) do
         moduledir = path.absolute(moduledir)
-        local modulefiles = os.files(path.join(moduledir, "**.lua|xmake.lua|private/**.lua|core/tools/**.lua|detect/tools/**.lua"))
+        local modulefiles = os.files(path.join(moduledir, "**.lua|**/xmake.lua|private/**.lua|core/tools/**.lua|detect/tools/**.lua"))
         if modulefiles then
             for _, modulefile in ipairs(modulefiles) do
                 local modulename = path.relative(modulefile, moduledir)
