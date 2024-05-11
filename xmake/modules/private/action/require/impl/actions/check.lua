15e79877b449b729a96a1205033f3bda61bdb898
ruki <waruqi@gmail.com>
2024-05-11 09:07:01 +0800

    add check script for package

---
+++ xmake/modules/private/action/require/impl/actions/check.lua
@@ -0,0 +1,34 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        check.lua
+--
+
+-- imports
+import("core.base.option")
+import("core.project.config")
+
+-- check the given package
+function main(package, opt)
+    opt = opt or {}
+    local script = package:script("check")
+    if script then
+        script(package)
+    end
+end
+
+
