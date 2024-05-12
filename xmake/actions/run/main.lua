6cc8833def0b68bcb23fe2ad4e4ad10acfd90e73
ruki <waruqi@gmail.com>
2024-05-12 23:09:53 +0800

    fix autobuild

--- xmake/actions/run/main.lua
+++ xmake/actions/run/main.lua
@@ -212,7 +212,7 @@ function main()
         -- we need clear the previous config and reload it
         -- to avoid trigger recheck configs
         config.clear()
-        task.run("build")
+        task.run("build", {target = option.get("target")})
     end
 
     -- load targets
