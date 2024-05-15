304943e79b6e2c67d2c010f053d8cd2a42460ddd
ruki <waruqi@gmail.com>
2024-05-15 22:35:57 +0800

    improve gn #5100

--- xmake/modules/package/tools/gn.lua
+++ xmake/modules/package/tools/gn.lua
@@ -66,7 +66,9 @@ function _get_configs(package, configs, opt)
     elseif package:is_arch("arm.*") then
         configs.target_cpu = "arm"
     end
-    configs.is_debug = package:is_debug() and "true" or "false"
+    if configs.is_debug == nil then
+        configs.is_debug = package:is_debug() and true or false
+    end
     return configs
 end
 
