b89dbe20c3d5c93feab00521207611ee67f6c51c
HapiFive <1143910315@qq.com>
2024-05-15 09:42:56 +0800

    add test for ts

---
+++ tests/projects/qt/with_ts/xmake.lua
@@ -0,0 +1,9 @@
+add_rules("mode.debug", "mode.release")
+
+target("demo")
+    add_rules("qt.widgetapp")
+    add_headerfiles("src/*.h")
+    add_files("src/*.cpp")
+    add_files("src/mainwindow.ui")
+    add_files("src/mainwindow.h")
+    add_files("src/*.ts")
