51efbf568fb6329a3ad6c0919adc7e93e4f58aec
ruki <waruqi@gmail.com>
2024-07-12 00:42:05 +0800

    fix inherit links #5331

--- xmake/rules/utils/inherit_links/inherit_links.lua
+++ xmake/rules/utils/inherit_links/inherit_links.lua
@@ -20,7 +20,7 @@
 
 -- get values from target
 function _get_values_from_target(target, name)
-    local values = table.wrap(target:get(name))
+    local values = table.clone(table.wrap(target:get(name)))
     for _, value in ipairs((target:get_from(name, "option::*"))) do
         table.join2(values, value)
     end
