59d4566fc7a88cb53f00b29bb7e5d5ceb7608858
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-07-12 18:15:26 +0200

    add proper warning for std module detection

--- xmake/rules/c++/modules/modules_support/clang/compiler_support.lua
+++ xmake/rules/c++/modules/modules_support/clang/compiler_support.lua
@@ -300,6 +300,7 @@ function get_stdmodules(target)
             end
         end
     end
+    wprint("std and std.compat modules not found ! disabling them for the build")
 end
 
 function get_bmi_extension()
