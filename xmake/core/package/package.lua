8691433782a9d16b42d39603161909df034e0892
ruki <waruqi@gmail.com>
2024-07-15 22:36:45 +0800

    fix ldflags check

--- xmake/core/package/package.lua
+++ xmake/core/package/package.lua
@@ -2403,7 +2403,7 @@ function _instance:_generate_build_configs(configs, opt)
         local links = table.wrap(configs.links)
         local ldflags = table.wrap(configs.ldflags)
         local frameworks = table.wrap(configs.frameworks)
-        if #links == 0 and #ldflags and #frameworks == 0 then
+        if #links == 0 and #ldflags == 0 and #frameworks == 0 then
             os.raise("package(%s): links not found!", self:name())
         end
     end
