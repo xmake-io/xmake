a6eb7d056ca136aa532abc39c9fdf78d6f8f4ebd
ruki <waruqi@gmail.com>
2024-07-17 22:58:45 +0800

    use libzip as test

--- tests/actions/install/xmake.lua
+++ tests/actions/install/xmake.lua
@@ -2,12 +2,12 @@ add_rules("mode.debug", "mode.release")
 
 set_version("1.0.1", {soname = true})
 
-add_requires("libplist", {system = false, configs = {shared = true}})
+add_requires("libzip", {system = false, configs = {shared = true}})
 
 target("foo")
     set_kind("shared")
     add_files("src/foo.cpp")
-    add_packages("libplist", {public = true})
+    add_packages("libzip", {public = true})
     add_headerfiles("src/foo.h", {public = true})
     add_installfiles("src/foo.txt", {prefixdir = "assets", public = true})
     set_prefixdir("/", {bindir = "foo_bin", libdir = "foo_lib"})
