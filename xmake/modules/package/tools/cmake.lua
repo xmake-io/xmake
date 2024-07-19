b94cb84a5f6bb49e933618a7a1394a057257047c
ruki <waruqi@gmail.com>
2024-07-20 00:14:05 +0800

    fix pdb dir

--- xmake/modules/package/tools/cmake.lua
+++ xmake/modules/package/tools/cmake.lua
@@ -982,8 +982,7 @@ function _build_for_ninja(package, configs, opt)
     -- Fix pdb issue, if multiple CL.EXE write to the same .PDB file, please use /FS
     -- @see https://github.com/xmake-io/xmake/issues/5353
     if package:is_plat("windows") and package:has_tool("cxx", "cl") then
-        local buildir = opt.buildir or package:buildir()
-        local pdbdir = path.join(package:buildir(), "pdb")
+        local pdbdir = "pdb"
         if not os.isdir(pdbdir) then
             os.mkdir(pdbdir)
         end
@@ -1075,8 +1074,7 @@ function _install_for_ninja(package, configs, opt)
     -- Fix pdb issue, if multiple CL.EXE write to the same .PDB file, please use /FS
     -- @see https://github.com/xmake-io/xmake/issues/5353
     if package:is_plat("windows") and package:has_tool("cxx", "cl") then
-        local buildir = opt.buildir or package:buildir()
-        local pdbdir = path.join(package:buildir(), "pdb")
+        local pdbdir = "pdb"
         if not os.isdir(pdbdir) then
             os.mkdir(pdbdir)
         end
