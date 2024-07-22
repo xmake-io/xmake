6bc66f276570973aced27afc6dd56cc6ce5b2d3b
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-07-22 18:25:35 +0200

    add staticlib2 test

---
+++ tests/projects/c++/modules/staticlib2/src/bar.cpp
@@ -0,0 +1,8 @@
+module bar;
+
+namespace bar {
+    int hello() {
+        return 2;
+    }
+}
+
---
+++ tests/projects/c++/modules/staticlib2/src/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+import foo;
+
+int main() {
+    printf("%d\n", foo::hello());
+    return 0;
+}
+
