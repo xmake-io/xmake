694c0ac091263a7bfeeda55cfc5a6a816c516a95
ruki <waruqi@gmail.com>
2024-07-23 23:44:08 +0800

    fix pack io.gsub

--- xmake/plugins/pack/srpm/main.lua
+++ xmake/plugins/pack/srpm/main.lua
@@ -209,21 +209,33 @@ function _pack_srpm(rpmbuild, package)
     end
 
     -- replace variables in specfile
+    -- and we need to avoid `attempt to yield across a C-call boundary` in io.gsub
     local specvars = _get_specvars(package)
     local pattern = package:extraconf("specfile", "pattern") or "%${([^\n]-)}"
+    local specvars_names = {}
+    local specvars_values = {}
+    io.gsub(specfile, "(" .. pattern .. ")", function(_, name)
+        table.insert(specvars_names, name)
+    end)
+    for _, name in ipairs(specvars_names) do
+        name = name:trim()
+        if specvars_values[name] == nil then
+            local value = specvars[name]
+            if type(value) == "function" then
+                value = value()
+            end
+            if value ~= nil then
+                dprint("  > replace %s -> %s", name, value)
+            end
+            if type(value) == "table" then
+                dprint("invalid variable value", value)
+            end
+            specvars_values[name] = value
+        end
+    end
     io.gsub(specfile, "(" .. pattern .. ")", function(_, name)
         name = name:trim()
-        local value = specvars[name]
-        if type(value) == "function" then
-            value = value()
-        end
-        if value ~= nil then
-            dprint("  > replace %s -> %s", name, value)
-        end
-        if type(value) == "table" then
-            dprint("invalid variable value", value)
-        end
-        return value
+        return specvars_values[name]
     end)
 
     -- archive source files
