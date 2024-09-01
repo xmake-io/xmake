a8c60009ba4b17694e13b45b733819c2fc3dce8b
ruki <waruqi@gmail.com>
2024-09-01 22:25:27 +0800

    improve cosmocc for windows

--- tests/projects/c/cosmocc/console/xmake.lua
+++ tests/projects/c/cosmocc/console/xmake.lua
@@ -2,6 +2,12 @@ add_rules("mode.debug", "mode.release")
 
 add_requires("cosmocc")
 
+-- TODO, we should add envs to toolchains/cosmocc
+if is_subhost("windoows") then
+    add_requires("msys2")
+    add_packages("msys2")
+end
+
 target("test")
     set_kind("binary")
     add_files("src/*.c")
