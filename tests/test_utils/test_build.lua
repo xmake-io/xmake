3c33328bc25382bbfd0995fa0a62d8f5d2fda181
ruki <waruqi@gmail.com>
2024-09-16 23:58:20 +0800

    fix tests

--- tests/test_utils/test_build.lua
+++ tests/test_utils/test_build.lua
@@ -3,7 +3,7 @@ local test_build = {}
 function test_build:build(argv)
     os.exec("xmake f -c -D -y")
     os.exec("xmake")
-    os.exec("xmake f -c -D -y --policy=compatibility.version=3.0")
+    os.exec("xmake f -c -D -y --policies=compatibility.version=3.0")
     os.exec("xmake -r")
 end
 
