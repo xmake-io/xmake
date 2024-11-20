ef16d2434c22fc36c3a9d79bee9dfec6c00a5292
ruki <waruqi@gmail.com>
2024-11-21 00:09:35 +0800

    fix cmake flags

--- xmake/modules/package/tools/cmake.lua
+++ xmake/modules/package/tools/cmake.lua
@@ -1245,10 +1245,8 @@ function _shrink_cmake_arguments(argv, oldir, opt)
                 if build_type then
                     table.insert(cmake_argv, ("if(CMAKE_BUILD_TYPE STREQUAL \"%s\")"):format(build_type))
                 end
-                for _, flag in ipairs(os.argv(v)) do
-                    flag = flag:replace(" ", "\\ ")
-                    table.insert(cmake_argv, ("add_compile_options($<$<COMPILE_LANGUAGE:%s>:%s>)"):format(kind, flag))
-                end
+                local flags = v:replace(" ", "\\ ")
+                table.insert(cmake_argv, ("add_compile_options($<$<COMPILE_LANGUAGE:%s>:%s>)"):format(kind, flags))
                 if build_type then
                     table.insert(cmake_argv, "endif()")
                 end
