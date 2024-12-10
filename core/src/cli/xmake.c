f2c9a6c2669c90e275753e1c1628dad7fca595da
ruki <waruqi@gmail.com>
2024-12-10 23:51:23 +0800

    format code

--- core/src/cli/xmake.c
+++ core/src/cli/xmake.c
@@ -6,7 +6,6 @@
 /* //////////////////////////////////////////////////////////////////////////////////////
  * main
  */
-tb_int_t main(tb_int_t argc, tb_char_t** argv)
-{
+tb_int_t main(tb_int_t argc, tb_char_t** argv) {
     return xm_engine_run("xmake", argc, argv, tb_null, tb_null);
 }
