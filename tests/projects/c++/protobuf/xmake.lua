4fdfe591a19e81d9390a5c310b81f95d50ccf5f0
star9029 <hengxings783@gmail.com>
2024-12-11 19:00:13 +0800

    Improve protobuf test

--- tests/projects/c++/protobuf/xmake.lua
+++ tests/projects/c++/protobuf/xmake.lua
@@ -1,11 +1,19 @@
 add_rules("mode.debug", "mode.release")
+
 add_requires("protobuf-cpp")
+if is_cross() then
+    add_requires("protoc")
+end
 
 target("test")
     set_kind("binary")
-    set_languages("c++11")
-    add_packages("protobuf-cpp")
+    set_languages("c++17")
     add_rules("protobuf.cpp")
     add_files("src/*.cpp")
     add_files("src/**.proto", {proto_rootdir = "src"})
+
+    add_packages("protobuf-cpp")
+    if is_cross() then
+        add_packages("protoc")
+    end
 
