154345c0c6f52a72418b1d545215ec0693794b73
ruki <waruqi@gmail.com>
2025-01-08 00:05:44 +0800

    improve comments

--- xmake/core/project/project.lua
+++ xmake/core/project/project.lua
@@ -785,7 +785,10 @@ function project.filelock()
 end
 
 -- get the root configuration
--- and we get values in namespace, e.g. project.get("ns1::ns2::name"), project.get("target.ns1::ns2::name")
+--
+-- get root values in project, e.g project.get("name")
+-- get root values in target, e.g. project.get("target.name")
+-- get root values in specific namespace, e.g. project.get("ns1::ns2::name"), project.get("target.ns1::ns2::name")
 function project.get(name)
     local rootinfo
     if name and name:startswith("target.") then
