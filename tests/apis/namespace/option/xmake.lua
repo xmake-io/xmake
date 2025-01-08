672112971ac26e4ed5bc5789938a82e8dbc22c86
ruki <waruqi@gmail.com>
2025-01-09 00:42:04 +0800

    improve has_config test

--- tests/apis/namespace/option/xmake.lua
+++ tests/apis/namespace/option/xmake.lua
@@ -29,14 +29,16 @@ namespace("ns1", function ()
         add_deps("foo", "ns2::bar")
         add_files("src/main.cpp")
         add_options("opt0", "opt1", "ns2::opt2")
-        if has_config("opt0") then
-            add_defines("HAS_OPT0")
-        end
-        if has_config("opt1") then
-            add_defines("HAS_NS1_OPT1")
-        end
-        if has_config("ns2::opt2") then
-            add_defines("HAS_NS2_OPT2")
-        end
+        on_load(function (target)
+            if has_config("opt0") then
+                target:add("defines", "HAS_OPT0")
+            end
+            if has_config("opt1") then
+                target:add("defines", "HAS_NS1_OPT1")
+            end
+            if has_config("ns2::opt2") then
+                target:add("defines", "HAS_NS2_OPT2")
+            end
+        end)
 end)
 
