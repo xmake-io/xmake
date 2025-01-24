2d3944ab6162717fc252483351705230b5dd773e
ruki <waruqi@gmail.com>
2025-01-25 00:47:46 +0800

    remove comments

--- tests/apis/custom_toolchain/src/foo.h
+++ tests/apis/custom_toolchain/src/foo.h
@@ -2,14 +2,7 @@
 extern "C" {
 #endif
 
-/*! calculate add(a, b) 
- *
- * @param a     the first argument
- * @param b     the second argument
- *
- * @return      the result
- */
-int             add(int a, int b);
+int add(int a, int b);
 
 #ifdef __cplusplus
 }
