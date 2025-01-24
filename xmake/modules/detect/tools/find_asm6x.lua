99e74d6106e97fa3fc3315ef646d59deff01621f
ruki <waruqi@gmail.com>
2025-01-24 22:39:31 +0800

    add ti-c6000 toolchain

---
+++ xmake/modules/detect/tools/find_asm6x.lua
@@ -0,0 +1,48 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        find_asm6x.lua
+--
+
+-- imports
+import("lib.detect.find_program")
+import("lib.detect.find_programver")
+
+-- find asm6x
+--
+-- @param opt   the argument options, e.g. {version = true}
+--
+-- @return      program, version
+--
+-- @code
+--
+-- local asm6x = find_asm6x()
+-- local asm6x, version = find_asm6x({program = "asm6x", version = true})
+--
+-- @endcode
+--
+function main(opt)
+    opt = opt or {}
+    opt.check = "--help"
+    opt.command = "--help"
+    local program = find_program(opt.program or "asm6x", opt)
+    local version = nil
+    if program and opt.version then
+        version = find_programver(program, opt)
+    end
+    return program, version
+end
