669d2db9d3468aa0dc2edea9771ace9bb99f0d19
ruki <waruqi@gmail.com>
2025-01-25 00:48:10 +0800

    update comments

--- xmake/templates/c++/tbox.shared/project/src/_library/interface.h
+++ xmake/templates/c++/tbox.shared/project/src/_library/interface.h
@@ -11,14 +11,6 @@
  */
 __tb_extern_c_enter__
 
-
-/*! calculate add(a, b)
- *
- * @param a     the first argument
- * @param b     the second argument
- *
- * @return      the result
- */
 __tb_export__ tb_int_t add(tb_int_t a, tb_int_t b);
 
 /* //////////////////////////////////////////////////////////////////////////////////////
