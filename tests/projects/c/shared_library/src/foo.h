9a6a8ada1ddde0d88e6eb03cbf82a7f02f3ed7c3
ruki <waruqi@gmail.com>
2025-01-26 22:18:27 +0800

    format code

--- tests/projects/c/shared_library/src/foo.h
+++ tests/projects/c/shared_library/src/foo.h
@@ -7,7 +7,7 @@ extern "C" {
 #elif defined(__GNUC__) && ((__GNUC__ >= 4) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 3))
 #   define __export         __attribute__((visibility("default")))
 #else
-#   define __export         
+#   define __export
 #endif
 
 __export int    add(int a, int b);
