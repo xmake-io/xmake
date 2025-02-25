764a267a30d0ad8f6bb360562463d257594e2474
ruki <waruqi@gmail.com>
2025-02-25 22:52:53 +0800

    add kotlin-native package stub

---
+++ xmake/modules/package/manager/kotlin-native/install_package.lua
@@ -0,0 +1,46 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        install_package.lua
+--
+
+-- imports
+import("core.base.option")
+import("core.base.json")
+import("core.base.semver")
+import("lib.detect.find_tool")
+import("package.manager.kotlin-native.configurations")
+import("net.http")
+
+-- install package
+--
+-- @param name  the package name, e.g. org.jetbrains.kotlinx:kotlinx-serialization-json 1.8.0
+-- @param opt   the options, e.g. {verbose = true}
+--
+function main(name, opt)
+    opt = opt or {}
+    local configs = opt.configs or {}
+
+    -- init triplet
+    local arch = opt.arch
+    local plat = opt.plat
+    plat = configurations.plat(plat)
+    arch = configurations.arch(arch)
+    local triplet = configurations.triplet(plat, arch)
+
+
+end
