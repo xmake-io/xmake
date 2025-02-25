b5ec74aa63fd8c0888400a9b6bd075b19da5c4c3
ruki <waruqi@gmail.com>
2025-02-25 22:56:49 +0800

    select package version

--- xmake/modules/package/manager/kotlin-native/install_package.lua
+++ xmake/modules/package/manager/kotlin-native/install_package.lua
@@ -20,11 +20,33 @@
 
 -- imports
 import("core.base.option")
+import("core.base.global")
 import("core.base.json")
 import("core.base.semver")
 import("lib.detect.find_tool")
 import("package.manager.kotlin-native.configurations")
 import("net.http")
+
+-- select package version
+-- e.g. https://repo.maven.apache.org/maven2/org/jetbrains/kotlinx/kotlinx-serialization-json-iosarm64/maven-metadata.xml
+function _select_package_version(name, opt)
+    local installdir = opt.installdir
+    local manifest_file = path.join(installdir, "maven-metadata.xml")
+    for _, repository in ipairs(opt.repositories) do
+        local manifest_url = ("%s/%s-%s/maven-metadata.xml", repository, (name:gsub("%.", "/"):gsub(":", "/")), opt.triplet)
+        local ok = try {
+            function()
+                http.download(manifest_url, manifest_file, {
+                    insecure = global.get("insecure-ssl")})
+                return true
+            end
+        }
+        if ok and os.isfile(manifest_file) then
+            local manifest = io.readfile(manifest_file)
+            print(manifest)
+        end
+    end
+end
 
 -- install package
 --
@@ -34,6 +56,7 @@ import("net.http")
 function main(name, opt)
     opt = opt or {}
     local configs = opt.configs or {}
+    local repositories = opt.repositories
 
     -- init triplet
     local arch = opt.arch
@@ -41,6 +64,21 @@ function main(name, opt)
     plat = configurations.plat(plat)
     arch = configurations.arch(arch)
     local triplet = configurations.triplet(plat, arch)
+
+    -- select version
+    local installdir = assert(opt.installdir, "installdir not found!")
+    local require_version = opt.require_version
+    if require_version == "latest" then
+        require_version = nil
+    end
+    local version, repository = _select_package_version(name, {
+        triplet = triplet,
+        version = require_version,
+        installdir = installdir,
+        repositories = repositories})
+    assert(version and repository, "package(%s): %s not found for %s!", name, require_version, triplet)
+
+
 
 
 end
