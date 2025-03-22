62286fff6ff52b2a23ba8a681708eb14c510cbbd
ruki <waruqi@gmail.com>
2025-03-22 22:28:30 +0800

    support for job group

--- tests/modules/async/run_jobgraph.lua
+++ tests/modules/async/run_jobgraph.lua
@@ -10,8 +10,8 @@ function _jobfunc(index, total, opt)
     print("%s: run job (%d/%d) end, progress: %s, dt: %d ms", scheduler.co_running(), index, total, opt.progress, dt)
 end
 
-function main()
-    print("==================================== test jobgraph ====================================")
+function _test_basic()
+    print("==================================== test basic ====================================")
     local jobs = jobgraph.new()
     jobs:add("job/root", _jobfunc)
     for i = 1, 3 do
@@ -25,5 +25,27 @@ function main()
     runjobs("test", jobs, {comax = 6, timeout = 1000, timer = function (running_jobs_indices)
         print("%s: timeout (%d ms), running: %s", scheduler.co_running(), os.mclock() - t, table.concat(running_jobs_indices, ","))
     end})
+end
+
+function _test_group()
+    print("==================================== test group ====================================")
+    local jobs = jobgraph.new()
+    jobs:add("job/root", _jobfunc)
+    for i = 1, 3 do
+        jobs:add("job/" .. i, _jobfunc, {groups = "bar"})
+        for j = 1, 50 do
+            jobs:add("job/" .. i .. "/" .. j, _jobfunc, {groups = "foo"})
+        end
+    end
+    jobs:add_deps("foo", "bar", "job/root")
+    t = os.mclock()
+    runjobs("test", jobs, {comax = 6, timeout = 1000, timer = function (running_jobs_indices)
+        print("%s: timeout (%d ms), running: %s", scheduler.co_running(), os.mclock() - t, table.concat(running_jobs_indices, ","))
+    end})
+end
+
+function main()
+    _test_basic()
+    _test_group()
 end
 
