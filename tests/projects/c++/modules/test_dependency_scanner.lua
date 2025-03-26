601edb4396896db28c75b47d28e168566ec64897
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-03-26 15:11:05 +0100

    (C++ modules) disable std module on some tests
    this should improve C++ module tests speed

--- tests/projects/c++/modules/test_dependency_scanner.lua
+++ tests/projects/c++/modules/test_dependency_scanner.lua
@@ -44,32 +44,32 @@ function main(t)
     if is_subhost("windows") then
         local clang = find_tool("clang", {version = true})
         if clang and clang.version and semver.compare(clang.version, "17.0") >= 0 then
-            os.exec("xmake f --toolchain=clang -c --yes")
+            os.exec("xmake f --toolchain=clang -c --yes --policies=build.c++.modules.std:n")
             _build()
             os.exec("xmake clean -a")
-            os.exec("xmake f --toolchain=clang --runtimes=c++_shared -c --yes")
+            os.exec("xmake f --toolchain=clang --runtimes=c++_shared -c --yes --policies=build.c++.modules.std:n")
             _build()
         end
 
         os.exec("xmake clean -a")
-        os.exec("xmake f -c --yes")
+        os.exec("xmake f -c --yes --policies=build.c++.modules.std:n")
         _build()
     elseif is_subhost("msys") then
-        os.exec("xmake f -c -p mingw --yes")
+        os.exec("xmake f -c -p mingw --yes --policies=build.c++.modules.std:n")
         _build()
     elseif is_host("linux") then
         local gcc = find_tool("gcc", {version = true})
         if gcc and gcc.version and semver.compare(gcc.version, "11.0") >= 0 then
-            os.exec("xmake f -c --yes")
+            os.exec("xmake f -c --yes --policies=build.c++.modules.std:n")
             _build()
         end
         local clang = find_tool("clang", {version = true})
         if clang and clang.version and semver.compare(clang.version, "14.0") >= 0 then
             os.exec("xmake clean -a")
-            os.exec("xmake f --toolchain=clang -c --yes")
+            os.exec("xmake f --toolchain=clang -c --yes --policies=build.c++.modules.std:n")
             _build()
             os.exec("xmake clean -a")
-            os.exec("xmake f --toolchain=clang --runtimes=c++_shared -c --yes")
+            os.exec("xmake f --toolchain=clang --runtimes=c++_shared -c --yes --policies=build.c++.modules.std:n")
             _build()
         end
     end
