630b7ef31325690e5a11ce4f5bf353c2ca41bd6e
star9029 <hengxings783@gmail.com>
2025-04-02 13:30:15 +0800

    Add msvc midl support

---
+++ xmake/rules/platform/windows/idl/xmake.lua
@@ -0,0 +1,67 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, TBOOX Open Source Group.
+--
+-- @author      ruki
+-- @file        xmake.lua
+--
+
+-- add *.idl for rc file
+rule("platform.windows.idl")
+    set_extensions(".idl")
+
+    on_config(function (target)
+        local autogendir = path.join(target:autogendir(), "platform/windows/idl")
+        os.mkdir(autogendir)
+        -- compile rc file maybe require .tlb dir
+        target:add("includedirs", autogendir, {public = true})
+    end)
+
+    before_buildcmd_file(function (target, batchcmds, sourcefile, opt)
+        import("lib.detect.find_tool")
+        import("core.tool.toolchain")
+
+        local msvc = toolchain.load("msvc", {plat = os.host(), arch = os.arch()})
+        local midl = assert(find_tool("midl", {envs = msvc:runenvs(), toolchain = msvc}), "midl not found!")
+
+        local name = path.basename(sourcefile)
+        local autogendir = path.join(target:autogendir(), "platform/windows/idl")
+
+        local flags = {"/nologo"}
+        table.join2(flags, table.wrap(target:values("idl.flags")))
+        table.join2(flags, {
+            "/env",    target:is_arch("x86") and "win32" or target:arch(),
+            "/out",    autogendir,
+            "/header", name .. ".h",
+            "/iid",    name .. "_i.c",
+            "/proxy",  name .. ".p.c",
+            "/tlb",    name .. ".tlb",
+            sourcefile
+        })
+
+        batchcmds:show_progress(opt.progress, "${color.build.object}compiling.idl %s", sourcefile)
+        batchcmds:vrunv(midl.program, flags, {envs = msvc:runenvs()})
+        
+        local iid_file = path.join(autogendir, name .. "_i.c")
+        local objectfile = target:objectfile(iid_file)
+        table.insert(target:objectfiles(), objectfile)
+
+        batchcmds:show_progress(opt.progress, "${color.build.object}compiling.$(mode) %s", iid_file)
+        batchcmds:compile(iid_file, objectfile)
+
+        batchcmds:add_depfiles(sourcefile, iid_file)
+        batchcmds:set_depmtime(os.mtime(objectfile))
+        batchcmds:set_depcache(target:dependfile(objectfile))
+    end)
