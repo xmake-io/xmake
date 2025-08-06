a1a488a62267c9318aa4258e3eeaf759b1783abd
ruki <waruqi@gmail.com>
2025-08-06 22:55:22 +0800

    modify test

--- tests/projects/c++/package_linkgroups/test.lua
+++ tests/projects/c++/package_linkgroups/test.lua
@@ -1,13 +1,7 @@
 function main(t)
-
-    -- freebsd ci is slower
-    if is_host("bsd") then
+    if not is_host("linux") then
         return
     end
-
-    -- only for x86/x64, because it will take too long time on ci with arm/mips
-    if os.subarch():startswith("x") or os.subarch() == "i386" then
-        t:build()
-    end
+    t:build()
 end
 
