f01018e500132d3b9c5d1dd8a4933af13e25d431
star9029 <hengxings783@gmail.com>
2025-08-11 21:56:49 +0800

    Improve protobuf test

--- tests/projects/c++/protobuf/xmake.lua
+++ tests/projects/c++/protobuf/xmake.lua
@@ -1,11 +1,14 @@
 add_rules("mode.debug", "mode.release")
 
+local language = "17"
+
 add_requires("protoc", "protobuf-cpp")
--- add_requireconfs("protoc.protobuf-cpp", {version = "1.0.0"})
+-- add_requireconfs("protoc.protobuf-cpp", {override = true, version = "1.0.0"})
+add_requireconfs("**.abseil", {override = true, configs = {cxx_standard = language}})
 
 target("test")
     set_kind("binary")
-    set_languages("c++17")
+    set_languages("c++" .. language)
     add_rules("protobuf.cpp")
     add_files("src/*.cpp")
     add_files("src/**.proto", {proto_rootdir = "src"})
