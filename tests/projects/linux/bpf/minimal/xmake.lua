bdb88d2f6a7c8118018c701c9bd96d0c086ed8f3
ruki <waruqi@gmail.com>
2025-08-28 22:55:55 +0800

    fix bpf test

--- tests/projects/linux/bpf/minimal/xmake.lua
+++ tests/projects/linux/bpf/minimal/xmake.lua
@@ -4,7 +4,7 @@ add_rules("platform.linux.bpf")
 add_requires("linux-tools", {configs = {bpftool = true}})
 add_requires("libbpf")
 if is_plat("android") then
-    add_requires("ndk >=22.0<26.0")
+    add_requires("ndk >=22.0 <26.0")
     set_toolchains("@ndk", {sdkver = "23"})
 else
     add_requires("llvm >=10.x")
