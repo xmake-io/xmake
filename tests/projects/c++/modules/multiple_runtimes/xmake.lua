0d92314dcb782bd460a1f1771acb4deb71152e73
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-09-15 03:22:35 +0200

    nfc(C++ modules) add a test for multiple c++ runtimes

---
+++ tests/projects/c++/modules/multiple_runtimes/xmake.lua
@@ -0,0 +1,31 @@
+add_rules("mode.debug", "mode.release")
+add_files("src/*.cpp")
+set_languages("c++23")
+set_encodings("utf-8")
+
+if not is_plat("windows") then -- remove when std module is supported for libc++ on windows
+    target("llvm")
+        set_kind("binary")
+        set_toolchains("clang")
+        set_runtimes("c++_shared")
+        set_policy("build.c++.modules", true)
+        add_files("src/main.cpp")
+end
+
+if is_plat("linux") or is_plat("mingw") then
+    target("gnu")
+        set_kind("binary")
+        set_toolchains("gcc")
+        set_runtimes("stdc++_shared")
+        set_policy("build.c++.modules", true)
+        set_policy("build.c++.modules.gcc.cxx11abi", true)
+        add_files("src/main.cpp")
+end
+
+if is_plat("windows") then
+    target("msvc")
+        set_kind("binary")
+        set_toolchains("msvc")
+        set_policy("build.c++.modules", true)
+        add_files("src/main.cpp")
+end
