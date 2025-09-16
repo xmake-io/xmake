f5c16b8fca10d6565993d0e05c89c485880c579e
ruki <waruqi@gmail.com>
2025-09-16 22:43:38 +0800

    fix subsystem #6807

--- xmake/rules/platform/windows/subsystem/xmake.lua
+++ xmake/rules/platform/windows/subsystem/xmake.lua
@@ -39,15 +39,15 @@ rule("platform.windows.subsystem")
             assert(valid, "Invalid subsystem " .. subsystem)
 
             if target:has_tool("ld", "clang", "clangxx", "clang_cl") then
-                target:add("ldflags", "-Xlinker", "-subsystem:" .. subsystem, { force = true })
+                target:add("ldflags", "-Xlinker", "-subsystem:" .. subsystem, {force = true})
             elseif target:has_tool("ld", "link", "lld-link") then
-                target:add("ldflags", "/SUBSYSTEM:" .. subsystem:upper(), { force = true })
+                target:add("ldflags", "/SUBSYSTEM:" .. subsystem:upper(), {force = true})
             elseif target:has_tool("ld", "gcc", "gxx") then
-                target:add("ldflags", "-Wl,-m" .. subsystem, { force = true })
+                target:add("ldflags", "-m" .. subsystem, {force = true})
             elseif target:has_tool("ld", "lld") then
-                target:add("ldflags", "-subsystem:" .. subsystem, { force = true })
+                target:add("ldflags", "-subsystem:" .. subsystem, {force = true})
             elseif target:has_tool("ld", "ld") then
-                target:add("ldflags", "-m" .. subsystem, { force = true })
+                target:add("ldflags", "-m" .. subsystem, {force = true})
             end
         end
     end)
