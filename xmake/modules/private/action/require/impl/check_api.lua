6d7e810e07d1ec1d86c829fdd3cd7b73a27dde79
ShifftC <shifftc@gmail.com>
2025-09-24 11:04:54 +0200

    add api checking to require action

---
+++ xmake/modules/private/action/require/impl/check_api.lua
@@ -0,0 +1,32 @@
+--!A cross-platform build utility based on Lua
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2015-present, Xmake Open Source Community.
+--
+-- @author      Shiffted
+-- @file        check_api.lua
+--
+
+import("private.check.checker")
+import("private.check.show")
+
+function main(package)
+    local checkers = checker.checkers()
+    for name, info in table.orderpairs(checkers) do
+        if info.load then
+            local check = import("private.check.checkers." .. name, {anonymous = true})
+            check({package = package, show = show.wshow})
+        end
+    end
+end
