4d7ff03f1f95b4edb3e1406fab6290ff1068170f
ruki <waruqi@gmail.com>
2025-11-19 23:43:14 +0800

    fix tests

--- tests/projects/other/multiplats_xcode/test.lua
+++ tests/projects/other/multiplats_xcode/test.lua
@@ -3,6 +3,22 @@ function main(t)
         os.exec("xmake f -c -vD")
         os.exec("xmake -rvD")
         os.exec("xmake f -c -vD -p iphoneos")
-        os.exec("xmake -rvD")
+        -- build only arm64 target first, avoid building armv7 if SDK doesn't support it
+        os.exec("xmake -rvD test_iphoneos")
+        -- try to build armv7 target, skip if SDK doesn't support it
+        try
+        {
+            function ()
+                os.exec("xmake f -c -vD -p iphoneos -a armv7")
+                os.exec("xmake -rvD test_iphoneos_armv7")
+            end,
+            catch
+            {
+                function (errors)
+                    -- SDK doesn't support armv7, skip this target
+                    cprint("${color.warning}skip test_iphoneos_armv7: SDK doesn't support armv7")
+                end
+            }
+        }
     end
 end
