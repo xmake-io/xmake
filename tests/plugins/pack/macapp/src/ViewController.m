7a15f43c268a3cd192fedbcf6f0a65ab80f1646d
ruki <waruqi@gmail.com>
2025-11-26 00:49:39 +0800

    add pack macapp test

---
+++ tests/plugins/pack/macapp/src/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  macapp
+//
+//  Created by ruki on 2020/4/4.
+//  Copyright © 2020 tboox. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
+
