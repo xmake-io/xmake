96e1ffb8e2991322c58e2c7ff5864367d9bea529
ruki <waruqi@gmail.com>
2025-11-27 23:38:25 +0800

    add qtapp

---
+++ tests/plugins/pack/qtapp/src/mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle("Qt Widget App");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
---
+++ tests/plugins/pack/qtapp/src/mainwindow.h
@@ -0,0 +1,23 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
+
