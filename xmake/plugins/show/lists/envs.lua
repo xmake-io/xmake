edb55f6a7799181a2dfbd4eb8b6cf1f8be2f4f32
ruki <waruqi@gmail.com>
2025-12-03 00:31:47 +0800

    update show envs

--- xmake/plugins/show/lists/envs.lua
+++ xmake/plugins/show/lists/envs.lua
@@ -38,7 +38,11 @@ function main()
                     XMAKE_TMPDIR         = {"Set the temporary directory.", os.tmpdir()},
                     XMAKE_PROFILE        = {"Start profiler, e.g. perf:call, perf:tag, trace, stuck.", os.getenv("XMAKE_PROFILE")},
                     XMAKE_PKG_CACHEDIR   = {"Set the cache directory of packages.", os.getenv("XMAKE_PKG_CACHEDIR")},
-                    XMAKE_PKG_INSTALLDIR = {"Set the install directory of packages.", os.getenv("XMAKE_PKG_INSTALLDIR")}}
+                    XMAKE_PKG_INSTALLDIR = {"Set the install directory of packages.", os.getenv("XMAKE_PKG_INSTALLDIR")},
+                    XMAKE_MAIN_REPO      = {"Set the official package master repository url.", os.getenv("XMAKE_MAIN_REPO")},
+                    XMAKE_BINARY_REPO    = {"Set the official package pre-compiled repository url.", os.getenv("XMAKE_BINARY_REPO")},
+                    XMAKE_THEME          = {"Set theme.", os.getenv("XMAKE_THEME") or global.get("theme")},
+                    XMAKE_STATS          = {"Enable or disable user statistics.", os.getenv("XMAKE_STATS")}}
     local width = 24
     for name, env in pairs(envs) do
         cprint("${color.dump.string}%s${clear}%s%s", name, (" "):rep(width - #name), env[1])
