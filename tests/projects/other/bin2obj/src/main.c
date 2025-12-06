bce2cef4a7832f9df5b9e41e8b1689a5490249b3
ruki <waruqi@gmail.com>
2025-12-07 00:37:58 +0800

    improve test again

--- tests/projects/other/bin2obj/src/main.c
+++ tests/projects/other/bin2obj/src/main.c
@@ -7,10 +7,10 @@ extern const uint8_t _binary_data_bin_end[];
 extern const uint8_t _binary_image_png_start[];
 extern const uint8_t _binary_image_png_end[];
 
-const uint32_t _binary_data_bin_size = (uint32_t)(_binary_data_bin_end - _binary_data_bin_start);
-const uint32_t _binary_image_png_size = (uint32_t)(_binary_image_png_end - _binary_image_png_start);
-
 int main(int argc, char** argv) {
+    const uint32_t _binary_data_bin_size = (uint32_t)(_binary_data_bin_end - _binary_data_bin_start);
+    const uint32_t _binary_image_png_size = (uint32_t)(_binary_image_png_end - _binary_image_png_start);
+
     printf("data.bin: %s, size: %u\n", (const char*)_binary_data_bin_start, (unsigned int)_binary_data_bin_size);
     printf("image.png: %s, size: %u\n", (const char*)_binary_image_png_start, (unsigned int)_binary_image_png_size);
     return 0;
