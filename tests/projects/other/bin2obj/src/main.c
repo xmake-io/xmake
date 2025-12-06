f678e6dbef86bd841fb6ef1cb4a220e1c5a4bb43
ruki <waruqi@gmail.com>
2025-12-07 00:36:30 +0800

    improve test

--- tests/projects/other/bin2obj/src/main.c
+++ tests/projects/other/bin2obj/src/main.c
@@ -7,34 +7,11 @@ extern const uint8_t _binary_data_bin_end[];
 extern const uint8_t _binary_image_png_start[];
 extern const uint8_t _binary_image_png_end[];
 
+const uint32_t _binary_data_bin_size = (uint32_t)(_binary_data_bin_end - _binary_data_bin_start);
+const uint32_t _binary_image_png_size = (uint32_t)(_binary_image_png_end - _binary_image_png_start);
+
 int main(int argc, char** argv) {
-    // calculate size from start and end
-    const uint32_t _binary_data_bin_size = (uint32_t)(_binary_data_bin_end - _binary_data_bin_start);
-    const uint32_t _binary_image_png_size = (uint32_t)(_binary_image_png_end - _binary_image_png_start);
-    
-    printf("data.bin size: %u\n", (unsigned int)_binary_data_bin_size);
-    printf("data.bin start: %p\n", _binary_data_bin_start);
-    printf("data.bin end: %p\n", _binary_data_bin_end);
-    
-    printf("image.png size: %u\n", (unsigned int)_binary_image_png_size);
-    printf("image.png start: %p\n", _binary_image_png_start);
-    printf("image.png end: %p\n", _binary_image_png_end);
-    
-    // print first few bytes of data.bin
-    if (_binary_data_bin_size > 0) {
-        printf("data.bin first byte: 0x%02x\n", _binary_data_bin_start[0]);
-    }
-    if (_binary_data_bin_size > 1) {
-        printf("data.bin second byte: 0x%02x\n", _binary_data_bin_start[1]);
-    }
-    
-    // print first few bytes of image.png
-    if (_binary_image_png_size > 0) {
-        printf("image.png first byte: 0x%02x\n", _binary_image_png_start[0]);
-    }
-    if (_binary_image_png_size > 1) {
-        printf("image.png second byte: 0x%02x\n", _binary_image_png_start[1]);
-    }
-    
+    printf("data.bin: %s, size: %u\n", (const char*)_binary_data_bin_start, (unsigned int)_binary_data_bin_size);
+    printf("image.png: %s, size: %u\n", (const char*)_binary_image_png_start, (unsigned int)_binary_image_png_size);
     return 0;
 }
