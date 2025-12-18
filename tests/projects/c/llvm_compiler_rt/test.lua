b661a57d7a4d081c06672bf2331d205a1f9e10e4
ruki <waruqi@gmail.com>
2025-12-18 23:17:46 +0800

    fix check llvm on ci

--- tests/projects/c/llvm_compiler_rt/test.lua
+++ tests/projects/c/llvm_compiler_rt/test.lua
@@ -8,7 +8,12 @@ function run_test(toolchain_name)
         flags = "-vD"
     end
 
-    local toolchain_inst = toolchain.load(toolchain_name)
+    local plat = os.host()
+    local arch = os.arch()
+    if is_subhost("msys2") then
+        plat = "mingw"
+    end
+    local toolchain_inst = toolchain.load(toolchain_name, {plat = plat, arch = arch})
     if not toolchain_inst or not toolchain_inst:check() then
         wprint(toolchain_name .. " not found, skipping tests")
         return
