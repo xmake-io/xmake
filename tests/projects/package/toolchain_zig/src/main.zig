073670f5d2bf8461e32889a1c2ff5d02d8ccede2
ruki <waruqi@gmail.com>
2025-12-19 00:55:43 +0800

    update zig examples

--- tests/projects/package/toolchain_zig/src/main.zig
+++ tests/projects/package/toolchain_zig/src/main.zig
@@ -1,6 +1,5 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {s}!\n", .{"world"});
+    std.debug.print("Hello, {s}!\n", .{"world"});
 }
