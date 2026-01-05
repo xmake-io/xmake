f49b27c442870afeaf42a98fb187097845bf814d
ruki <waruqi@gmail.com>
2026-01-05 22:53:51 +0800

    add alpine ci

--- tests/projects/linux/bpf/minimal/test.lua
+++ tests/projects/linux/bpf/minimal/test.lua
@@ -1,5 +1,5 @@
 function main(t)
-    if is_host("linux") and os.arch() == "x86_64" then
+    if is_host("linux") and os.arch() == "x86_64" and linuxos.name() ~= "alpine" then
         os.vrun("xmake f -y -p android -vD")
         os.vrun("xmake -y -vD")
     end
--- tests/projects/package/components/test.lua
+++ tests/projects/package/components/test.lua
@@ -2,6 +2,9 @@ function main(t)
     if is_host("bsd", "solaris") or is_subhost("msys") then
         return
     end
+    if is_host("linux") and linuxos.name() == "alpine" then
+        return
+    end
 
     -- only for x86/x64, because it will take too long time on ci with arm/mips
     if os.subarch():startswith("x") or os.subarch() == "i386" then
