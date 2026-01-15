5e6df0bcfe9768e84545cd94466973435268aef1
ruki <waruqi@gmail.com>
2026-01-15 22:43:56 +0800

    add charset.c

--- core/src/xmake/io/file_convert.c
+++ core/src/xmake/io/file_convert.c
@@ -29,62 +29,7 @@
  * includes
  */
 #include "prefix.h"
-
-/* //////////////////////////////////////////////////////////////////////////////////////
- * globals
- */
-
-// the charset entry type
-typedef struct __xm_charset_entry_t {
-    // the charset type
-    tb_size_t type;
-
-    // the charset name
-    tb_char_t const *name;
-} xm_charset_entry_t, *xm_charset_entry_ref_t;
-
-// the charsets, @note: type & name is sorted
-static xm_charset_entry_t g_charsets[] = {
-    { TB_CHARSET_TYPE_ANSI, "ansi" },
-    { TB_CHARSET_TYPE_ASCII, "ascii" },
-    { TB_CHARSET_TYPE_GB2312, "gb2312" },
-    { TB_CHARSET_TYPE_GBK, "gbk" },
-    { TB_CHARSET_TYPE_ISO8859, "iso8859" },
-    { TB_CHARSET_TYPE_UCS2 | TB_CHARSET_TYPE_NE, "ucs2" },
-    { TB_CHARSET_TYPE_UCS4 | TB_CHARSET_TYPE_NE, "ucs4" },
-    { TB_CHARSET_TYPE_UTF16 | TB_CHARSET_TYPE_NE, "utf16" },
-    { TB_CHARSET_TYPE_UTF16 | TB_CHARSET_TYPE_BE, "utf16be" },
-    { TB_CHARSET_TYPE_UTF16 | TB_CHARSET_TYPE_NE, "utf16bom" },
-    { TB_CHARSET_TYPE_UTF16 | TB_CHARSET_TYPE_LE, "utf16le" },
-    { TB_CHARSET_TYPE_UTF16 | TB_CHARSET_TYPE_LE, "utf16lebom" },
-    { TB_CHARSET_TYPE_UTF32 | TB_CHARSET_TYPE_NE, "utf32" },
-    { TB_CHARSET_TYPE_UTF32 | TB_CHARSET_TYPE_BE, "utf32be" },
-    { TB_CHARSET_TYPE_UTF32 | TB_CHARSET_TYPE_LE, "utf32le" },
-    { TB_CHARSET_TYPE_UTF8, "utf8" },
-    { TB_CHARSET_TYPE_UTF8, "utf8bom" },
-};
-
-/* //////////////////////////////////////////////////////////////////////////////////////
- * finder
- */
-static tb_long_t xm_io_charset_comp_by_name(tb_iterator_ref_t iterator, tb_cpointer_t item, tb_cpointer_t name) {
-    return tb_stricmp(((xm_charset_entry_ref_t)item)->name, (tb_char_t const *)name);
-}
-static xm_charset_entry_ref_t xm_io_charset_find_by_name(tb_char_t const *name) {
-    // make iterator
-    tb_array_iterator_t array_iterator;
-    tb_iterator_ref_t   iterator =
-        tb_array_iterator_init_mem(&array_iterator, g_charsets, tb_arrayn(g_charsets), sizeof(xm_charset_entry_t));
-    tb_assert_and_check_return_val(iterator, tb_null);
-
-    // find it by the binary search
-    tb_size_t itor = tb_binary_find_all_if(iterator, xm_io_charset_comp_by_name, name);
-    if (itor != tb_iterator_tail(iterator)) {
-        return (xm_charset_entry_ref_t)tb_iterator_item(iterator, itor);
-    } else {
-        return tb_null;
-    }
-}
+#include "../utils/charset.h"
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
@@ -117,13 +62,13 @@ tb_int_t xm_io_file_convert(lua_State *lua) {
     // get charset type
     tb_size_t            ftype = TB_CHARSET_TYPE_UTF8;
     tb_size_t            ttype = TB_CHARSET_TYPE_UTF8;
-    xm_charset_entry_ref_t fentry = xm_io_charset_find_by_name(fname);
+    xm_charset_entry_ref_t fentry = xm_charset_find_by_name(fname);
     if (fentry) ftype = fentry->type;
     else {
         lua_pushfstring(lua, "invalid charset: %s", fname);
         lua_error(lua);
     }
-    xm_charset_entry_ref_t tentry = xm_io_charset_find_by_name(tname);
+    xm_charset_entry_ref_t tentry = xm_charset_find_by_name(tname);
     if (tentry) ttype = tentry->type;
     else {
         lua_pushfstring(lua, "invalid charset: %s", tname);
--- core/src/xmake/string/convert.c
+++ core/src/xmake/string/convert.c
@@ -29,59 +29,7 @@
  * includes
  */
 #include "prefix.h"
-
-/* //////////////////////////////////////////////////////////////////////////////////////
- * globals
- */
-
-// the charset entry type
-typedef struct __xm_charset_entry_t {
-    // the charset type
-    tb_size_t type;
-
-    // the charset name
-    tb_char_t const *name;
-} xm_charset_entry_t, *xm_charset_entry_ref_t;
-
-// the charsets, @note: type & name is sorted
-static xm_charset_entry_t g_charsets[] = {
-    { TB_CHARSET_TYPE_ANSI, "ansi" },
-    { TB_CHARSET_TYPE_ASCII, "ascii" },
-    { TB_CHARSET_TYPE_GB2312, "gb2312" },
-    { TB_CHARSET_TYPE_GBK, "gbk" },
-    { TB_CHARSET_TYPE_ISO8859, "iso8859" },
-    { TB_CHARSET_TYPE_UCS2 | TB_CHARSET_TYPE_NE, "ucs2" },
-    { TB_CHARSET_TYPE_UCS4 | TB_CHARSET_TYPE_NE, "ucs4" },
-    { TB_CHARSET_TYPE_UTF16 | TB_CHARSET_TYPE_NE, "utf16" },
-    { TB_CHARSET_TYPE_UTF16 | TB_CHARSET_TYPE_BE, "utf16be" },
-    { TB_CHARSET_TYPE_UTF16 | TB_CHARSET_TYPE_LE, "utf16le" },
-    { TB_CHARSET_TYPE_UTF32 | TB_CHARSET_TYPE_NE, "utf32" },
-    { TB_CHARSET_TYPE_UTF32 | TB_CHARSET_TYPE_BE, "utf32be" },
-    { TB_CHARSET_TYPE_UTF32 | TB_CHARSET_TYPE_LE, "utf32le" },
-    { TB_CHARSET_TYPE_UTF8, "utf8" },
-};
-
-/* //////////////////////////////////////////////////////////////////////////////////////
- * finder
- */
-static tb_long_t xm_string_charset_comp_by_name(tb_iterator_ref_t iterator, tb_cpointer_t item, tb_cpointer_t name) {
-    return tb_stricmp(((xm_charset_entry_ref_t)item)->name, (tb_char_t const *)name);
-}
-static xm_charset_entry_ref_t xm_string_charset_find_by_name(tb_char_t const *name) {
-    // make iterator
-    tb_array_iterator_t array_iterator;
-    tb_iterator_ref_t   iterator =
-        tb_array_iterator_init_mem(&array_iterator, g_charsets, tb_arrayn(g_charsets), sizeof(xm_charset_entry_t));
-    tb_assert_and_check_return_val(iterator, tb_null);
-
-    // find it by the binary search
-    tb_size_t itor = tb_binary_find_all_if(iterator, xm_string_charset_comp_by_name, name);
-    if (itor != tb_iterator_tail(iterator)) {
-        return (xm_charset_entry_ref_t)tb_iterator_item(iterator, itor);
-    } else {
-        return tb_null;
-    }
-}
+#include "../utils/charset.h"
 
 /* //////////////////////////////////////////////////////////////////////////////////////
  * implementation
@@ -109,8 +57,8 @@ tb_int_t xm_string_convert(lua_State *lua) {
     tb_check_return_val(src_cstr && ftype_cstr && ttype_cstr, 0);
 
     // find charsets
-    xm_charset_entry_ref_t fcharset = xm_string_charset_find_by_name(ftype_cstr);
-    xm_charset_entry_ref_t tcharset = xm_string_charset_find_by_name(ttype_cstr);
+    xm_charset_entry_ref_t fcharset = xm_charset_find_by_name(ftype_cstr);
+    xm_charset_entry_ref_t tcharset = xm_charset_find_by_name(ttype_cstr);
     luaL_argcheck(lua, fcharset, 2, "charset not found");
     luaL_argcheck(lua, tcharset, 3, "charset not found");
     tb_check_return_val(fcharset && tcharset, 0);
