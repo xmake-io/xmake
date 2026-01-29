e7fba25e4f5ea63b0dd2bcb0a827b89edde3e777
Saikari <lin@sz.cn.eu.org>
2026-01-29 17:44:43 +0300

    Refactor access.c to improve documentation and formatting

--- core/src/xmake/os/access.c
+++ core/src/xmake/os/access.c
@@ -1,5 +1,38 @@
+/*!A cross-platform build utility based on Lua
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2015-present, Xmake Open Source Community.
+ *
+ * @author      ruki
+ * @file        access.c
+ *
+ */
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * trace
+ */
+#define TB_TRACE_MODULE_NAME "access"
+#define TB_TRACE_MODULE_DEBUG (0)
+
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * includes
+ */
 #include "prefix.h"
 
+/* //////////////////////////////////////////////////////////////////////////////////////
+ * implementation
+ */
 tb_int_t xm_os_access(lua_State *lua) {
     tb_assert_and_check_return_val(lua, 0);
 
