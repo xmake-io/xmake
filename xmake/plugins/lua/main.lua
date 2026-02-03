f99b99b2ae3cfeb47356e6542c393bba009f8583
ruki <waruqi@gmail.com>
2026-02-03 10:17:50 +0800

    Refactor script content handling in main.lua
    Removed Windows-specific shell handling for script content.

--- xmake/plugins/lua/main.lua
+++ xmake/plugins/lua/main.lua
@@ -51,16 +51,6 @@ function _get_script_from_stdin()
     if script_content then
         -- remove utf8 bom
         script_content = script_content:ltrim(utf8.bom)
-        local shell = os.shell()
-        if shell == "cmd" or shell == "powershell" or shell == "pwsh" or is_host("windows") then
-            local trimmed = script_content:trim()
-            if trimmed:startswith('"') and trimmed:endswith('"') then
-                script_content = trimmed:trim('"')
-            else
-                script_content = trimmed
-            end
-        end
-
         if not script_content:find("function main", 1, true) then
             script_content = "function main(...)\n" .. script_content .. "\nend"
         end
