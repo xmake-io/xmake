cc60f116f272ac596b1ab1f779e02aecc719e8eb
ruki <waruqi@gmail.com>
2026-02-11 00:50:01 +0800

    improve xcode toolchain

--- xmake/toolchains/xcode/xmake.lua
+++ xmake/toolchains/xcode/xmake.lua
@@ -25,44 +25,4 @@ toolchain("xcode")
     set_runtimes("c++_static", "c++_shared", "stdc++_static", "stdc++_shared")
 
     on_check("check")
-    on_load(function (toolchain)
-
-        -- set toolset
-        local arch              = toolchain:arch()
-        local bindir            = toolchain:bindir()
-        local appledev          = toolchain:config("appledev")
-        local xc_clang          = bindir and path.join(bindir, "clang") or "clang"
-        local xc_clangxx        = bindir and path.join(bindir, "clang++") or "clang++"
-        local xc_ar             = bindir and path.join(bindir, "ar") or "ar"
-        local xc_strip          = bindir and path.join(bindir, "strip") or "strip"
-        local xc_swift_frontend = bindir and path.join(bindir, "swift-frontend") or "swift-frontend"
-        local xc_swiftc         = bindir and path.join(bindir, "swiftc") or "swiftc"
-        local xc_dsymutil       = bindir and path.join(bindir, "dsymutil") or "dsymutil"
-
-        toolchain:set("toolset", "cc", xc_clang)
-        toolchain:set("toolset", "cxx", xc_clangxx, xc_clang)
-        toolchain:set("toolset", "ld", xc_clangxx, xc_clang)
-        toolchain:set("toolset", "sh", xc_clangxx, xc_clang)
-        toolchain:set("toolset", "ar", xc_ar)
-        toolchain:set("toolset", "strip", xc_strip)
-        toolchain:set("toolset", "dsymutil", xc_dsymutil, "dsymutil")
-        toolchain:set("toolset", "mm", xc_clang)
-        toolchain:set("toolset", "mxx", xc_clangxx, xc_clang)
-        toolchain:set("toolset", "sc", xc_swift_frontend, "swift_frontend", xc_swiftc, "swiftc")
-        toolchain:set("toolset", "scld", xc_swiftc, "swiftc")
-        toolchain:set("toolset", "scsh", xc_swiftc, "swiftc")
-        toolchain:set("toolset", "scar", xc_swiftc, "swiftc")
-        if arch then
-            toolchain:set("toolset", "cpp", xc_clang .. " -arch " .. arch .. " -E")
-        end
-        if toolchain:is_plat("macosx") then
-            toolchain:set("toolset", "as", xc_clang)
-        elseif appledev == "simulator" or appledev == "catalyst" then
-            toolchain:set("toolset", "as", xc_clang)
-        else
-            toolchain:set("toolset", "as", path.join(os.programdir(), "scripts", "gas-preprocessor.pl " .. xc_clang))
-        end
-
-        -- load configurations
-        import("load_" .. toolchain:plat())(toolchain)
-    end)
+    on_load("load")
