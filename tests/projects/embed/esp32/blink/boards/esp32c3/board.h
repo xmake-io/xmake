3c40bc1cbd5515256c36ab7ea6a45c0d4bc203e3
ruki <waruqi@gmail.com>
2026-08-15 23:11:34 +0800

    add blink test

---
+++ tests/projects/embed/esp32/blink/boards/esp32c3/board.h
@@ -0,0 +1,7 @@
+// the board configuration of esp32c3, it's selected by the `board` option
+//
+// @note gpio12-17 are the spi flash of the esp32c3, do not use them
+//
+#pragma once
+
+#define LED_GPIO 8
---
+++ tests/projects/embed/esp32/blink/xmake-addons.lua
@@ -0,0 +1,2 @@
+-- the addon is installed automatically when we load this project
+add_addons("esp32-devel")
